147742fe87d1f82dfd59153fe710a61eb6c5977a
vladislav-pochernin <vspochernin@gmail.com>
2021-02-05 11:57:31 +0300

    Delete comments
    Deletd all comments because they were bad-written and were written in
    Russian

--- pochernin.vladislav/A1/circle.hpp
+++ pochernin.vladislav/A1/circle.hpp
@@ -7,25 +7,19 @@
 class Circle : public Shape
 {
 public:
-  // Консутрктор без параметров.
   Circle() : radius_(0), pos_(point_t(0,0)) {}
 
-  // Конструктор с параметрами.
   Circle(const double radius, const point_t pos)
     : radius_(radius),
     pos_(pos)
   {}
 
-  // Вычисление площади.
   virtual double getArea() const override;
 
-  // Получение ограничивающего прямоугольника для фигуры.
   virtual rectangle_t getFrameRect() const override;
 
-  // Перемещение в конкретную точку.
   virtual void move(point_t destination) override;
 
-  // Смещение по осям абцисс и ординат.
   virtual void move(double dx, double dy) override;
 
 private:
--- pochernin.vladislav/A1/rectangle.hpp
+++ pochernin.vladislav/A1/rectangle.hpp
@@ -7,30 +7,23 @@
 class Rectangle : public Shape
 {
 public:
-  // Конструктор без параметров.
   Rectangle() : width_(0), heigth_(0), pos_(point_t()) {}
 
-  // Конструктор с параметрами.
   Rectangle(const double width, const double heigth, const point_t pos)
     : width_(width),
     heigth_(heigth),
     pos_(pos)
   {}
 
-  // Вычисление площади.
   virtual double getArea() const override;
 
-  // Получение ограничивающего прямоугольника для фигуры.
   virtual rectangle_t getFrameRect() const override;
 
-  // Перемещение в конкретную точку.
   virtual void move(point_t destination) override;
 
-  // Смещение по осям абцисс и ординат.
   virtual void move(double dx, double dy) override;
 
 private:
-  // TODO: Надо ли тут инициализировать?
   double width_ = 0;
   double heigth_ = 0;
   point_t pos_ = point_t(0, 0);

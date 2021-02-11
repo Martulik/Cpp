13f630bc16e83ddb6a4ea2d0b72f4c9a72d39a9a
Marta <ezerinya.m@edu.spbstu.ru>
2021-02-11 12:18:49 +0000

    Update ezerinia.marta/A1/circle.hpp

--- ezerinia.marta/A1/circle.hpp
+++ ezerinia.marta/A1/circle.hpp
@@ -9,9 +9,9 @@ public:
   Circle();
   Circle(double radius, point_t center);
   virtual ~Circle();
-  virtual void showShape() const override;
+  virtual void showShape() override;
   virtual double getArea() const override;
-  virtual rectangle_t getFrameRect() override;
+  virtual rectangle_t getFrameRect() const override;
   virtual void move(const point_t& point) override;
   virtual void move(double x, double y) override;
 private:

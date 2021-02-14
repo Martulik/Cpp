db2754b45bcc85e8ea0a163558d2f976672a239f
Ivanova Maria <ivanova5.mv@edu.spbstu.ru>
2021-02-15 00:25:51 +0300

    Initial commit

---
+++ ivanova.maria/A1/circle.hpp
@@ -0,0 +1,32 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "shape.hpp"
+
+#include <ostream>
+
+class Circle : public Shape
+{
+public:
+  Circle();
+
+  Circle(int rad, point_t ctr);
+
+  virtual ~Circle();
+
+  virtual double getArea() const override;
+
+  virtual rectangle_t getFrameRect() const;
+
+  virtual void move(const point_t &newPos) override;
+
+  virtual void move(int dx, int dy) override;
+
+  virtual void outShape () override;
+private:
+  point_t ctr_;
+  int radius_;
+
+};
+
+#endif

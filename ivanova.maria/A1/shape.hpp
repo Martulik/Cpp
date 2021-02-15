9bcc15151575f62accd8b986f45c449b6a2e59c1
Ivanova Maria <ivanova5.mv@edu.spbstu.ru>
2021-02-15 19:38:23 +0300

    added destructor in shape.hpp

--- ivanova.maria/A1/shape.hpp
+++ ivanova.maria/A1/shape.hpp
@@ -6,6 +6,9 @@
 class Shape
 {
 public:
+  virtual ~Shape()
+  {
+  }
   virtual double getArea() const =0;
   virtual rectangle_t getFrameRect() const =0;
   virtual void move(const point_t &newPos) = 0;

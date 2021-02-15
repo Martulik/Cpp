14e609cfcc8ae20374a2d72616657525dd5c4d75
Danil Doroshin <ddddanil5555@gmail.com>
2021-02-15 10:05:48 +0300

    Added shape.hpp with abstract class Shape

---
+++ doroshin.danil/A1/shape.hpp
@@ -0,0 +1,18 @@
+#ifndef __SHAPE_HPP__
+#define __SHAPE_HPP__
+
+#include "base_types.hpp"
+
+class Shape
+{
+public:
+  virtual double getArea() const = 0;
+
+  virtual rectangle_t getFrameRect() const = 0;
+
+  virtual void move(point_t vec, bool absolute = false) = 0;
+
+  virtual ~Shape() {};
+};
+
+#endif //__SHAPE_HPP__

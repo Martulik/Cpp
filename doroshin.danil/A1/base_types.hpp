34d7c7172c84847998168e984a7896c74349a1da
Danil Doroshin <ddddanil5555@gmail.com>
2021-02-15 10:03:29 +0300

    Added base_types.hpp

---
+++ doroshin.danil/A1/base_types.hpp
@@ -0,0 +1,15 @@
+#ifndef __BASE_TYPES_HPP__
+#define __BASE_TYPES_HPP__
+
+struct point_t
+{
+  double x, y;
+};
+
+struct rectangle_t
+{
+  double width, height;
+  point_t pos;
+};
+
+#endif //__BASE_TYPES_HPP__

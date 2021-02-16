69d8ef4e970cb7fe0ddcdefaf4ca61d8d09e3681
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-02-16 19:15:04 +0300

    Correcting tabs

--- diurdeva.dasha/A1/circle.hpp
+++ diurdeva.dasha/A1/circle.hpp
@@ -3,17 +3,21 @@
 
 #include "shape.hpp"
 
-class Circle: public Shape
-{
+class Circle: public Shape {
 public:
-	Circle(double rad, point_t& pos);
-	double getArea() const override;
-	rectangle_t getFrameRect() const override;
-	void move(const point_t& newCenter) override;
-	void move(double axisX, double axisY) override;
+  Circle(double rad, point_t &pos);
+
+  double getArea() const override;
+
+  rectangle_t getFrameRect() const override;
+
+  void move(const point_t &newCenter) override;
+
+  void move(double axisX, double axisY) override;
+
 private:
-	double rad_;
-	point_t pos_;
+  double rad_;
+  point_t pos_;
 };
 
 #endif
--- diurdeva.dasha/A1/shape.hpp
+++ diurdeva.dasha/A1/shape.hpp
@@ -3,14 +3,17 @@
 
 #include "base-types.hpp"
 
-class Shape
-{
+class Shape {
 public:
-	virtual ~Shape() = default;
-	virtual double getArea() const = 0;
-	virtual rectangle_t getFrameRect() const = 0;
-	virtual void move(const point_t& pos) = 0;
-	virtual void move(double axisX, double axisY) = 0;
+  virtual ~Shape() = default;
+
+  virtual double getArea() const = 0;
+
+  virtual rectangle_t getFrameRect() const = 0;
+
+  virtual void move(const point_t &pos) = 0;
+
+  virtual void move(double axisX, double axisY) = 0;
 };
 
 #endif

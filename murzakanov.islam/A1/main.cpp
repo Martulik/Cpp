9e1f7a1065e1a1fee0c8137dea8eb6d3beb8bbf0
Islam <murzakanovislam21@gmail.com>
2021-02-20 02:49:57 +0300

    add polymorphic behavior

--- murzakanov.islam/A1/main.cpp
+++ murzakanov.islam/A1/main.cpp
@@ -1,14 +1,19 @@
-#include <iostream>
+1#include <iostream>
 #include "circle.hpp"
 #include "rectangle.hpp"
 
 int main(){
-  Rectangle rectangle(5, 6, { 3, 2 });
-  Rectangle rectangle2(3, 1, { 0, 0 });
-  rectangle = rectangle2;
-  rectangle.move(2, 3);
-  Circle circle(2, { 0, 0 });
-  std::cout << circle.getArea() << std::endl;
-  std::cout << rectangle.getArea() << std::endl;
+  Shape* polyRectangle = new Rectangle(1, 2, { 3, 5 });
+  std::cout << "Rectangle's width is " << polyRectangle->getFrameRect().width << std::endl;
+  std::cout << "Rectangle's height is " << polyRectangle->getFrameRect().height << std::endl;
+  std::cout << "Rectangle's area is " << polyRectangle->getArea() << std::endl;
+  std::cout << "Rectangle's center is (" << polyRectangle->getFrameRect().pos.x
+  << ", " << polyRectangle->getFrameRect().pos.y << ")" << std::endl;
+
+  Shape* polyCircle = new Circle(5.25, { 13, -12 });
+  std::cout << "Circle's radius is " << polyCircle->getFrameRect().width / 2 << std::endl;
+  std::cout << "Circle's area is " << polyCircle->getArea() << std::endl;
+  delete polyRectangle;
+  delete polyCircle;
   return 0;
 }

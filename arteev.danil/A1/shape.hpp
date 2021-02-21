76cf30741e27eb9e36d86c19fd881d5094036602
DanilArteev <arteev.dd@edu.spbstu.ru>
2021-02-21 22:09:07 +0300

    Add shape.hpp

--- arteev.danil/A1/shape.hpp
+++ arteev.danil/A1/shape.hpp
@@ -5,12 +5,12 @@
 
 class Shape
 {
-public:	
-	virtual ~Shape() = default;
-	virtual double getArea() const = 0;
-	virtual rectangle_t getFrameRect() const = 0;
-	virtual void move(point_t pos) = 0;
-	virtual void move(double dx, double dy) = 0;  
+public:
+  virtual ~Shape() = default;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(point_t pos) = 0;
+  virtual void move(double dx, double dy) = 0;
 };
 
 #endif

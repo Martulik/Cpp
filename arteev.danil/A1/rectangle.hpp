8697d51d0b86dde43b68342fd6d367646230a1e9
DanilArteev <arteev.dd@edu.spbstu.ru>
2021-02-21 21:11:52 +0300

    Add rectangle.hpp

---
+++ arteev.danil/A1/rectangle.hpp
@@ -0,0 +1,21 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "shape.hpp"
+
+class Rectangle : public Shape
+{
+public:
+  Rectangle(double width, double height, point_t pos);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(point_t pos) override;
+  void move(double dx, double dy) override;
+private:
+  point_t pos_;
+  double width_;
+  double height_;
+
+};
+
+#endif

3884b1b86363e50b000748b0200bb0474b5cca83
Vladislav <vrazbusiness@mail.ru>
2021-02-22 00:26:56 +0300

    initial commit

---
+++ razukrantov.vladislav/A1/rectangle.cpp
@@ -0,0 +1,30 @@
+#include "rectangle.hpp"
+#include <iostream>
+
+Rectangle::Rectangle(double width, double height, point_t &center):
+	width_(width),
+	height_(height),
+	center_(center)
+{
+}
+
+double Rectangle::getArea() const
+{
+	return (width_ * height_);
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+	return rectangle_t{width_, height_, center_};
+}
+
+void Rectangle::move(const point_t& center)
+{
+	center_ = center;
+}
+
+void Rectangle::move(const double dx, const double dy)
+{
+	center_.x += dx;
+	center_.y += dy;
+}

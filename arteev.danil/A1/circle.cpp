758d3b4dac6f122b1c003026128e7b8a27c48b21
DanilArteev <arteev.dd@edu.spbstu.ru>
2021-02-21 21:10:23 +0300

    Add circle.cpp

---
+++ arteev.danil/A1/circle.cpp
@@ -0,0 +1,30 @@
+#include "circle.hpp"
+
+const double PI = 3.14;
+
+Circle::Circle(double radius, point_t pos):
+  radius_(radius),
+  pos_(pos)
+{
+}
+
+double Circle::getArea() const
+{ 
+  return  (PI * radius_ * radius_);
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+  return { 2 * radius_, 2 * radius_, pos_ };
+}
+
+void Circle::move(point_t pos)
+{
+  pos_ = pos;
+}
+
+void Circle::move(double dx, double dy)
+{
+  pos_.x = pos_.x + dx;
+  pos_.y = pos_.y + dy;
+}

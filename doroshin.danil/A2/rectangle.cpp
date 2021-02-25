178fda2189d18f2edeae9cab9a4e1c6500fb7c6c
Danil Doroshin <ddddanil5555@gmail.com>
2021-02-25 10:09:06 +0300

    Initial Moved code from the A1 lab

---
+++ doroshin.danil/A2/rectangle.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cassert>
+#include "rectangle.hpp"
+
+Rectangle::Rectangle(rectangle_t rect):
+  rect(rect)
+{
+  assert(rect.width >= 0);
+  assert(rect.height >= 0);
+}
+
+double Rectangle::getArea() const
+{
+  return rect.width * rect.height;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return rect;
+}
+
+void Rectangle::move(point_t vec, bool absolute)
+{
+  if (absolute) {
+    rect.pos = vec;
+  }
+  else {
+    rect.pos.x += vec.x;
+    rect.pos.y += vec.y;
+  }
+}

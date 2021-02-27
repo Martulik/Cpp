80fb59f313389e0e75e13ade5d53c470b14bb4dd
Danil Doroshin <ddddanil5555@gmail.com>
2021-02-27 21:04:39 +0300

    Reorganise tests

--- doroshin.danil/A3/test-main.cpp
+++ doroshin.danil/A3/test-main.cpp
@@ -1,52 +1,55 @@
+
 #include <cassert>
 #include <iostream>
 #include "composite-shape.hpp"
 
 namespace d = doroshin;
 
-void test_move_circle()
+void test_move_rel(d::Shape& s, d::point_t vec)
 {
   const auto eq = [](const d::rectangle_t& lhs, const d::rectangle_t& rhs) {
     return lhs.height == rhs.height && lhs.width == rhs.width;
   };
+  d::rectangle_t orig_frame = s.getFrameRect();
+  s.move_rel(vec);
+  assert(eq(orig_frame, s.getFrameRect()));
+}
+
+void test_move_abs(d::Shape& s, d::point_t vec)
+{
+  const auto eq = [](const d::rectangle_t& lhs, const d::rectangle_t& rhs) {
+    return lhs.height == rhs.height && lhs.width == rhs.width;
+  };
+  d::rectangle_t orig_frame = s.getFrameRect();
+  s.move_rel(vec);
+  assert(eq(orig_frame, s.getFrameRect()));
+}
+
+void test_move_circle()
+{
   d::Circle c{{1, 2}, 3};
-  d::rectangle_t orig_frame = c.getFrameRect();
-  c.move_rel({1, 2});
-  assert(eq(orig_frame, c.getFrameRect()));
-  c.move_rel({-5, 7});
-  assert(eq(orig_frame, c.getFrameRect()));
-  c.move_abs({65, -33});
-  assert(eq(orig_frame, c.getFrameRect()));
+
+  test_move_rel(c, {1, 2});
+  test_move_rel(c, {-5, 7});
+  test_move_abs(c, {65, -33});
 }
 
 void test_move_rectangle()
 {
-  const auto eq = [](const d::rectangle_t& lhs, const d::rectangle_t& rhs) {
-    return lhs.height == rhs.height && lhs.width == rhs.width;
-  };
   d::Rectangle r(d::rectangle_t {3, 4, {1, 2}});
-  d::rectangle_t orig_frame = r.getFrameRect();
-  r.move_rel({1, 2});
-  assert(eq(orig_frame, r.getFrameRect()));
-  r.move_rel({-5, 7});
-  assert(eq(orig_frame, r.getFrameRect()));
-  r.move_abs({65, -33});
-  assert(eq(orig_frame, r.getFrameRect()));
+
+  test_move_rel(r, {1, 2});
+  test_move_rel(r, {-5, 7});
+  test_move_abs(r, {65, -33});
 }
 
 void test_move_composite()
 {
-  const auto eq = [](const d::rectangle_t& lhs, const d::rectangle_t& rhs) {
-    return lhs.height == rhs.height && lhs.width == rhs.width;
-  };
   d::CompositeShape s {d::Rectangle(d::rectangle_t {2, 3, {-1, 0}}), d::Circle{{1, 1}, 2}};
-  d::rectangle_t orig_frame = s.getFrameRect();
-  s.move_rel({1, 2});
-  assert(eq(orig_frame, s.getFrameRect()));
-  s.move_rel({-5, 7});
-  assert(eq(orig_frame, s.getFrameRect()));
-  s.move_abs({65, -33});
-  assert(eq(orig_frame, s.getFrameRect()));
+
+  test_move_rel(s, {1, 2});
+  test_move_rel(s, {-5, 7});
+  test_move_abs(s, {65, -33});
 }
 
 int main()

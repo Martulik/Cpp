7429c31f2cc9939e568dca9da1c5d8c775f24329
vladislav-pochernin <vspochernin@gmail.com>
2021-02-27 18:49:38 +0300

    Change main.cpp
    -Сделал функцию main более короткой и читаемой.
    -Изменил метод getRectInfo(), сделав его методом print(), который, кроме
    прочего, выводит площадь фигуры.

--- pochernin.vladislav/A1/main.cpp
+++ pochernin.vladislav/A1/main.cpp
@@ -5,33 +5,27 @@
 #include "rectangle.hpp"
 #include "circle.hpp"
 
-void showFrameRect(Shape* shape);
+void print(const Shape& shape, std::ostream& out);
 
 int main()
 {
   Shape* testRectangle = new Rectangle(50.123, 100.0, {13.10123, 12.20});
   std::cout << "\nRectangle:\n";
-  showFrameRect(testRectangle);
-  std::cout << "Area of Rectangle is: " << testRectangle->getArea() << "\n";
+  print(*testRectangle, std::cout);
   point_t posPoint = {111.0, 222.0};
   testRectangle->move(posPoint);
-  std::cout << "Now pos of Rectangle is (" << testRectangle->getFrameRect().pos.x
-            << "; " << testRectangle->getFrameRect().pos.y << ")\n";
+  print(*testRectangle, std::cout);
   testRectangle->move(9.0, 8.0);
-  std::cout << "Now pos of Rectangle is (" << testRectangle->getFrameRect().pos.x
-            << "; " << testRectangle->getFrameRect().pos.y << ")\n";
+  print(*testRectangle, std::cout);
 
   Shape* testCircle = new Circle(13.1235, {0.0, 0.0});
   std::cout << "\nCircle:\n";
-  showFrameRect(testCircle);
-  std::cout << "Area of Circle is: " << testCircle->getArea() << "\n";
+  print(*testCircle, std::cout);
   posPoint = {111.0, 222.0};
   testCircle->move(posPoint);
-  std::cout << "Now pos of Circle is (" << testCircle->getFrameRect().pos.x
-            << "; " << testCircle->getFrameRect().pos.y << ")\n";
+  print(*testCircle, std::cout);
   testCircle->move(19.0, 38.0);
-  std::cout << "Now pos of Circle is (" << testCircle->getFrameRect().pos.x
-            << "; " << testCircle->getFrameRect().pos.y << ")\n";
+  print(*testCircle, std::cout);
 
   delete testRectangle;
   delete testCircle;
@@ -39,12 +33,13 @@ int main()
   return 0;
 }
 
-void showFrameRect(Shape* shape)
+void print(const Shape& shape, std::ostream& out)
 {
-  std::cout << "-------------------------------------------\n";
-  std::cout << "width: " << shape->getFrameRect().width << "\n";
-  std::cout << "height: " << shape->getFrameRect().height << "\n";
-  std::cout << "pos: " << "(" << shape->getFrameRect().pos.x
-            << "; " << shape->getFrameRect().pos.y << ")\n";
-  std::cout << "-------------------------------------------\n";
+  out << "-------------------------------------------\n";
+  out << "width: " << shape.getFrameRect().width << "\n";
+  out << "height: " << shape.getFrameRect().height << "\n";
+  out << "pos: " << "(" << shape.getFrameRect().pos.x
+      << "; " << shape.getFrameRect().pos.y << ")\n";
+  out << "area: " << shape.getArea() << "\n";
+  out << "-------------------------------------------\n";
 }

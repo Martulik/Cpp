d7bcff23f902ea5edacda40fd510e94c9fbdd640
ekaterina-borisova <kate_boriso2002@mail.ru>
2021-02-28 13:18:51 +0400

    Cut back main

--- borisova.ekaterina/A1/main.cpp
+++ borisova.ekaterina/A1/main.cpp
@@ -5,6 +5,14 @@
 
 int main()
 {
+  void testRectangle();
+  void testCircle();
+  testRectangle();
+  testCircle();
+}
+
+void testRectangle()
+{
   double x = 6.2;
   double y = 1.55;
   point_t point1{x, y};
@@ -25,8 +33,13 @@ int main()
   rectangle->move(point2);
   std::cout << "Rectangle moved on: " << x << ", " << y << '\n';
 
-  x = 7.99;
-  y = 16.83;
+  delete rectangle;
+}
+
+void testCircle()
+{
+  double x = 7.99;
+  double y = 16.83;
 
   double radius = 123.98;
   Shape* circle = new Circle(x, y, radius);
@@ -40,7 +53,5 @@ int main()
   y = 0.555;
   circle->move(x, y);
   std::cout << "Circle moved on: " << x << ", " << y << '\n';
-
-  delete rectangle;
   delete circle;
 }

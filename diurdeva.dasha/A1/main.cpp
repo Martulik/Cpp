ad1d9ff9765539bf2f771c78cd5e921b8c999e81
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-02-28 15:16:00 +0300

    add new function in main

--- diurdeva.dasha/A1/main.cpp
+++ diurdeva.dasha/A1/main.cpp
@@ -10,12 +10,21 @@ void getInfoFrameRect(Shape *figura)
             << std::endl;
 }
 
-void moveShape(Shape *figura, const double x, const double y)
+void getShapeArea(Shape *figura)
 {
+  std::cout << "Area is: " << figura->getArea() << std::endl;
+}
+
+void moveShape(Shape *figura)
+{
+  double x = 3.2;
+  double y = 5.4;
   std::cout << "\nMove the center  to a point: (" << x << ";" << y << ")" << std::endl;
   figura->move({x, y});
   std::cout << "New pos center: x = " << figura->getFrameRect().pos.x << ", y = " << figura->getFrameRect().pos.y
             << std::endl;
+  x = 4.1;
+  y = 2.2;
   std::cout << "Move the center of the rectangle along the x-axis by " << x << " along the y-axis by " << y
             << std::endl;
   figura->move(x, y);
@@ -28,20 +37,16 @@ int main()
   std::cout << "Rectangle: " << std::endl;
   point_t pointCenterRectangle = {3.5, 4.0};
   Shape *rectangle = new Rectangle(8.0, 5.0, pointCenterRectangle);
-  std::cout << "Area is: " << rectangle->getArea() << std::endl;
+  getShapeArea(rectangle);
   getInfoFrameRect(rectangle);
-  double x = 3.2;
-  double y = 5.4;
-  moveShape(rectangle, x, y);
+  moveShape(rectangle);
   delete rectangle;
 
   std::cout << "\nCircle: " << std::endl;
   point_t pointCenterCircle = {1, 4};
   Shape *circle = new Circle(8.0, pointCenterCircle);
+  getShapeArea(circle);
   getInfoFrameRect(circle);
-  std::cout << "Area is: " << circle->getArea() << std::endl;
-  x = 2.7;
-  y = 1.5;
   moveShape(circle, x, y);
   delete circle;
 }

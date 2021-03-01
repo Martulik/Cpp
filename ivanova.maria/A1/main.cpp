ef1d564b5000b692e71eea84912538b2f9804dee
Ivanova Maria <ivanova5.mv@edu.spbstu.ru>
2021-03-01 23:30:24 +0300

    ADDED VOID INFO()
    added function to show info about shape to not copypaste code

--- ivanova.maria/A1/main.cpp
+++ ivanova.maria/A1/main.cpp
@@ -2,29 +2,28 @@
 #include "rectangle.hpp"
 #include "circle.hpp"
 
+void info(Shape* shape);
+
 int main()
 {
   Rectangle *rectangle = new Rectangle (10, 12, {3.0, 18.0});
   Shape *shape0 = rectangle;
-  std::cout << *shape0;
-  std::cout << "\narea is " << shape0->getArea() << std::endl;
-  shape0->move(12, 7);
-  shape0->move({4, 8});
-  std::cout << "\nframe rectangle: " << std::endl << "height: " << shape0->getFrameRect().height << std::endl
-  << "width: " << shape0->getFrameRect().width << std::endl << "center: {" << shape0->getFrameRect().pos.x
-  << ";" << shape0->getFrameRect().pos.y << "}\n" << std::endl;
-
+  info(shape0);
   Circle *circle  = new Circle({1, 4}, 12);
   Shape *shape1 = circle;
-  std::cout << *shape1;
-  std::cout << "\narea is " << shape1->getArea() << std::endl;
-  shape1->move(12, 7);
-  shape1->move({4, 8});
-  std::cout << "\nframe rectangle: " << std::endl << "height: " << shape1->getFrameRect().height << std::endl
-  << "width: " << shape1->getFrameRect().width << std::endl << "center: {" << shape1->getFrameRect().pos.x
-  << ";" << shape1->getFrameRect().pos.y << "}" << std::endl;
-
+  info(shape1);
   delete rectangle;
   delete circle;
   return 0;
+}
+
+void info(Shape *shape)
+{
+  std::cout << shape;
+  std::cout << "\narea is " << shape->getArea() << std::endl;
+  shape->move(12, 7);
+  shape->move({4, 8});
+  std::cout << "\nframe rectangle: " << std::endl << "height: " << shape0->getFrameRect().height << std::endl
+  << "width: " << shape0->getFrameRect().width << std::endl << "center: {" << shape0->getFrameRect().pos.x
+  << ";" << shape0->getFrameRect().pos.y << "}\n" << std::endl;
 }

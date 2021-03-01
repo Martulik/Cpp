bde0d2e378f9f1d433457d2ac15f4ea1f7c8d697
Danil Doroshin <ddddanil5555@gmail.com>
2021-03-01 16:46:36 +0300

    Migrate to Boost.Test

--- doroshin.danil/A3/test-main.cpp
+++ doroshin.danil/A3/test-main.cpp
@@ -1,7 +1,9 @@
-
 #include <cassert>
 #include <iostream>
 #include "composite-shape.hpp"
+
+#define BOOST_TEST_MODULE test
+#include <boost/test/included/unit_test.hpp>
 
 namespace d = doroshin;
 
@@ -12,7 +14,7 @@ void test_move_rel(d::Shape& s, d::point_t vec)
   };
   d::rectangle_t orig_frame = s.getFrameRect();
   s.move_rel(vec);
-  assert(eq(orig_frame, s.getFrameRect()));
+  BOOST_CHECK(eq(orig_frame, s.getFrameRect()));
 }
 
 void test_move_abs(d::Shape& s, d::point_t vec)
@@ -22,7 +24,7 @@ void test_move_abs(d::Shape& s, d::point_t vec)
   };
   d::rectangle_t orig_frame = s.getFrameRect();
   s.move_rel(vec);
-  assert(eq(orig_frame, s.getFrameRect()));
+  BOOST_CHECK(eq(orig_frame, s.getFrameRect()));
 }
 
 void test_scale(d::Shape& s, double coeff)
@@ -31,10 +33,10 @@ void test_scale(d::Shape& s, double coeff)
   double predicted = coeff * coeff * orig;
   s.scale(coeff);
   double n_area = s.getArea();
-  assert(predicted == n_area);
+  BOOST_CHECK(predicted == n_area);
 }
 
-void test_move_circle()
+BOOST_AUTO_TEST_CASE(move_circle)
 {
   d::Circle c{{1, 2}, 3};
 
@@ -43,7 +45,7 @@ void test_move_circle()
   test_move_abs(c, {65, -33});
 }
 
-void test_move_rectangle()
+BOOST_AUTO_TEST_CASE(move_rectangle)
 {
   d::Rectangle r(d::rectangle_t {3, 4, {1, 2}});
 
@@ -52,7 +54,7 @@ void test_move_rectangle()
   test_move_abs(r, {65, -33});
 }
 
-void test_move_composite()
+BOOST_AUTO_TEST_CASE(move_composite)
 {
   d::CompositeShape s {d::Rectangle(d::rectangle_t {2, 3, {-1, 0}}), d::Circle{{1, 1}, 2}};
 
@@ -61,34 +63,23 @@ void test_move_composite()
   test_move_abs(s, {65, -33});
 }
 
-void test_scale_circle()
+BOOST_AUTO_TEST_CASE(scale_circle)
 {
   d::Circle c{{0, 0}, 2};
   test_scale(c, 3);
   test_scale(c, 0.5);
 }
 
-void test_scale_rectangle()
+BOOST_AUTO_TEST_CASE(scale_rectangle)
 {
   d::Rectangle r(d::rectangle_t {3, 4, {1, 2}});
   test_scale(r, 5);
   test_scale(r, 0.3);
 }
 
-void test_scale_composite()
+BOOST_AUTO_TEST_CASE(scale_composite)
 {
   d::CompositeShape s{d::Rectangle(d::rectangle_t {2, 3, {-1, 0}}), d::Circle{{1, 1}, 2}};
   test_scale(s, 4.5);
   test_scale(s, 0.1);
-}
-
-int main()
-{
-  test_move_circle();
-  test_move_rectangle();
-  test_move_composite();
-  test_scale_circle();
-  test_scale_rectangle();
-  test_scale_composite();
-  return 0;
 }

6922e9950c7b06e1f933ad58b9c6f259959eb096
Ivanova Maria <ivanova5.mv@edu.spbstu.ru>
2021-03-01 23:51:46 +0300

    CG

--- ivanova.maria/A1/shape.hpp
+++ ivanova.maria/A1/shape.hpp
@@ -6,11 +6,11 @@
 class Shape
 {
 public:
-  virtual ~Shape() = default;
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
   virtual void move(const point_t &newPos) = 0;
   virtual void move(double dx, double dy) = 0;
+  virtual ~Shape() = default;
 protected:
   friend std::ostream &operator << (std::ostream& out, Shape& shape)
   {

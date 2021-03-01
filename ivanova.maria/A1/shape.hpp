6cddc314912238bab89e40788bf464e3cda59c72
Ivanova Maria <ivanova5.mv@edu.spbstu.ru>
2021-03-02 01:17:51 +0300

    namespace checked

--- ivanova.maria/A1/shape.hpp
+++ ivanova.maria/A1/shape.hpp
@@ -7,7 +7,7 @@ namespace ivanova {
   public:
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(const i::point_t &newPos) = 0;
+    virtual void move(const point_t &newPos) = 0;
     virtual void move(double dx, double dy) = 0;
     virtual std::ostream &fout(std::ostream &out) const = 0;
     virtual ~Shape() = default;

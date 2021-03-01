eaa589ec85881096e02b331ccd7f9f5c856777be
Danil Doroshin <ddddanil5555@gmail.com>
2021-03-01 18:27:24 +0300

    Fix Rectangle constructor

--- doroshin.danil/A1/rectangle.cpp
+++ doroshin.danil/A1/rectangle.cpp
@@ -3,6 +3,21 @@
 #include <cmath>
 #include <cassert>
 
+rectangle_t from_two_points(point_t a, point_t b)
+{
+  double min_x = std::min(a.x, b.x);
+  double max_x = std::max(a.x, b.x);
+  double min_y = std::min(a.y, b.y);
+  double max_y = std::max(a.y, b.y);
+
+  double x = (max_x + min_x) / 2;
+  double y = (max_y + min_y) / 2;
+  double w = max_x - min_x;
+  double h = max_y - min_y;
+
+  return rectangle_t{w, h, {x, y}};
+}
+
 Rectangle::Rectangle(rectangle_t rect):
   rect(rect)
 {
@@ -18,18 +33,8 @@ Rectangle::Rectangle(double w, double h, point_t pos):
 }
 
 Rectangle::Rectangle(point_t a, point_t b):
-  rect{0, 0, {0, 0}}
-{
-  double min_x = std::min(a.x, b.x);
-  double max_x = std::max(a.x, b.x);
-  double min_y = std::min(a.y, b.y);
-  double max_y = std::max(a.y, b.y);
-
-  rect.pos.x = (max_x + min_x) / 2;
-  rect.pos.y = (max_y + min_y) / 2;
-  rect.width = max_x - min_x;
-  rect.width = max_y - min_y;
-}
+  rect(from_two_points(a, b))
+{}
 
 double Rectangle::getArea() const
 {

8cafa4e1f736786cf658ea8b7d4329534d8f2886
Islam <murzakanovislam21@gmail.com>
2021-03-02 16:26:23 +0300

    Set destructor in first place

--- murzakanov.islam/A1/shape.hpp
+++ murzakanov.islam/A1/shape.hpp
@@ -7,12 +7,12 @@
 class Shape
 {
 public:
+  virtual ~Shape() = default;
   virtual std::string getName() const = 0;
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
   virtual void move(const point_t& point) = 0;
   virtual void move(double dx, double dy) = 0;
-  virtual ~Shape() = default;
 };
 
 #endif

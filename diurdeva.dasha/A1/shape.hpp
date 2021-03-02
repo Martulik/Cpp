2667869df5d7c4880c3683c1c6f6e8a2ada12889
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-03-02 17:47:24 +0300

    Coding registration in shape.hpp

--- diurdeva.dasha/A1/shape.hpp
+++ diurdeva.dasha/A1/shape.hpp
@@ -9,7 +9,7 @@ public:
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
   virtual void move(const point_t& pos) = 0;
-  virtual void move(double axisX, double axisY) = 0;
+  virtual void move(double dX, double dY) = 0;
 };
 
 #endif

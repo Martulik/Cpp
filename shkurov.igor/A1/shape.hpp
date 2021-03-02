2e440a4c24d94197dc3f5923b3bbf032c6a23560
Shkurov Igor <lilthewil@gmail.com>
2021-03-02 16:06:46 +0300

    removed const args from func declaration

--- shkurov.igor/A1/shape.hpp
+++ shkurov.igor/A1/shape.hpp
@@ -10,7 +10,7 @@ class Shape {
         virtual double getArea() const  = 0;
         virtual rectangle_t getFrameRect() const = 0;
         virtual void move(const point_t& new_pos) = 0;
-        virtual void move(const double x, const double y) = 0;
+        virtual void move(double x, double y) = 0;
 };
 
 #endif

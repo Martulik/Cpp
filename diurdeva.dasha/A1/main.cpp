4e0bf6097501a7be69f84794e65df9f1bec2e734
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-03-02 15:32:53 +0300

    Chenched main

--- diurdeva.dasha/A1/main.cpp
+++ diurdeva.dasha/A1/main.cpp
@@ -7,42 +7,30 @@ void printInfoFrameRect(const char *nameShape, const Shape *figura)
   std::cout << nameShape << " - Bounding rectangle: width = " << figura->getFrameRect().width << ", height = "
             << figura->getFrameRect().height
             << ", center: x = " << figura->getFrameRect().pos.x << ", y = " << figura->getFrameRect().pos.y
-            << "\nArea is: " << figura->getArea() << std::endl;
+            << "\nArea is: " << figura->getArea();
 }
 
-void checkPosition(const Shape *figura, const point_t checkCenter)
+void testShape(Shape *figura, const char *nameShape, const point_t newCenter)
 {
-  bool check = ((figura->getFrameRect().pos.x == checkCenter.x) && (figura->getFrameRect().pos.y == checkCenter.y));
-  std::cout << "The move has passed: " << (check ? "right" : "wrong") << std::endl;
-}
-
-void moveShape(Shape *figura, const point_t newCenter, bool flag)
-{
-  if (flag) {
-    figura->move(newCenter);
-  } else {
-    figura->move(newCenter.x, newCenter.y);
-  }
+  printInfoFrameRect(nameShape, figura);
+  figura->move(newCenter);
+  bool check = ((figura->getFrameRect().pos.x == newCenter.x) && (figura->getFrameRect().pos.y == newCenter.y));
+  std::cout << "\nThe move has passed: " << (check ? "right" : "wrong") << "\n";
 }
 
 int main()
 {
   const point_t pointCenter = {3.5, 4.0};
   const point_t newCenter = {3.3, 4.6};
-  const point_t checkCenterForTranslationAlongTheAxis = {6.8, 8.6};
   const double w = 8.0;
   const double h = 5.0;
   const double radius = 3.2;
 
   Shape *rectangle = new Rectangle(w, h, pointCenter);
-  printInfoFrameRect("Rectangle", rectangle);
-  moveShape(rectangle, newCenter, true);
-  checkPosition(rectangle, newCenter);
+  testShape(rectangle, "Rectangle", newCenter);
   delete rectangle;
 
   Shape *circle = new Circle(radius, pointCenter);
-  printInfoFrameRect("Circle", circle);
-  moveShape(circle, newCenter, false);
-  checkPosition(circle, checkCenterForTranslationAlongTheAxis);
+  testShape(circle, "Circle", newCenter);
   delete circle;
 }

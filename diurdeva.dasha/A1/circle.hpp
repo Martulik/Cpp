8807d4651fed601e211768259eccdba9a7e763a6
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-03-02 16:00:03 +0300

    Add const for point_t in heder

--- diurdeva.dasha/A1/circle.hpp
+++ diurdeva.dasha/A1/circle.hpp
@@ -5,7 +5,7 @@
 
 class Circle: public Shape {
 public:
-  Circle(double rad, point_t &pos);
+  Circle(double rad, const point_t &pos);
   double getArea() const override;
   rectangle_t getFrameRect() const override;
   void move(const point_t &newCenter) override;
--- diurdeva.dasha/A1/rectangle.hpp
+++ diurdeva.dasha/A1/rectangle.hpp
@@ -5,7 +5,7 @@
 
 class Rectangle: public Shape {
 public:
-  Rectangle(double width, double height, point_t& pos);
+  Rectangle(double width, double height, const point_t& pos);
   double getArea() const override;
   rectangle_t getFrameRect() const override;
   void move(const point_t& newCenter) override;

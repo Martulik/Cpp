2b0c75d8d781701ba6e990134777dc6d62932d9c
Islam <murzakanovislam21@gmail.com>
2021-03-03 17:23:44 +0300

    Fix main.cpp

--- murzakanov.islam/A1/main.cpp
+++ murzakanov.islam/A1/main.cpp
@@ -2,32 +2,37 @@
 #include "circle.hpp"
 #include "rectangle.hpp"
 
-void print(std::ostream& out, const Shape& shp)
+void print(std::ostream& out, const Shape* shp)
 {
-  out << "Shape:\n" <<"::Position: " << "(" << shp.getFrameRect().pos.x
-      << ", " << shp.getFrameRect().pos.y << ")";
-  out << "\nSpecific: " << shp.getName() << "\n\n";
+  out << "Shape:\n" <<"::Position: " << "(" << shp->getFrameRect().pos.x
+      << ", " << shp->getFrameRect().pos.y << ")";
+  out << "\nSpecific: " << shp->getName() << "\n\n";
 }
 
+void printAfterMove(Shape* shp, const point_t& point, bool isRelative)
+{
+  if (isRelative)
+  {
+    shp->move(point.x, point.y);
+  }
+  else
+  {
+    shp->move(point);
+  }
+  std::cout << "Figure's info after move()\n";
+  print(std::cout, shp);
+}
 
 int main()
 {
   Shape* polyRectangle = new Rectangle(1, 2, { 3, 5 });
   Shape* polyCircle = new Circle(5.25, { 13, -12 });
-  print(std::cout, *polyRectangle);
-  print(std::cout, *polyCircle);
+  print(std::cout, polyRectangle);
+  print(std::cout, polyCircle);
 
-  point_t newPoint({ 15, 23 });
-  polyRectangle->move(newPoint);
-  std::cout << "Rectangle's info after move()\n";
-  print(std::cout, *polyRectangle);
-
-  double dx = -3.5;
-  double dy = -1.8;
-  polyCircle->move(dx, dy);
-  std::cout<< "Circle's info after move()\n";
-  print(std::cout, *polyCircle);
-
+  point_t point({ 15, 23 });
+  printAfterMove(polyRectangle, point, false);
+  printAfterMove(polyCircle, point, true);
   delete polyRectangle;
   delete polyCircle;
   return 0;

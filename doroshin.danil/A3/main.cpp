01ef7f2e0a750b503fe13b0f6d803859e49e8736
Danil Doroshin <ddddanil5555@gmail.com>
2021-03-06 18:23:52 +0300

    New main program

--- doroshin.danil/A3/main.cpp
+++ doroshin.danil/A3/main.cpp
@@ -10,50 +10,36 @@
 namespace dan = doroshin;
 
 /*
-  This program echos the parameters of an inputted shape
-  Usage:
-    argument - type of the shape to use
-      ./lab circle
-      ./lab rectangle
-      ./lab composite
-    stdin - shape parameters
-    Circle:
-      "x y r" - one line with three floating point values
-    Rectangle:
-      "x y w h" - one line with four floating point values
-    Composite:
-      "type (parameters)"
-        type - one of circle/rectangle
-        parameters - same as above
-        Consumes an arbitrary amount of lines. Finishes on EOF.
+  This program makes a composite shape, adding its elements one by one
   Examples:
   >>> 1
-    $ ./lab circle
-    3 4 5^J
-    Circle:
-    Frame: (3, 4) w: 10 h: 10
-    Area: 78.5398
+    $ ./lab
+    circle 1 2 3^D^D
+    Result:
+    Frame: (1, 2) w: 6 h: 6
+    Area: 28.2743
   >>> 2
-    $ ./lab rectangle
-    7 1 2 5^J
-    Rectangle:
+    $ ./lab
+    rectangle 7 1 2 5^D^D
+    Result:
     Frame: (7, 1) w: 2 h: 5
     Area: 10
   >>> 3
-    $ ./lab composite
-    circle 1 2 3^J
-    Circle:
-    Frame: (1, 2) w: 6 h: 6
-    Area: 28.2743
-    rectangle 3 4 5 6^J
-    Rectangle:
-    Frame: (3, 4) w: 5 h: 6
-    Area: 30
-    ^D
-    Composite:
+    $ ./lab
+    circle 3 4 5^J
+    rectangle 3 4 5 6^D^D
+    Result:
     Frame: (1.75, 3) w: 7.5 h: 8
     Area: 58.2743
 */
+
+void print_shape(std::ostream& out, const dan::Shape& s)
+{
+  dan::rectangle_t frame = s.getFrameRect();
+  out << "Frame: (" << frame.pos.x << ", " << frame.pos.y
+      << ") w: " << frame.width << " h: " << frame.height << std::endl
+      << "Area: " << s.getArea() << std::endl;
+}
 
 dan::Circle input_circle(std::istream& in)
 {
@@ -69,122 +55,48 @@ dan::Rectangle input_rectangle(std::istream& in)
   return dan::Rectangle{w, h, {x_pos, y_pos}};
 }
 
-void print(std::ostream& out, const dan::Shape& s)
+std::unique_ptr<dan::Shape> input_shape(std::istream& in)
 {
-  dan::rectangle_t frame = s.getFrameRect();
-  out << "Frame: (" << frame.pos.x << ", " << frame.pos.y
-      << ") w: " << frame.width << " h: " << frame.height << std::endl
-      << "Area: " << s.getArea() << std::endl;
-}
+  std::string line_;
+  std::getline(in, line_);
+  std::istringstream line(line_);
 
-void print(std::ostream& out, const dan::Circle& c)
-{
-  out << "Circle:" << std::endl;
-  print(out, static_cast<const dan::Shape&>(c));
-}
+  std::string type;
+  line >> type;
 
-void print(std::ostream& out, const dan::Rectangle& r)
-{
-  out << "Rectangle:" << std::endl;
-  print(out, static_cast<const dan::Shape&>(r));
-}
-
-void print(std::ostream& out, const dan::CompositeShape& s)
-{
-  out << "Composite:" << std::endl;
-  print(out, static_cast<const dan::Shape&>(s));
-}
-
-int echo_circle()
-{
-  std::string line;
-  std::getline(std::cin, line);
-  std::istringstream in(line);
-
-  dan::Circle c = input_circle(in);
-  if(in.fail()) {
-    std::cerr << "Incorrect parameters" << std::endl;
-    return 1;
+  if(type == "circle") {
+    dan::Circle c = input_circle(line);
+    if(in.fail()) {
+      std::cerr << "Incorrect parameters" << std::endl;
+      return nullptr;
+    }
+    return dan::make_shape(c);
   }
-
-  print(std::cout, c);
-  return 0;
-}
-
-int echo_rectangle()
-{
-  std::string line;
-  std::getline(std::cin, line);
-  std::istringstream in(line);
-
-  dan::Rectangle r = input_rectangle(in);
-  if(in.fail()) {
-    std::cerr << "Incorrect parameters" << std::endl;
-    return 1;
+  else if(type == "rectangle") {
+    dan::Rectangle r = input_rectangle(line);
+    if(in.fail()) {
+      std::cerr << "Incorrect parameters" << std::endl;
+      return nullptr;
+    }
+    return dan::make_shape(r);
   }
-
-  print(std::cout, r);
-  return 0;
+  else {
+    std::cerr << "Unknown shape" << std::endl;
+    return nullptr;
+  }
 }
 
-int echo_composite()
+int main()
 {
-  dan::CompositeShape shape;
+  dan::CompositeShape aggregate;
   while(!std::cin.eof()) {
-    std::string line;
-    std::getline(std::cin, line);
-    if(line.empty())
-      break;
-    std::istringstream in(line);
-
-    std::string type;
-    in >> type;
-
-    if(type == "circle") {
-      dan::Circle c = input_circle(in);
-      if(in.fail()) {
-        std::cerr << "Incorrect parameters" << std::endl;
-        return 1;
-      }
-      print(std::cout, c);
-      shape.add(dan::make_shape(dan::Circle(c)));
-    }
-    else if(type == "rectangle") {
-      dan::Rectangle r = input_rectangle(in);
-      if(in.fail()) {
-        std::cerr << "Incorrect parameters" << std::endl;
-        return 1;
-      }
-      print(std::cout, r);
-      shape.add(dan::make_shape(dan::Rectangle(r)));
-    }
-    else if(type.empty())
-      break;
-    else {
-      std::cerr << "Unknown shape" << std::endl;
+    std::unique_ptr<dan::Shape> next = input_shape(std::cin);
+    std::skipws(std::cin);
+    if(!next)
       return 1;
-    }
+    aggregate.add(std::move(next));
   }
-  print(std::cout, shape);
-  return 0;
-}
-
-int main(int argc, char* argv[])
-{
-  if(argc > 1) {
-    if(!strcmp(argv[1], "rectangle")) {
-      return echo_rectangle();
-    }
-    else if(!strcmp(argv[1], "circle")) {
-      return echo_circle();
-    }
-    else if(!strcmp(argv[1], "composite")) {
-      return echo_composite();
-    }
-    else
-      std::cout << "Unknown shape" << std::endl;
-  }
-  else
-    std::cout << "No arguments given" << std::endl;
+  std::cout << "\nResult:\n";
+  print_shape(std::cout, aggregate);
   return 0;
 }

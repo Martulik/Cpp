8d2f51fab31bcc5c126ad94dfb9a2db773deb6a8
ilya-pyatizbyantsev <ilya_2.0@mail.ru>
2021-03-09 15:17:05 +0300

    Init commit Add all files from A1

---
+++ pyatizbyantsev.ilya/A3/circle.hpp
@@ -0,0 +1,23 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "shape.hpp"
+
+namespace pyatizbyantsev
+{
+  class Circle: public Shape
+  {
+  public:
+    Circle(double valueRadius, const point_t& valuePos);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(const point_t& newPos) override;
+    void move(double abscissa, double ordinate) override;
+
+  private:
+    double radius_;
+    point_t pos_;
+  };
+}
+
+#endif

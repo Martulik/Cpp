4ee6f7a3b5cd7b648ae5e190d293616b58a95d9c
DanilArteev <arteev.dd@edu.spbstu.ru>
2021-03-23 16:33:36 +0300

    Reworked the output

--- arteev.danil/A1/main.cpp
+++ arteev.danil/A1/main.cpp
@@ -3,59 +3,57 @@
 #include "circle.hpp"
 #include "rectangle.hpp"
 
-void printCircleParameters(const Shape* figureCircle)
+void printFrameRect(const Shape* s)
 {
-  std::cout << "Circle radius: " << figureCircle->getFrameRect().height / 2 << "\n";
+  rectangle_t reserve = s->getFrameRect();
+  std::cout << "Coordinates: " << "X = " << reserve.pos.x
+  << "; Y = " << reserve.pos.y << '\n';
+  std::cout << "\nHeight and width: ";
+  std::cout << "h = " << reserve.height <<
+  "; w = " << reserve.width << '\n';
 }
 
-void printCircleCoordinates(const Shape* figureCircle)
+void printArea(const Shape *s)
 {
-  std::cout << "Coordinates of the center of the circle: \n"
-    << "X = " << figureCircle->getFrameRect().pos.x << "\t"
-    << "Y = " << figureCircle->getFrameRect().pos.y << "\n";
-}
-
-void printRectangleParameters(const Shape * figureRectangle)
-{
-  std::cout << "\nRectangle height: " << figureRectangle->getFrameRect().height << "\n";
-  std::cout << "Rectangle width: " << figureRectangle->getFrameRect().width << "\n";
-  std::cout << "Rectangle area: " << figureRectangle->getArea() << "\n\n";
-}
-
-void printRectangleCoordinates(const Shape* figureRectangle)
-{
-  std::cout << "Coordinates of the center of the rectangle: \n"
-    << "X = " << figureRectangle->getFrameRect().pos.x << "\t"
-    << "Y = " << figureRectangle->getFrameRect().pos.y << "\n";
+  std::cout << "\nArea: " << s->getArea() << '\n';
 }
 
 int main()
 {
   Shape* figureCircle = new Circle({ 2, 2 }, 3);
-  printCircleParameters(figureCircle);
-  printCircleCoordinates(figureCircle);
+  std::cout << "Parameters circle before move: " << '\n';
+  printFrameRect(figureCircle);
 
   point_t pos = { 12, 7 };
   figureCircle->move(pos);
-  printCircleCoordinates(figureCircle);
+  std::cout << "\nParameters circle after move: " << '\n';
+  printFrameRect(figureCircle);
+
 
   figureCircle->move(10, 12);
-  printCircleCoordinates(figureCircle);
+  std::cout << "\nParameters circle after another move :" << '\n';
+  printFrameRect(figureCircle);
+
+  printArea(figureCircle);
 
   delete figureCircle;
 
   pos = { 5, 8 };
 
   Shape* figureRectangle = new Rectangle(10, 12, { 5, 5 });
-  printRectangleParameters(figureRectangle);
-  printRectangleCoordinates(figureRectangle);
+  std::cout << "\nParameters rectangle before move: " << '\n';
+  printFrameRect(figureRectangle);
+
 
   figureRectangle->move(10, 65);
-  printRectangleCoordinates(figureRectangle);
+  std::cout << "\nParameters rectangle after move: " << '\n';
+  printFrameRect(figureRectangle);
 
   figureRectangle->move(pos);
-  printRectangleCoordinates(figureRectangle);
+  std::cout << "\nParameters rectangle after another move :" << '\n';
+  printFrameRect(figureRectangle);
 
-  delete figureRectangle;
+  printArea(figureRectangle);
+
+delete figureRectangle;
 }
-

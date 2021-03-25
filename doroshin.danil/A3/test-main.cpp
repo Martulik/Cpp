78a783e02f54df2af15bf11825716fc80e05a70c
Danil Doroshin <ddddanil5555@gmail.com>
2021-03-25 11:52:06 +0300

    Remove incorrect shape checks

--- doroshin.danil/A3/test-main.cpp
+++ doroshin.danil/A3/test-main.cpp
@@ -5,6 +5,7 @@
 #include "composite-shape.hpp"
 
 namespace dan = doroshin;
+using namespace dan::literals;
 
 void equal_size(const dan::rectangle_t& lhs, const dan::rectangle_t& rhs)
 {
@@ -26,7 +27,7 @@ void test_move_abs(dan::Shape& s, dan::point_t point)
   equal_size(orig_frame, s.getFrameRect());
 }
 
-void test_scale(dan::Shape& s, double coeff)
+void test_scale(dan::Shape& s, dan::udouble_t coeff)
 {
   const double tolerance = std::numeric_limits<double>::epsilon();
   double orig = s.getArea();
@@ -38,7 +39,7 @@ void test_scale(dan::Shape& s, double coeff)
 
 BOOST_AUTO_TEST_CASE(move_circle)
 {
-  dan::Circle c{{1, 2}, 3};
+  dan::Circle c{{1, 2}, 3_ud};
 
   test_move_rel(c, {1, 2});
   test_move_rel(c, {-5, 7});
@@ -47,7 +48,7 @@ BOOST_AUTO_TEST_CASE(move_circle)
 
 BOOST_AUTO_TEST_CASE(move_rectangle)
 {
-  dan::Rectangle r{3, 4, {1, 2}};
+  dan::Rectangle r{3_ud, 4_ud, {1, 2}};
 
   test_move_rel(r, {1, 2});
   test_move_rel(r, {-5, 7});
@@ -57,8 +58,8 @@ BOOST_AUTO_TEST_CASE(move_rectangle)
 BOOST_AUTO_TEST_CASE(move_composite)
 {
   dan::CompositeShape s(
-      dan::Rectangle{2, 3, dan::point_t{-1, 0}},
-      dan::Circle{dan::point_t{1, 1}, 2});
+      dan::Rectangle{2_ud, 3_ud, dan::point_t{-1, 0}},
+      dan::Circle{dan::point_t{1, 1}, 2_ud});
 
   test_move_rel(s, {1, 2});
   test_move_rel(s, {-5, 7});
@@ -67,46 +68,32 @@ BOOST_AUTO_TEST_CASE(move_composite)
 
 BOOST_AUTO_TEST_CASE(scale_circle)
 {
-  dan::Circle c{{0, 0}, 2};
-  test_scale(c, 3);
-  test_scale(c, 0.5);
+  dan::Circle c{{0, 0}, 2_ud};
+  test_scale(c, 3_ud);
+  test_scale(c, 0.5_ud);
 }
 
 BOOST_AUTO_TEST_CASE(scale_rectangle)
 {
-  dan::Rectangle r{3, 4, {1, 2}};
-  test_scale(r, 5);
-  test_scale(r, 0.3);
+  dan::Rectangle r{3_ud, 4_ud, {1, 2}};
+  test_scale(r, 5_ud);
+  test_scale(r, 0.3_ud);
 }
 
 BOOST_AUTO_TEST_CASE(scale_composite)
 {
   dan::CompositeShape s(
-      dan::Rectangle{2, 3, dan::point_t{-1, 0}},
-      dan::Circle{dan::point_t{1, 1}, 2});
-  test_scale(s, 4.5);
-  test_scale(s, 0.1);
-}
-
-BOOST_AUTO_TEST_CASE(incorrect_circle)
-{
-  BOOST_CHECK_NO_THROW((dan::Circle{{0, 0}, 1}));
-  BOOST_CHECK_THROW((dan::Circle{{0, 0}, -1}), dan::IncorrectShape);
-}
-
-BOOST_AUTO_TEST_CASE(incorrect_rectangle)
-{
-  BOOST_CHECK_NO_THROW((dan::Rectangle{1, 1, {0, 0}}));
-  BOOST_CHECK_THROW((dan::Rectangle{-1, 1, {0, 0}}), dan::IncorrectShape);
-  BOOST_CHECK_THROW((dan::Rectangle{1, -1, {0, 0}}), dan::IncorrectShape);
-  BOOST_CHECK_THROW((dan::Rectangle{-1, -1, {0, 0}}), dan::IncorrectShape);
+      dan::Rectangle{2_ud, 3_ud, dan::point_t{-1, 0}},
+      dan::Circle{dan::point_t{1, 1}, 2_ud});
+  test_scale(s, 4.5_ud);
+  test_scale(s, 0.1_ud);
 }
 
 BOOST_AUTO_TEST_CASE(copy_composite)
 {
   dan::CompositeShape src(
-      dan::Rectangle{2, 3, dan::point_t{-1, 0}},
-      dan::Circle{dan::point_t{1, 1}, 2});
+      dan::Rectangle{2_ud, 3_ud, dan::point_t{-1, 0}},
+      dan::Circle{dan::point_t{1, 1}, 2_ud});
 
   dan::CompositeShape cpy(src);
 

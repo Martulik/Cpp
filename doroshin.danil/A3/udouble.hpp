197ec0b800f820752559ebb512d2748dd15ee706
Danil Doroshin <ddddanil5555@gmail.com>
2021-03-25 12:29:35 +0300

    Make udouble_t funcitons constexpr

--- doroshin.danil/A3/udouble.hpp
+++ doroshin.danil/A3/udouble.hpp
@@ -1,24 +1,50 @@
 #ifndef UDOUBLE_HPP
 #define UDOUBLE_HPP
 
+#include <cmath>
+#include <stdexcept>
+
 namespace doroshin
 {
+  // constexpr functions are automatically inline
+  // thus they cannot be shared between translation units
+  // and have to be declared in the header
   class udouble_t
   {
     double value_;
   public:
-    explicit udouble_t() noexcept;
-    explicit udouble_t(double) noexcept;
+    constexpr explicit udouble_t() noexcept:
+      value_()
+    {}
 
-    operator double() const noexcept;
+    constexpr explicit udouble_t(const double value) noexcept:
+      value_(std::fabs(value))
+    {}
+
+    constexpr operator double() const noexcept
+    {
+      return value_;
+    }
   };
 
-  udouble_t make_abs(double);
+  constexpr udouble_t make_abs(const double value)
+  {
+    if(value < 0)
+      throw std::invalid_argument("Unsigned double value must be non-negative");
+    return udouble_t(value);
+  }
 
   inline namespace literals
   {
-    udouble_t operator""_ud(unsigned long long) noexcept;
-    udouble_t operator""_ud(long double) noexcept;
+    constexpr udouble_t operator""_ud(const unsigned long long value) noexcept
+    {
+      return udouble_t(static_cast< double >(value));
+    }
+
+    constexpr udouble_t operator""_ud(const long double value) noexcept
+    {
+      return udouble_t(static_cast< double >(value));
+    }
   }
 }
 

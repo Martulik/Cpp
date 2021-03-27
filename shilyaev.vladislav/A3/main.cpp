196efb54f080201bff9b27ebad09a879479d9156
Vladislav Shilyaev <schilyaev.vlad@gmail.com>
2021-03-28 01:41:17 +0300

    Rewrite main.cpp

--- shilyaev.vladislav/A3/main.cpp
+++ shilyaev.vladislav/A3/main.cpp
@@ -6,26 +6,33 @@
 void printShapeDescription(const shilyaev::Shape &shape)
 {
   const shilyaev::rectangle_t frameRect = shape.getFrameRect();
-  std::cout << "Area: " << shape.getArea() << ", Width × Height: " << frameRect.width << " × " << frameRect.height
+  std::cout << "Area: " << shape.getArea()
+            << ", Width × Height: " << frameRect.width << " × " << frameRect.height
             << ", Center: " << frameRect.pos.x << ' ' << frameRect.pos.y << '\n';
 }
 
 int main()
 {
+  using CompositeShapePtr = std::unique_ptr< shilyaev::CompositeShape >;
   const shilyaev::point_t startPos{2.0, 2.0};
   const shilyaev::point_t finishPos{-1.0, -1.0};
   const double rectangleWidth = 0.5;
   const double rectangleHeight = 5.0;
   const double radius = 2.0;
   const double scaleFactor = 3.5;
+  const size_t arraySize = 3;
 
-  shilyaev::CompositeShape compositeShape(
-      std::make_unique< shilyaev::Rectangle >(rectangleWidth, rectangleHeight, startPos));
-  compositeShape.pushBack(std::make_unique< shilyaev::Circle >(radius, startPos));
-  printShapeDescription(compositeShape.at(0));
-  printShapeDescription(compositeShape.at(1));
-  printShapeDescription(compositeShape);
-  compositeShape.move(finishPos);
-  compositeShape.scale(scaleFactor);
-  printShapeDescription(compositeShape);
+  shilyaev::ShapeArray shapes = std::make_unique< shilyaev::ShapePtr[] >(arraySize);
+  shapes[0] = std::make_unique< shilyaev::Circle >(radius, startPos);
+  shapes[1] = std::make_unique< shilyaev::Rectangle >(rectangleWidth, rectangleHeight, startPos);
+  CompositeShapePtr compositeShape = std::make_unique< shilyaev::CompositeShape >(shapes[0]->clone());
+  compositeShape->pushBack(shapes[1]->clone());
+  shapes[2] = std::move(compositeShape);
+
+  for (size_t i = 0; i < arraySize; ++i) {
+    printShapeDescription(*shapes[i]);
+    shapes[i]->move(finishPos);
+    shapes[i]->scale(scaleFactor);
+    printShapeDescription(*shapes[i]);
+  }
 }

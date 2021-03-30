fce7cc0b4049dcb79cdddc99b90ebe19bfd61a60
Shkurov Igor <lilthewil@gmail.com>
2021-03-31 01:19:57 +0300

    changed test-main.cpp

--- shkurov.igor/A3/test-main.cpp
+++ shkurov.igor/A3/test-main.cpp
@@ -38,96 +38,96 @@ BOOST_AUTO_TEST_CASE(move_correct_center_translocation)
   BOOST_CHECK_CLOSE(END_POS.y, rect_ptr->getFrameRect().pos.y, EPSILON);
 }
 
-BOOST_AUTO_TEST_CASE(scale_correct_modification)
-{
-  rect_ptr->scale(SCALE);
-  BOOST_CHECK_CLOSE(LENGTH_1 * SCALE, rect_ptr->getFrameRect().height, EPSILON);
-  BOOST_CHECK_CLOSE(LENGTH_2 * SCALE, rect_ptr->getFrameRect().width, EPSILON);
-  BOOST_CHECK_CLOSE(SCALE * SCALE * RECT_AREA, rect_ptr->getArea(), EPSILON);
-}
-
-BOOST_AUTO_TEST_CASE(scale_throw_exception)
-{
-  BOOST_CHECK_THROW(rect_ptr->scale(NEGATIVE_SCALE), std::invalid_argument);
-}
-
-BOOST_AUTO_TEST_SUITE_END();
-
-
-BOOST_AUTO_TEST_SUITE(circle_test)
-
-std::unique_ptr< shkurov::Shape > circle_ptr(new shkurov::Circle(BEGIN_POS, LENGTH_1));
-
-// BOOST_AUTO_TEST_CASE(constructor_throw_exception)
+// BOOST_AUTO_TEST_CASE(scale_correct_modification)
 // {
-//   BOOST_CHECK_THROW(std::unique_ptr< shkurov::Shape >
-//    circle_ptr2(new shkurov::Circle(BEGIN_POS, NEGATIVE_LENGTH)), std::invalid_argument);
+//   rect_ptr->scale(SCALE);
+//   BOOST_CHECK_CLOSE(LENGTH_1 * SCALE, rect_ptr->getFrameRect().height, EPSILON);
+//   BOOST_CHECK_CLOSE(LENGTH_2 * SCALE, rect_ptr->getFrameRect().width, EPSILON);
+//   BOOST_CHECK_CLOSE(SCALE * SCALE * RECT_AREA, rect_ptr->getArea(), EPSILON);
 // }
 
-BOOST_AUTO_TEST_CASE(move_correct_center_translocation)
-{
-  circle_ptr->move(END_POS);
-  BOOST_CHECK_CLOSE(CIRCLE_AREA, circle_ptr->getArea(), EPSILON);
-  BOOST_CHECK_CLOSE(END_POS.x, circle_ptr->getFrameRect().pos.x, EPSILON);
-  BOOST_CHECK_CLOSE(END_POS.y, circle_ptr->getFrameRect().pos.y, EPSILON);
-}
-
-BOOST_AUTO_TEST_CASE(scale_correct_modification)
-{
-  circle_ptr->scale(SCALE);
-  BOOST_CHECK_CLOSE(LENGTH_1 * SCALE, circle_ptr->getFrameRect().height / 2, EPSILON);
-  BOOST_CHECK_CLOSE(SCALE * SCALE * CIRCLE_AREA, circle_ptr->getArea(), EPSILON);
-}
-
-BOOST_AUTO_TEST_CASE(scale_throw_exception)
-{
-  BOOST_CHECK_THROW(circle_ptr->scale(NEGATIVE_SCALE), std::invalid_argument);
-}
+// BOOST_AUTO_TEST_CASE(scale_throw_exception)
+// {
+//   BOOST_CHECK_THROW(rect_ptr->scale(NEGATIVE_SCALE), std::invalid_argument);
+// }
 
 BOOST_AUTO_TEST_SUITE_END();
 
 
-BOOST_AUTO_TEST_SUITE(composite_shape_test)
+// BOOST_AUTO_TEST_SUITE(circle_test)
 
-shkurov::Shape* rect = new shkurov::Rectangle(BEGIN_POS, LENGTH_1, LENGTH_2);
-shkurov::Shape* circle = new shkurov::Circle(BEGIN_POS, LENGTH_1);
-std::unique_ptr< shkurov::Shape > composite_ptr(new shkurov::CompositeShape({rect, circle}));
+// std::unique_ptr< shkurov::Shape > circle_ptr(new shkurov::Circle(BEGIN_POS, LENGTH_1));
 
-BOOST_AUTO_TEST_CASE(move_semantics_check)
-{
-  double origin_area = composite_ptr->getArea();
-  std::unique_ptr< shkurov::Shape > composite_ptr2 = std::move(composite_ptr);
-  BOOST_CHECK_CLOSE(origin_area, composite_ptr2->getArea(), EPSILON);
-  BOOST_CHECK_EQUAL(composite_ptr, nullptr);
+// // BOOST_AUTO_TEST_CASE(constructor_throw_exception)
+// // {
+// //   BOOST_CHECK_THROW(std::unique_ptr< shkurov::Shape >
+// //    circle_ptr2(new shkurov::Circle(BEGIN_POS, NEGATIVE_LENGTH)), std::invalid_argument);
+// // }
 
-  composite_ptr = std::move(composite_ptr2);
-  BOOST_CHECK_CLOSE(origin_area, composite_ptr->getArea(), EPSILON);
-  BOOST_CHECK_EQUAL(composite_ptr2, nullptr);
-}
+// BOOST_AUTO_TEST_CASE(move_correct_center_translocation)
+// {
+//   circle_ptr->move(END_POS);
+//   BOOST_CHECK_CLOSE(CIRCLE_AREA, circle_ptr->getArea(), EPSILON);
+//   BOOST_CHECK_CLOSE(END_POS.x, circle_ptr->getFrameRect().pos.x, EPSILON);
+//   BOOST_CHECK_CLOSE(END_POS.y, circle_ptr->getFrameRect().pos.y, EPSILON);
+// }
 
-BOOST_AUTO_TEST_CASE(move_correct_center_translocation)
-{
-  double origin_area = RECT_AREA + CIRCLE_AREA;
-  composite_ptr->move(END_POS);
-  BOOST_CHECK_CLOSE(origin_area, composite_ptr->getArea(), EPSILON);
-  BOOST_CHECK_CLOSE(END_POS.x, composite_ptr->getFrameRect().pos.x, EPSILON);
-  BOOST_CHECK_CLOSE(END_POS.y, composite_ptr->getFrameRect().pos.y, EPSILON);
-}
+// BOOST_AUTO_TEST_CASE(scale_correct_modification)
+// {
+//   circle_ptr->scale(SCALE);
+//   BOOST_CHECK_CLOSE(LENGTH_1 * SCALE, circle_ptr->getFrameRect().height / 2, EPSILON);
+//   BOOST_CHECK_CLOSE(SCALE * SCALE * CIRCLE_AREA, circle_ptr->getArea(), EPSILON);
+// }
 
-BOOST_AUTO_TEST_CASE(scale_correct_modification)
-{
-  double origin_area = RECT_AREA + CIRCLE_AREA;
-  composite_ptr->scale(SCALE);
-  BOOST_CHECK_CLOSE(RECT_AREA * SCALE * SCALE, rect->getArea(), EPSILON);
-  BOOST_CHECK_CLOSE(CIRCLE_AREA * SCALE * SCALE, circle->getArea(), EPSILON);
-  BOOST_CHECK_CLOSE(origin_area * SCALE * SCALE, composite_ptr->getArea(), EPSILON);
-  BOOST_CHECK_CLOSE(LENGTH_1 * SCALE, rect->getFrameRect().height, EPSILON);
-}
+// BOOST_AUTO_TEST_CASE(scale_throw_exception)
+// {
+//   BOOST_CHECK_THROW(circle_ptr->scale(NEGATIVE_SCALE), std::invalid_argument);
+// }
 
-BOOST_AUTO_TEST_CASE(scale_throw_exception)
-{
-  BOOST_CHECK_THROW(composite_ptr->scale(NEGATIVE_SCALE), std::invalid_argument);
-}
+// BOOST_AUTO_TEST_SUITE_END();
 
-BOOST_AUTO_TEST_SUITE_END();
+
+// BOOST_AUTO_TEST_SUITE(composite_shape_test)
+
+// shkurov::Shape* rect = new shkurov::Rectangle(BEGIN_POS, LENGTH_1, LENGTH_2);
+// shkurov::Shape* circle = new shkurov::Circle(BEGIN_POS, LENGTH_1);
+// std::unique_ptr< shkurov::Shape > composite_ptr(new shkurov::CompositeShape({rect, circle}));
+
+// BOOST_AUTO_TEST_CASE(move_semantics_check)
+// {
+//   double origin_area = composite_ptr->getArea();
+//   std::unique_ptr< shkurov::Shape > composite_ptr2 = std::move(composite_ptr);
+//   BOOST_CHECK_CLOSE(origin_area, composite_ptr2->getArea(), EPSILON);
+//   BOOST_CHECK_EQUAL(composite_ptr, nullptr);
+
+//   composite_ptr = std::move(composite_ptr2);
+//   BOOST_CHECK_CLOSE(origin_area, composite_ptr->getArea(), EPSILON);
+//   BOOST_CHECK_EQUAL(composite_ptr2, nullptr);
+// }
+
+// BOOST_AUTO_TEST_CASE(move_correct_center_translocation)
+// {
+//   double origin_area = RECT_AREA + CIRCLE_AREA;
+//   composite_ptr->move(END_POS);
+//   BOOST_CHECK_CLOSE(origin_area, composite_ptr->getArea(), EPSILON);
+//   BOOST_CHECK_CLOSE(END_POS.x, composite_ptr->getFrameRect().pos.x, EPSILON);
+//   BOOST_CHECK_CLOSE(END_POS.y, composite_ptr->getFrameRect().pos.y, EPSILON);
+// }
+
+// BOOST_AUTO_TEST_CASE(scale_correct_modification)
+// {
+//   double origin_area = RECT_AREA + CIRCLE_AREA;
+//   composite_ptr->scale(SCALE);
+//   BOOST_CHECK_CLOSE(RECT_AREA * SCALE * SCALE, rect->getArea(), EPSILON);
+//   BOOST_CHECK_CLOSE(CIRCLE_AREA * SCALE * SCALE, circle->getArea(), EPSILON);
+//   BOOST_CHECK_CLOSE(origin_area * SCALE * SCALE, composite_ptr->getArea(), EPSILON);
+//   BOOST_CHECK_CLOSE(LENGTH_1 * SCALE, rect->getFrameRect().height, EPSILON);
+// }
+
+// BOOST_AUTO_TEST_CASE(scale_throw_exception)
+// {
+//   BOOST_CHECK_THROW(composite_ptr->scale(NEGATIVE_SCALE), std::invalid_argument);
+// }
+
+// BOOST_AUTO_TEST_SUITE_END();
 

1afe2a1d7dfd2fc82f05035d7e23e8e8d4440dc8
Islam <murzakanovislam21@gmail.com>
2021-04-01 22:32:47 +0300

    Edit main.cpp

--- murzakanov.islam/A3/main.cpp
+++ murzakanov.islam/A3/main.cpp
@@ -1,41 +1,49 @@
 #include <iostream>
-
 #include "circle.hpp"
 #include "rectangle.hpp"
 #include "composite-shape.hpp"
 
+namespace mur = murzakanov;
+using RectPtr = std::shared_ptr< mur::Rectangle >;
+using CirclePtr = std::shared_ptr< mur::Circle >;
+using CmpShpPtr = std::shared_ptr< mur::CompositeShape >;
+
+void print(std::ostream& out, const murzakanov::Shape::ShapePtr shp)
+{
+  out << "Shape:\n" <<"::Position: " << "(" << shp->getX()
+      << ", " << shp->getY() << ")\n";
+  out << "Width of frame rectangle: " << shp->getWidth() << "\n"
+      << "Height of frame rectangle: " << shp->getHeight() << "\n";    
+  out << "Specific: " << shp->getName() << "\n\n";
+}
+
 int main()
 {
-  double width = 3,
-         height = 2;
-  murzakanov::point_t rectanglePos{1, 2};
-  murzakanov::Rectangle testRectangle(width, height, rectanglePos);
+  double width = 2.0,
+         height = 3.0,
+         radius = 1.766;
+  mur::point_t pos{1, 2};
+  RectPtr polyRectangle(std::make_shared< mur::Rectangle >(width, height, pos));
+  CirclePtr polyCircle(std::make_shared< mur::Circle >(radius, pos));
+  print(std::cout, polyRectangle);
+  print(std::cout, polyCircle);
 
-  double radius = 1.5;
-  murzakanov::point_t circlePos{2, 3};
-  std::shared_ptr< murzakanov::Shape > testCircle;
-  testCircle = std::make_shared< murzakanov::Circle >(radius, circlePos);
-  murzakanov::CompositeShape testArray(testCircle);
-  testArray.addShape(std::make_shared< murzakanov::Rectangle >(testRectangle));
-  std::cout << testArray.at(0).getName() << std::endl;
-  std::cout << testArray.at(1).getName() << std::endl;
-  std::cout << testArray.getFrameRect().pos.x << " "
-            << testArray.getFrameRect().pos.y << std::endl;
-  std::cout << "Координаты до скейла\n";
-  for (int i = 0; i < testArray.size(); i++)
-  {
-    std::cout << testArray.at(i).getName() << '\n';
-    std::cout << testArray.at(i).getFrameRect().pos.x
-              << " " << testArray.at(i).getFrameRect().pos.y << std::endl;
-  }
-  std::cout << "Площадь до скейла: \n" << testArray.getArea() << "\n";
-  testArray.scale(5);
-  std::cout << "Площадь после скейла: \n" << testArray.getArea() << "\n";
-  std::cout << "Координаты после скейла\n";
-  for (int i = 0; i < testArray.size(); i++)
-  {
-    std::cout << testArray.at(i).getFrameRect().pos.x
-              << " " << testArray.at(i).getFrameRect().pos.y << std::endl;
-  }
+  mur::point_t point({ 15, 23 });
+  polyRectangle->move(point);
+  std::cout << "Rectangle's info after move\n";
+  print(std::cout, polyRectangle);
+  polyRectangle->scale(2.5);
+  std::cout << "Rectangle's info after scale\n";
+  print(std::cout, polyRectangle);
+  polyCircle->move(point.x, point.y);
+  std::cout << "Circle's info after move\n";
+  print(std::cout, polyCircle);
+  
+  CmpShpPtr polyCmpShp(std::make_shared< mur::CompositeShape >(polyCircle));
+  polyCmpShp->addShape(polyRectangle);
+  print(std::cout, polyCmpShp);
+  polyCmpShp->scale(2);
+  std::cout << "Composite shape's info after scale\n";
+  print(std::cout, polyCmpShp);
   return 0;
 }

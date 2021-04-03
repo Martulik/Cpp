f5c30b3608bf98ac4181a52f1a4b1031d867481c
Shkurov Igor <lilthewil@gmail.com>
2021-04-03 18:16:25 +0300

    remade test-main.cpp for new version of composite-shape

--- shkurov.igor/A3/test-main.cpp
+++ shkurov.igor/A3/test-main.cpp
@@ -1,22 +1,14 @@
 #define BOOST_TEST_MODULE A3
+#include "test-functions.hpp"
 
 #include <limits>
 #include <stdexcept>
 #include <boost/test/included/unit_test.hpp>
 
-#include "shape.hpp"
 #include "rectangle.hpp"
 #include "circle.hpp"
 #include "composite-shape.hpp"
 
-void make_bad_circle();
-void make_bad_rectangle();
-shkurov::Shape* make_circle();
-shkurov::Shape* make_rectangle();
-shkurov::Shape* make_composite();
-void test_move_to_point(shkurov::Shape* shape);
-void test_scale(shkurov::Shape* shape);
-void invalid_scale(shkurov::Shape* shape);
 const double EPSILON = std::numeric_limits< double >::epsilon();
 
 BOOST_AUTO_TEST_SUITE(rectangle_test)

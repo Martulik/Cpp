a9b40f869d297cc6d79a97576752aa5d45eaffa1
Shkurov Igor <lilthewil@gmail.com>
2021-04-03 18:16:45 +0300

    remade main.cpp for new version of composite-shape

--- shkurov.igor/A3/main.cpp
+++ shkurov.igor/A3/main.cpp
@@ -3,6 +3,7 @@
 #include "rectangle.hpp"
 #include "circle.hpp"
 #include "composite-shape.hpp"
+#include "test-functions.hpp"
 
 void printCoordinates(const shkurov::Shape* figure)
 {
@@ -15,33 +16,34 @@ int main()
 {
   std::cout << "Rectangle test:" << '\n';
   shkurov::point_t begin_pos = {2, 2};
-  shkurov::Rectangle rect(begin_pos, 4, 4);
+  shkurov::Shape* rect = new shkurov::Rectangle(begin_pos, 4, 4);
+;
+  printCoordinates(rect);
 
-  shkurov::Shape* figure = std::addressof(rect);
-  printCoordinates(figure);
+  rect->move(-2, -2);
+  printCoordinates(rect);
 
-  figure->move(-2, -2);
-  printCoordinates(figure);
-
-  std::cout << "Area of rectangle is: " << figure->getArea() << '\n';
+  std::cout << "Area of rectangle is: " << rect->getArea() << '\n';
 
   std::cout << '\n' << "Circle test:" << '\n';
   begin_pos = {-31.2, 9.21};
-  shkurov::Circle circle(begin_pos, 2);
+  shkurov::Shape* circle = new shkurov::Circle(begin_pos, 2);
 
-  figure = std::addressof(circle);
-  printCoordinates(figure);
+  printCoordinates(circle);
 
   shkurov::point_t new_pos = {2, 2};
-  figure->move(new_pos);
-  printCoordinates(figure);
+  circle->move(new_pos);
+  printCoordinates(circle);
 
-  std::cout << "Area of circle is: " << figure->getArea() << "\n\n";
+  std::cout << "Area of circle is: " << circle->getArea() << "\n\n";
 
 
   std::cout << '\n' << "Composite-shape test:" << '\n';
-  shkurov::Rectangle rect2({4, 4}, 4, 4);
-  std::unique_ptr< shkurov::Shape > composite_shape(new shkurov::CompositeShape({&rect, &rect2, &circle}));
+  shkurov::Shape* rect2 = new shkurov::Rectangle({4, 4}, 4, 4);
+
+  std::unique_ptr< shkurov::Shape > composite_shape(
+    new shkurov::CompositeShape({rect, rect2, circle}));
+
   printCoordinates(composite_shape.get());
 
   composite_shape->move(2, -2);

3cf7a45c0c83fc151f6df621d63f7a4f5c43825c
Danil Doroshin <ddddanil5555@gmail.com>
2021-04-03 19:33:32 +0300

    Guard from malformed task numbers

--- doroshin.danil/B1/main.cpp
+++ doroshin.danil/B1/main.cpp
@@ -44,6 +44,9 @@ int main(int argc, char* argv[])
     if(argc < 2) {
       throw ArgumentParseException("No task number");
     }
+    if(argv[1][1] != '\0') {
+      throw ArgumentParseException("Task number malformed");
+    }
     const int task_num = argv[1][0] - '0';
     switch (task_num)
     {

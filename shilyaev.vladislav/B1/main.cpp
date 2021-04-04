928a1637a32ce2052a504e5614f701579909a28f
Vladislav Shilyaev <schilyaev.vlad@gmail.com>
2021-04-04 20:37:55 +0300

    Initial B1 commit

---
+++ shilyaev.vladislav/B1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "task-sort.hpp"
+#include "task-copy.hpp"
+
+int main(int argc, char *argv[])
+{
+  if (argc <= 1) {
+    std::cerr << "Task not specified";
+    return 1;
+  }
+  int taskNumber = std::atoi(argv[1]);
+  switch (taskNumber) {
+    default: {
+      std::cerr << "Unknown task";
+      return 1;
+    }
+  }
+}

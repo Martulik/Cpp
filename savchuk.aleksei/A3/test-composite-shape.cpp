735e4f26208d9193e37cb0415083b28b73fa38e4
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-04-05 01:01:44 +0300

    Fix the Composite's tests

--- savchuk.aleksei/A3/test-composite-shape.cpp
+++ savchuk.aleksei/A3/test-composite-shape.cpp
@@ -8,8 +8,10 @@
 
 namespace lab = savchuk;
 
-lab::CompositeShape makeComposite(const lab::Rectangle& r, const lab::Circle& c)
+lab::CompositeShape makeComposite()
 {
+  lab::Rectangle r = makeRect(RECT_POS, WIDTH, HEIGHT);
+  lab::Circle c = makeCirc(CIRC_POS, RADIUS);
   return lab::CompositeShape({ &r, &c });
 }
 
@@ -17,34 +19,26 @@ BOOST_AUTO_TEST_SUITE(composite_shape)
 
 BOOST_AUTO_TEST_CASE(move_invariant)
 {
-  lab::Rectangle r = makeRect(RECT_POS, WIDTH, HEIGHT);
-  lab::Circle c = makeCirc(CIRC_POS, RADIUS);
-  lab::CompositeShape comp = makeComposite(r, c);
+  lab::CompositeShape comp = makeComposite();
   checkMoveInvariant(comp, { -1.6, 13.5 });
   checkMoveInvariant(comp, 2.3, -12.6);
 }
 
 BOOST_AUTO_TEST_CASE(scale_frame_rectangle)
 {
-  lab::Rectangle r = makeRect(RECT_POS, WIDTH, HEIGHT);
-  lab::Circle c = makeCirc(CIRC_POS, RADIUS);
-  lab::CompositeShape comp = makeComposite(r, c);
+  lab::CompositeShape comp = makeComposite();
   checkScaleFrameRect(comp, CORRECT_SCALE_FACTOR);
 }
 
 BOOST_AUTO_TEST_CASE(scale_area)
 {
-  lab::Rectangle r = makeRect(RECT_POS, WIDTH, HEIGHT);
-  lab::Circle c = makeCirc(CIRC_POS, RADIUS);
-  lab::CompositeShape comp = makeComposite(r, c);
+  lab::CompositeShape comp = makeComposite();
   checkScaleArea(comp, CORRECT_SCALE_FACTOR);
 }
 
 BOOST_AUTO_TEST_CASE(scale_invalid_argument)
 {
-  lab::Rectangle r = makeRect(RECT_POS, WIDTH, HEIGHT);
-  lab::Circle c = makeCirc(CIRC_POS, RADIUS);
-  lab::CompositeShape comp = makeComposite(r, c);
+  lab::CompositeShape comp = makeComposite();
   checkScaleInvalidArgument(comp, WRONG_SCALE_FACTOR);
 }
 
@@ -56,7 +50,7 @@ BOOST_AUTO_TEST_CASE(correct_frame_rectangle)
   const lab::rectangle_t FRAME_RECT = { POS, 2 * RAD, 2 * RAD };
   lab::Rectangle r = makeRect(POS, SIDE, SIDE);
   lab::Circle c = makeCirc(POS, RAD);
-  lab::CompositeShape comp = makeComposite(r, c);
+  lab::CompositeShape comp({ &r, &c });
   checkCorrectFrameRect(comp, FRAME_RECT);
 }
 

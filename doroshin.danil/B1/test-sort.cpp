3c5a0d9c8f558fa774501c05cf228fee49f7af25
Danil Doroshin <ddddanil5555@gmail.com>
2021-04-04 23:37:57 +0300

    Generalise make_suite for any type

--- doroshin.danil/B1/test-sort.cpp
+++ doroshin.danil/B1/test-sort.cpp
@@ -7,22 +7,24 @@
 #include "test-random.hpp"
 #include "sort-strategies.hpp"
 
+using namespace std::literals::string_literals;
 namespace mpl = boost::mpl;
 namespace test = boost::unit_test;
 namespace dan = doroshin;
 
+template< typename T, typename RandomStrat >
 test::test_suite* make_suite()
 {
-  using Strategies = mpl::list< dan::VectorIndexStrat< int >, dan::VectorAtStrat< int >, dan::ListIterStrat< int > >;
-  using Orderings = mpl::list< std::less< int >, std::greater< int > >;
-  const std::vector< std::pair< int, int > > limits = {
-      { 0, 1 },
-      { -10, 10 },
-      { -1000, 1000 },
-      { std::numeric_limits< int >::min(), std::numeric_limits< int >::max() }
-    };
+  using Strategies = mpl::list< dan::VectorIndexStrat< T >, dan::VectorAtStrat< T >, dan::ListIterStrat< T > >;
+  using Orderings = mpl::list< std::less< T >, std::greater< T > >;
+  const std::vector< std::pair< T, T > > limits = {
+    { 0, 1 },
+    { -10, 10 },
+    { -1000, 1000 },
+    { std::numeric_limits< T >::lowest(), std::numeric_limits< T >::max() }
+  };
 
-  test::test_suite* suite = BOOST_TEST_SUITE("Random int");
+  test::test_suite* suite = BOOST_TEST_SUITE("Random "s + typeid(T).name());
 
   mpl::for_each< Strategies >(
     [&](auto _strat) {
@@ -32,14 +34,14 @@ test::test_suite* make_suite()
           using Order = decltype(_order);
 
           for(const auto& limit: limits) {
-            int min = limit.first;
-            int max = limit.second;
+            T min = limit.first;
+            T max = limit.second;
             for(size_t i = 1; i < 300; ++i) {
               size_t repeat = std::floor(std::log(i));
               for(size_t j = 0; j <= repeat; ++j) {
                 std::ostringstream name;
-                name << typeid(Strategy).name() << ' ' << typeid(Order).name() << ' ' << min << ' ' << max << ' ' << i << ' ' << j;
-                suite->add(BOOST_TEST_CASE_NAME(std::bind(*std::make_shared< dan::TestRandomSort< int, dan::UniformIntStrat< int >, Strategy, Order > >(min, max, i, _order)), name.str()));
+                name << typeid(T).name() << ' ' << typeid(Strategy).name() << ' ' << typeid(Order).name() << ' ' << min << ' ' << max << ' ' << i << ' ' << j;
+                suite->add(BOOST_TEST_CASE_NAME(std::bind(*std::make_shared< dan::TestRandomSort< T, RandomStrat, Strategy, Order > >(min, max, i, _order)), name.str()));
               }
             }
           }
@@ -51,4 +53,4 @@ test::test_suite* make_suite()
   return suite;
 }
 
-static dan::test_suite_registrar registrar(make_suite(), &test::framework::master_test_suite(), test::decorator::collector_t::instance());
+static dan::test_suite_registrar register_int(make_suite< int, dan::UniformIntStrat< int > >(), &test::framework::master_test_suite(), test::decorator::collector_t::instance());

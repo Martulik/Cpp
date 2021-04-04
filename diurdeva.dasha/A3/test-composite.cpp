e6d30ecf30e71132d3a3a23b0028e7785e7d0abb
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-04-05 00:07:03 +0300

    Add  function makeCopmositeShape

--- diurdeva.dasha/A3/test-composite.cpp
+++ diurdeva.dasha/A3/test-composite.cpp
@@ -9,33 +9,27 @@
 const diurdeva::point_t pointCenter = { 3.5, 4.0 };
 const diurdeva::point_t newCenter = { 3.0, 2.0 };
 const double width = 8.0;
-const double width2 = 1.0;
 const double height = 5.0;
-const double height2 = 2.0;
 const double radius = 3.0;
-const double radius2 = 4.0;
 const double negativeOptions = -8.0;
 const size_t testIndex = 200;
-const size_t reserveValue = 2;
+const size_t reserveValue = 10;
 
-using shapePtr = std::shared_ptr< diurdeva::Shape >;
-
-void pushShapes(diurdeva::CompositeShape* shape)
+std::unique_ptr< diurdeva::CompositeShape > makeCompositeShape()
 {
-  shapePtr rectangle = std::make_shared< diurdeva::Rectangle >(width2, height2, pointCenter);
-  shape->pushBack(rectangle);
-
-  shapePtr circle = std::make_shared< diurdeva::Circle >(radius2, pointCenter);
-  shape->pushBack(circle);
+  auto circle = std::make_unique< diurdeva::Circle >(radius, pointCenter);
+  auto rectangle = std::make_unique< diurdeva::Rectangle >(width, height, pointCenter);
+  auto shape = std::make_unique< diurdeva::CompositeShape >(std::move(circle));
+  shape->pushBack(std::move(rectangle));
+  return shape;
 }
 
 BOOST_AUTO_TEST_SUITE(testCompositeShape)
 
 BOOST_AUTO_TEST_CASE(invalidArgumentCompositeShape)
 {
-  diurdeva::CompositeShape composite(std::make_shared< diurdeva::Rectangle >(width, height, pointCenter));
-  BOOST_CHECK_THROW(composite.scale(negativeOptions), std::invalid_argument);
-  BOOST_CHECK_THROW(composite.at(testIndex), std::out_of_range);
+  BOOST_CHECK_THROW(makeCompositeShape()->scale(negativeOptions), std::invalid_argument);
+  BOOST_CHECK_THROW(makeCompositeShape()->at(testIndex), std::out_of_range);
 }
 
 BOOST_AUTO_TEST_CASE(testPopBack)
@@ -46,23 +40,19 @@ BOOST_AUTO_TEST_CASE(testPopBack)
 
 BOOST_AUTO_TEST_CASE(testReserve)
 {
-  diurdeva::CompositeShape composite(std::make_shared< diurdeva::Circle >(radius, pointCenter));
+  diurdeva::CompositeShape composite(makeCompositeShape());
   composite.reserve(reserveValue);
   BOOST_CHECK_EQUAL(composite.capacity(), reserveValue);
 }
 
 BOOST_AUTO_TEST_CASE(moveCompositeShape)
 {
-  diurdeva::CompositeShape composite(std::make_shared< diurdeva::Rectangle >(width, height, pointCenter));
-  pushShapes(&composite);
-  checkConstOptionsBeforeMoving(&composite);
+  checkConstOptionsBeforeMoving(*makeComposireShape());
 }
 
 BOOST_AUTO_TEST_CASE(scaleCompositeShape)
 {
-  diurdeva::CompositeShape composite(std::make_shared< diurdeva::Circle >(radius, pointCenter));
-  pushShapes(&composite);
-  checkAreaBeforeScale(&composite);
+  checkAreaBeforeScale(*makeComposireShape());
 }
 
 BOOST_AUTO_TEST_SUITE_END()

3882cf2b2c59a30f30d416b1e0b1ef2b304cd427
Islam <murzakanovislam21@gmail.com>
2021-04-04 04:57:05 +0300

    Add test files

---
+++ murzakanov.islam/A3/test-composite.cpp
@@ -0,0 +1,20 @@
+#include "test-functions.hpp"
+
+BOOST_AUTO_TEST_SUITE(test_CompositeShape)
+
+  BOOST_AUTO_TEST_CASE(test_move)
+  {
+    murzakanov::testMove(murzakanov::makeCompositeSh());
+  }
+
+  BOOST_AUTO_TEST_CASE(test_scale)
+  {
+    murzakanov::testScale(murzakanov::makeCompositeSh());
+  }
+
+  BOOST_AUTO_TEST_CASE(test_invalid_argument)
+  {
+    BOOST_CHECK_THROW(murzakanov::CompositeShape({}), std::invalid_argument);
+  }
+  
+BOOST_AUTO_TEST_SUITE_END()

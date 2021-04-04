fb633be87c68eb6c29d12479a7aeb20370522701
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-04-05 00:32:57 +0300

    Add specifier extern for the test values

--- savchuk.aleksei/A3/test-values.hpp
+++ savchuk.aleksei/A3/test-values.hpp
@@ -5,12 +5,12 @@
 
 namespace lab = savchuk;
 
-const lab::point_t& RECT_POS = { 2.3, -7.1 };
-const double WIDTH = 6.1;
-const double HEIGHT = 9.4;
-const lab::point_t& CIRC_POS = { 42.8, -11.7 };
-const double RADIUS = 3.5;
-const double CORRECT_SCALE_FACTOR = 4.6;
-const double WRONG_SCALE_FACTOR = -5.1;
+extern const lab::point_t& RECT_POS;
+extern const double WIDTH;
+extern const double HEIGHT;
+extern const lab::point_t& CIRC_POS;
+extern const double RADIUS;
+extern const double CORRECT_SCALE_FACTOR;
+extern const double WRONG_SCALE_FACTOR;
 
 #endif

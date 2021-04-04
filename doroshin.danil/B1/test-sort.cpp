e027a8060f3505c24cbc400d97fc3dd8d936cf08
Danil Doroshin <ddddanil5555@gmail.com>
2021-04-04 21:33:48 +0300

    Random int suite v1

--- doroshin.danil/B1/test-sort.cpp
+++ doroshin.danil/B1/test-sort.cpp
@@ -1,41 +1,81 @@
 #include <boost/test/unit_test.hpp>
+#include <boost/test/parameterized_test.hpp>
+#include <functional>
+#include <iomanip>
+#include <algorithm>
+#include <random>
+#include <limits>
 #include "insert-sort.hpp"
 #include "sort-strategies.hpp"
 
+namespace test = boost::unit_test;
 namespace dan = doroshin;
 
-BOOST_AUTO_TEST_SUITE(insert_sort)
-
-BOOST_AUTO_TEST_CASE(vector_ix_empty)
+template< typename Strat, typename Cmp >
+class TestRandomInts
 {
-  std::vector< int > empty;
-  dan::insert_sort< int, dan::VectorIndexStrat< int > >
-      (empty, dan::VectorIndexStrat< int >::begin(empty), dan::VectorIndexStrat< int >::end(empty));
-  BOOST_CHECK(empty == std::vector< int >{});
+public:
+  TestRandomInts(size_t N, Cmp cmp);
+
+  void sort();
+  void test() const;
+
+  void operator()();
+private:
+  static size_t count;
+  Cmp compare;
+  typename Strat::container_t values;
+};
+
+template< typename Strat, typename Cmp >
+size_t TestRandomInts< Strat, Cmp >::count = 0;
+
+template< typename Strat, typename Cmp >
+TestRandomInts< Strat, Cmp >::TestRandomInts(size_t N, Cmp cmp):
+  compare(cmp)
+{
+  const int min = std::numeric_limits<int>::min();
+  const int max = std::numeric_limits<int>::max();
+  std::random_device rd;
+  std::default_random_engine gen(rd());
+  std::uniform_int_distribution< int > dist(min, max);
+  auto random = std::bind(dist, gen);
+
+  std::cout << "Random int: " << std::setw(3) << (++count) << '\r';
+
+  values.reserve(N);
+  std::generate_n(std::back_inserter(values), N, random);
 }
 
-BOOST_AUTO_TEST_CASE(vector_ix_asc)
+template< typename Strat, typename Cmp >
+void TestRandomInts< Strat, Cmp >::sort()
 {
-  std::vector< int > vec { 5, 7, 6, 1, 4, 3, 2 };
-  dan::insert_sort< int, dan::VectorIndexStrat< int > >
-      (vec, dan::VectorIndexStrat< int >::begin(vec), dan::VectorIndexStrat< int >::end(vec));
-  BOOST_CHECK(vec == (std::vector< int >{ 1, 2, 3, 4, 5, 6, 7 }));
+  dan::insert_sort< int, Strat >(values, Strat::begin(values), Strat::end(values), compare);
 }
 
-BOOST_AUTO_TEST_CASE(vector_ix_desc)
+template< typename Strat, typename Cmp >
+void TestRandomInts< Strat, Cmp >::test() const
 {
-  std::vector< int > vec { 5, 7, 6, 1, 4, 3, 2 };
-  dan::insert_sort< int, dan::VectorIndexStrat< int > >
-      (vec, dan::VectorIndexStrat< int >::begin(vec), dan::VectorIndexStrat< int >::end(vec), std::greater<>());
-  BOOST_CHECK(vec == (std::vector< int >{ 7, 6, 5, 4, 3, 2, 1 }));
+  BOOST_CHECK(std::is_sorted(std::begin(values), std::end(values), compare));
 }
 
-BOOST_AUTO_TEST_CASE(vector_ix_eq)
+template< typename Strat, typename Cmp >
+void TestRandomInts< Strat, Cmp >::operator()()
 {
-  std::vector< int > vec { 1, 0, 0, 1, 1, 0, 0 };
-  dan::insert_sort< int, dan::VectorIndexStrat< int > >
-      (vec, dan::VectorIndexStrat< int >::begin(vec), dan::VectorIndexStrat< int >::end(vec));
-  BOOST_CHECK(vec == (std::vector< int >{ 0, 0, 0, 0, 1, 1, 1 }));
+  sort();
+  test();
+}
+
+BOOST_AUTO_TEST_SUITE(sort_int)
+
+BOOST_AUTO_TEST_CASE(sort)
+{
+  test::test_suite& suite = test::framework::current_auto_test_suite();
+  for(size_t i = 0; i < 300; ++i) {
+    for(size_t j = 0; j <= std::floor(std::log(i)); ++j) {
+      suite.add(BOOST_TEST_CASE(std::bind(*std::make_shared< TestRandomInts< dan::VectorIndexStrat< int >, std::less<int> > >(i, std::less< int >()))));
+    }
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()

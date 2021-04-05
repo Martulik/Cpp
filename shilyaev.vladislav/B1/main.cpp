80f39cf68fdb5569255623fe310369ec7e007172
Vladislav Shilyaev <schilyaev.vlad@gmail.com>
2021-04-06 01:31:48 +0300

    Add check for one digit as task number

--- shilyaev.vladislav/B1/main.cpp
+++ shilyaev.vladislav/B1/main.cpp
@@ -4,13 +4,29 @@
 #include "task-iterators.hpp"
 #include "task-random.hpp"
 
+namespace shilyaev {
+  int toDigit(const std::string &string)
+  {
+    if (string.size() != 1 || string[0] < '0' || string[1] > '9') {
+      throw std::invalid_argument("String must be one digit");
+    }
+    return string[0] - '0';
+  }
+}
+
 int main(int argc, char *argv[])
 {
   if (argc <= 1) {
     std::cerr << "Task not specified";
     return 1;
   }
-  int taskNumber = std::atoi(argv[1]);
+  int taskNumber = -1;
+  try {
+    taskNumber = shilyaev::toDigit(argv[1]);
+  } catch (const std::invalid_argument &) {
+    std::cerr << "Task must be one digit";
+    return 1;
+  }
   switch (taskNumber) {
     case 1: {
       return shilyaev::taskSort(argc, argv);

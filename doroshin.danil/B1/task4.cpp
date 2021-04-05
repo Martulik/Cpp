2c3ee9a832d51ac3d22578c4e01fb06c59e12c71
Danil Doroshin <ddddanil5555@gmail.com>
2021-04-05 10:19:05 +0300

    Use generate_n

--- doroshin.danil/B1/task4.cpp
+++ doroshin.danil/B1/task4.cpp
@@ -1,4 +1,6 @@
 #include "task4.hpp"
+#include <algorithm>
+#include <functional>
 #include <random>
 
 void doroshin::fillRandom(double* array, size_t size)
@@ -6,8 +8,7 @@ void doroshin::fillRandom(double* array, size_t size)
   std::random_device rd;
   std::default_random_engine gen(rd());
   std::uniform_real_distribution< double > dist(-1.0, 1.0);
+  auto random = std::bind(dist, gen);
 
-  for(size_t i = 0; i < size; ++i) {
-    array[i] = dist(gen);
-  }
+  std::generate_n(array, size, random);
 }

41bd45c860546164930234e819895db2c50a82d5
Islam <murzakanovislam21@gmail.com>
2021-04-09 21:43:19 +0300

    Fix task 1

--- murzakanov.islam/B1/task1.cpp
+++ murzakanov.islam/B1/task1.cpp
@@ -6,45 +6,35 @@
 #include "tasks.hpp"
 #include "strategy.hpp"
 #include "sorting.hpp"
+#include "tools.hpp"
 
 void murzakanov::task1(const std::string& order)
 {
-  int x;
-  std::vector< int > firstVector;
-  while (!(std::cin >> x).eof())
+  if (order != "ascending" && order != "descending")
   {
-    if (std::cin.fail())
-    {
-      throw std::invalid_argument("Wrong input\n");
-    }
+    throw std::invalid_argument("Wrong order");
+  }
+  int x = 0;
+  std::vector< int > firstVector;
+  while (std::cin >> x)
+  {
     firstVector.push_back(x);
   }
-
-  unsigned int i = 0;
-  std::cout << "\n";
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    throw std::invalid_argument("Read error");
+  }
+  if (firstVector.empty())
+  {
+    return;
+  }
   std::vector< int > secondVector(firstVector);
   std::forward_list< int > list(firstVector.begin(), firstVector.end());
   sort< StrategyBrackets< int > >(firstVector, order);
   sort< StrategyAt< int > >(secondVector, order);
   sort< StrategyIter< int > >(list, order);
 
-  i = 0;
-  while (i < firstVector.size())
-  {
-    std::cout << firstVector.at(i++) << " ";
-  }
-  std::cout << "\n";
-  i = 0;
-  while (i < secondVector.size())
-  {
-    std::cout << secondVector.at(i++) << " ";
-  }
-  std::cout << "\n";
-  auto it = list.begin();
-  while (it != list.end())
-  {
-    std::cout << *it << " ";
-    it++;
-  }
-  std::cout << "\n";
+  print(firstVector, std::cout);
+  print(secondVector, std::cout);
+  print(list, std::cout);
 }

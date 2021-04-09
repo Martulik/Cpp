05abec1e6f94253bcd95601491c9a311121b9357
Islam <murzakanovislam21@gmail.com>
2021-04-09 21:37:50 +0300

    Fix main 2x

--- murzakanov.islam/B1/main.cpp
+++ murzakanov.islam/B1/main.cpp
@@ -10,18 +10,18 @@ int main(int argc, char* argv[])
     throw std::invalid_argument("Should be more parametrs");
   }
   int exNum = int(argv[1][0] - '0');
-  if (exNum == 1 && argc == 3) {
+  if (exNum == 1) {
     std::string order = argv[2];
     murzakanov::task1(order);
   }
-  else if (exNum == 2 && argc == 2) {
+  else if (exNum == 2) {
     std::string name = argv[2];
     murzakanov::task2(name);
   }
-  else if (exNum == 3 && argc == 1) {
+  else if (exNum == 3) {
     murzakanov::task3();
   }
-  else if (exNum == 4 && argc == 4) {
+  else if (exNum == 4) {
     std::string order = argv[2];
     int size = atoi(argv[3]);
     murzakanov::task4(order, size);

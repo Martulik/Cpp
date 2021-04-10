c0ef85d9936c17dc63f43ded663c54ac65ccc06a
Islam <murzakanovislam21@gmail.com>
2021-04-10 14:41:23 +0300

    Add srand

--- murzakanov.islam/B1/tools.cpp
+++ murzakanov.islam/B1/tools.cpp
@@ -1,10 +1,13 @@
 #include "tools.hpp"
+
 #include <random>
+#include <ctime>
 
 void murzakanov::fillRandom(double* array, int size)
 {
-    for (int i = 0; i < size; i++)
-    {
-      array[i] = double(rand()) / RAND_MAX * 2 - 1;
-    }
+  srand(time(0));
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = double(rand()) / RAND_MAX * 2 - 1;
+  }
 }

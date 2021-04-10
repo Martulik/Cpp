7ec952f89cdd3e84161a2b5ec35e9a0972668da6
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-04-10 16:18:12 +0300

    Rewrite the test values as constexpr

--- savchuk.aleksei/A3/test-values.hpp
+++ savchuk.aleksei/A3/test-values.hpp
@@ -5,12 +5,12 @@
 
 namespace lab = savchuk;
 
-extern const lab::point_t& RECT_POS;
-extern const double WIDTH;
-extern const double HEIGHT;
-extern const lab::point_t& CIRC_POS;
-extern const double RADIUS;
-extern const double CORRECT_SCALE_FACTOR;
-extern const double WRONG_SCALE_FACTOR;
+constexpr lab::point_t RECT_POS = { 2.3, -7.1 };
+constexpr double WIDTH = 6.1;
+constexpr double HEIGHT = 9.4;
+constexpr lab::point_t CIRC_POS = { 42.8, -11.7 };
+constexpr double RADIUS = 3.5;
+constexpr double CORRECT_SCALE_FACTOR = 4.6;
+constexpr double WRONG_SCALE_FACTOR = -5.1;
 
 #endif

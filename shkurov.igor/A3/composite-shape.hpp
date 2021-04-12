bd8fca3098f57033da8fd13c46cdb9e00f65493c
Shkurov Igor <lilthewil@gmail.com>
2021-04-12 23:37:07 +0300

    fixed idents in composite-shape.hpp

--- shkurov.igor/A3/composite-shape.hpp
+++ shkurov.igor/A3/composite-shape.hpp
@@ -33,27 +33,26 @@ namespace shkurov
     array_ptr shape_array_;
   };
 
-    template< typename... Pointers >
-    CompositeShape::CompositeShape(Pointers... pointers):
-      shape_count_(sizeof...(Pointers)),
-      shape_array_(std::make_unique< member_ptr[] >(shape_count_))
+  template< typename... Pointers >
+  CompositeShape::CompositeShape(Pointers... pointers):
+    shape_count_(sizeof...(Pointers)),
+    shape_array_(std::make_unique< member_ptr[] >(shape_count_))
+  {
+    if (shape_count_ == 0)
     {
-      if (shape_count_ == 0)
-      {
-        throw std::invalid_argument("Exception: empty parameter pack is not allowed");
-      }
-
-      std::unique_ptr< shkurov::Shape > temp_collection[] = {std::move(pointers)...};
-      for (size_t i = 0; i < shape_count_; i++)
-      {
-        if (temp_collection[i] == nullptr)
-        {
-          throw std::invalid_argument("Exception: nullptr ");
-        }
-        shape_array_[i] = std::move(temp_collection[i]);
-      }
+      throw std::invalid_argument("Exception: empty parameter pack is not allowed");
     }
 
+    std::unique_ptr< shkurov::Shape > temp_collection[] = {std::move(pointers)...};
+    for (size_t i = 0; i < shape_count_; i++)
+    {
+      if (temp_collection[i] == nullptr)
+      {
+        throw std::invalid_argument("Exception: nullptr ");
+      }
+      shape_array_[i] = std::move(temp_collection[i]);
+    }
+  }
 }
 
 #endif

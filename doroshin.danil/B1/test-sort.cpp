29937b7465d6b2103df23917654797066d10e1fd
Danil Doroshin <ddddanil5555@gmail.com>
2021-04-12 09:44:57 +0300

    Simplify testing

--- doroshin.danil/B1/test-sort.cpp
+++ doroshin.danil/B1/test-sort.cpp
@@ -1,78 +1,49 @@
-#include <limits>
-#include <typeinfo>
 #include <boost/test/unit_test.hpp>
-#include <boost/test/parameterized_test.hpp>
-#include <boost/mpl/list.hpp>
-#include <boost/mpl/for_each.hpp>
-#include "test-registrar.hpp"
-#include "test-random.hpp"
+#include "insert-sort.hpp"
 #include "sort-strategies.hpp"
 
-using namespace std::literals::string_literals;
-namespace mpl = boost::mpl;
-namespace test = boost::unit_test;
 namespace dan = doroshin;
 
-template< typename T, typename RandomStrat >
-test::test_suite* make_suite()
+BOOST_AUTO_TEST_SUITE(insert_sort)
+
+BOOST_AUTO_TEST_CASE(vector_ix_empty)
 {
-  using Strategies = mpl::list<
-      dan::VectorIndexStrat< T >,
-      dan::VectorAtStrat< T >,
-      dan::ListIterStrat< T >
-  >;
-  using Orderings = mpl::list<
-      std::less< T >,
-      std::greater< T >
-  >;
-  const std::vector< std::pair< T, T > > limits = {
-    { 0, 1 },
-    { -10, 10 },
-    { std::numeric_limits< T >::lowest(), std::numeric_limits< T >::max() }
-  };
-
-  test::test_suite* suite = BOOST_TEST_SUITE("Random "s + typeid(T).name());
-
-  mpl::for_each< Strategies >(
-    [&](auto _strat) {
-      using Strategy = decltype(_strat);
-      mpl::for_each< Orderings >(
-        [&](auto _order) {
-          using Order = decltype(_order);
-
-          for(const auto& limit: limits) {
-            T min = limit.first;
-            T max = limit.second;
-            for(size_t i = 1; i < 10; ++i) {
-              size_t length = i * 10;
-              std::ostringstream name;
-              name << typeid(T).name() << ' '
-                  << typeid(Strategy).name() << ' '
-                  << typeid(Order).name() << ' '
-                  << min << ' ' << max
-                  << ' ' << i;
-              suite->add(
-                  test::make_test_case(
-                    *std::make_shared<
-                      dan::TestRandomSort< T, RandomStrat, Strategy, Order >
-                    >(min, max, length, _order),
-                    name.str(), __FILE__, __LINE__
-                    ));
-            }
-          }
-        }
-      );
-    }
-  );
-
-  return suite;
+  std::vector< int > empty;
+  dan::insert_sort< int, dan::VectorIndexStrat< int > >
+      (empty, dan::VectorIndexStrat< int >::begin(empty), dan::VectorIndexStrat< int >::end(empty));
+  BOOST_CHECK(empty == std::vector< int >{});
 }
 
-BOOST_AUTO_TEST_SUITE(sort);
-static dan::test_suite_registrar register_int(
-    make_suite< int, dan::UniformIntStrat< int > >(),
-    boost::unit_test::decorator::collector::instance());
-static dan::test_suite_registrar register_double(
-    make_suite< double, dan::UniformRealStrat< double > >(),
-    boost::unit_test::decorator::collector::instance());
-BOOST_AUTO_TEST_SUITE_END();
+BOOST_AUTO_TEST_CASE(vector_ix_asc)
+{
+  std::vector< int > vec { 5, 7, 6, 1, 4, 3, 2 };
+  dan::insert_sort< int, dan::VectorIndexStrat< int > >
+      (vec, dan::VectorIndexStrat< int >::begin(vec), dan::VectorIndexStrat< int >::end(vec));
+  BOOST_CHECK(vec == (std::vector< int >{ 1, 2, 3, 4, 5, 6, 7 }));
+}
+
+BOOST_AUTO_TEST_CASE(vector_ix_desc)
+{
+  std::vector< int > vec { 5, 7, 6, 1, 4, 3, 2 };
+  dan::insert_sort< int, dan::VectorIndexStrat< int > >
+      (vec, dan::VectorIndexStrat< int >::begin(vec), dan::VectorIndexStrat< int >::end(vec), std::greater<>());
+  BOOST_CHECK(vec == (std::vector< int >{ 7, 6, 5, 4, 3, 2, 1 }));
+}
+
+BOOST_AUTO_TEST_CASE(vector_at_asc)
+{
+  std::vector< int > vec { 5, 7, 6, 1, 4, 3, 2 };
+  dan::insert_sort< int, dan::VectorAtStrat< int > >
+      (vec, dan::VectorAtStrat< int >::begin(vec), dan::VectorAtStrat< int >::end(vec));
+  BOOST_CHECK(vec == (std::vector< int >{ 1, 2, 3, 4, 5, 6, 7 }));
+}
+
+BOOST_AUTO_TEST_CASE(list_it_asc)
+{
+  std::forward_list< int > vec { 5, 7, 6, 1, 4, 3, 2 };
+  dan::insert_sort< int, dan::ListIterStrat< int > >
+      (vec, dan::ListIterStrat< int >::begin(vec), dan::ListIterStrat< int >::end(vec));
+  BOOST_CHECK(vec == (std::forward_list< int >{ 1, 2, 3, 4, 5, 6, 7 }));
+}
+
+BOOST_AUTO_TEST_SUITE_END()

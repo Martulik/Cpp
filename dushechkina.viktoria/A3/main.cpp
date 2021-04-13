bdcfceeaf3eeb401e95242978ac6119b839fd4d5
viktoria.dushechkina <byzaya@icloud.com>
2021-04-13 18:45:00 +0300

    fix main whitespaces

--- dushechkina.viktoria/A3/main.cpp
+++ dushechkina.viktoria/A3/main.cpp
@@ -24,29 +24,30 @@ int main()
 {
   try
   {
-  const double width = 0.08;
-  const double height = 20.48;
-  const vika::point_t p1 = { 12.8, 25.6 };
-  const vika::point_t p2 = { -8.0, 3.2 };
+    const double width = 0.08;
+    const double height = 20.48;
+    const vika::point_t p1 = { 12.8, 25.6 };
+    const vika::point_t p2 = { -8.0, 3.2 };
 
-  ShapePtr rec = std::make_shared< vika::Rectangle >(width, height, p1);
-  std::cout << "Rectangle: ";
-  testShape(rec, std::cout, p2);
+    ShapePtr rec = std::make_shared< vika::Rectangle >(width, height, p1);
+    std::cout << "Rectangle: ";
+    testShape(rec, std::cout, p2);
 
-  ShapePtr circle = std::make_shared< vika::Circle >(p1, width);
-  std::cout << "\nCircle: ";
-  testShape(circle, std::cout, p2);
-  circle->move(p1);
+    ShapePtr circle = std::make_shared< vika::Circle >(p1, width);
+    std::cout << "\nCircle: ";
+    testShape(circle, std::cout, p2);
+    circle->move(p1);
 
-  vika::CompositeShape compositeShape(rec);
-  compositeShape.pushBack(circle);
-  compositeShape.pushBack(std::make_shared< vika::Circle >(p2, height));
-  std::cout << "\nComposite Shape: ";
-  for (size_t i = 0; i < compositeShape.size(); i++)
-  {
-    testShape(compositeShape.at(i), std::cout, p2);
+    vika::CompositeShape compositeShape(rec);
+    compositeShape.pushBack(circle);
+    compositeShape.pushBack(std::make_shared< vika::Circle >(p2, height));
+    std::cout << "\nComposite Shape: ";
+    for (size_t i = 0; i < compositeShape.size(); i++)
+    {
+      testShape(compositeShape.at(i), std::cout, p2);
+    }
   }
-  }
+
   catch (const std::invalid_argument& e)
   {
     std::cerr << e.what() << "\n";

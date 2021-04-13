4dc84d48f95e4786c84f51d12cd6a5d11d9ef2df
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-04-13 22:53:12 +0300

    Change main

--- diurdeva.dasha/B1/main.cpp
+++ diurdeva.dasha/B1/main.cpp
@@ -6,16 +6,6 @@
 
 namespace lab = diurdeva;
 
-std::string error = "Null";
-int code = 0;
-
-int setError(std::string err)
-{
-  code = 1;
-  error = err;
-  return code;
-}
-
 int main(int argc, char *argv[])
 {
   if ((argc < 2) || (argc > 4)) {
@@ -30,17 +20,21 @@ int main(int argc, char *argv[])
     return 1;
   }
 
+  std::string error = "Null";
+  int code = 0;
+
   if (taskNumber == 1 && argc == 3) {
     const std::function< bool(int, int) > &compare = lab::getCompare< int >(argv[2]);
-    code = compare ? lab::task1(compare) : setError("Invalid direction");
+    error = compare ? "Null" : "Incorrect direction";
+    code = (error == "Null") ? lab::task1(compare) : 1;
   } else if (taskNumber == 2 && argc == 3) {
     code = lab::task2(argv[2]);
   } else if (taskNumber == 3 && argc == 2) {
     code = lab::task3();
   } else if (taskNumber == 4 && argc == 4) {
     const std::function< bool(double, double) > &compare = lab::getCompare< double >(argv[2]);
-    code = compare && lab::checkIsNumber(argv[3]) ?
-            lab::task4(compare, atoi(argv[3])) : setError("Invalid direction");
+    error = compare && lab::checkIsNumber(argv[3]) ? "Null" : "Incorrect direction";
+    code = (error == "Null") ? lab::task4(compare, atoi(argv[3])) : 1;
   } else {
     code = setError("Incorrect number of arguments.\n");
   }

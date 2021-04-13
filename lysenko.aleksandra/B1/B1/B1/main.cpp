f966a46a2f7f54a7305adbce2f7e999f32f8215a
LysenkoAleksandra <orange260302@gmail.com>
2021-04-13 21:24:56 +0300

    Draft of main.cpp v2

--- lysenko.aleksandra/B1/B1/B1/main.cpp
+++ lysenko.aleksandra/B1/B1/B1/main.cpp
@@ -1,12 +1,17 @@
 #include <stdexcept>
+#include <cstring>
+#include <iostream>
 
-bool correctNumberOfArgs(int argc);///проверяем что argc в диапазоне от 2 до 4, если нет выбрасываем исключение
-bool correctData(char* argv);///проверяем не содержит ли строка в себе пробелов
-bool isTypeOfSort(char* argv);///проверяем на ascending/descending и возвращаем 1 в случае совпадения, в противном случае возвращаем 0
+namespace lysenko
+{
+  bool correctNumberOfArgs(int argc);///проверяем что argc в диапазоне от 2 до 4, если нет выбрасываем исключение
+  bool correctData(char* argv);///проверяем не содержит ли строка в себе пробелов
+  bool isTypeOfSort(char* argv);///проверяем на ascending/descending и возвращаем 1 в случае совпадения, в противном случае возвращаем 0
+}
 
 int main(int argc, char* argv[])
 {
-  if (correctNumberOfArgs(argc))
+  if (lysenko::correctNumberOfArgs(argc))
   {
     if (argc == 2)
     {
@@ -14,10 +19,11 @@ int main(int argc, char* argv[])
     }
     else if (argc == 3)
     {
-      if (correctData(argv[1])) 
+      if (lysenko::correctData(argv[1])) 
       {
-        if (isTypeOfSort(argv[2]))
+        if (lysenko::isTypeOfSort(argv[2]))
         {
+          std::cout << "Yohooo";
           ///вызов 1 задания
         }
         else
@@ -35,6 +41,42 @@ int main(int argc, char* argv[])
   else
   {
     throw std::invalid_argument("Invalid number of arguments");
+  }
+  return 0;
+}
+
+bool lysenko::correctNumberOfArgs(int argc)
+{
+  if ((argc > 1) && (argc < 5))
+  {
+    return 1;
+  }
+  return 0;
+}
+
+bool lysenko::correctData(char* argv)
+{
+  size_t i = 0;
+  while (argv[i] != '\0')
+  {
+    if (argv[i] == ' ')
+    {
+      return 0;
+    }
+    i += 1;
+  }
+  return 1;
+}
+
+bool lysenko::isTypeOfSort(char* argv)
+{
+  if (strcmp(argv, "ascending") == 0)
+  {
+    return 1;
+  }
+  else if (strcmp(argv, "descending") == 0)
+  {
+    return 1;
   }
   return 0;
 }

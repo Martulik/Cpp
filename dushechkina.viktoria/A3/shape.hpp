2a5cfdd6c861c9edfc6f351cf4bb18a143d9f9b3
viktoria.dushechkina <byzaya@icloud.com>
2021-04-13 18:48:12 +0300

    fix2 shape whitespaces

--- dushechkina.viktoria/A3/shape.hpp
+++ dushechkina.viktoria/A3/shape.hpp
@@ -6,22 +6,22 @@
 
 namespace dushechkina
 {
- class Shape
- {
- public:
-   virtual ~Shape() = default;
-   virtual double getArea() const = 0;
-   virtual rectangle_t getFrameRect() const = 0;
-   virtual void move(const point_t& pos) = 0;
-   virtual void move(double dx, double dy) = 0;
-   void scale(double ratio);
-   virtual std::shared_ptr< Shape > clone() const = 0;
- private:
-   virtual void doScale(double ratio) = 0;
- };
- double getX(const Shape& shape);
- double getY(const Shape& shape);
- double getWidth(const Shape& shape);
- double getHeight(const Shape& shape);
+  class Shape
+  {
+  public:
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(const point_t& pos) = 0;
+    virtual void move(double dx, double dy) = 0;
+    void scale(double ratio);
+    virtual std::shared_ptr< Shape > clone() const = 0;
+  private:
+    virtual void doScale(double ratio) = 0;
+  };
+  double getX(const Shape& shape);
+  double getY(const Shape& shape);
+  double getWidth(const Shape& shape);
+  double getHeight(const Shape& shape);
 }
 #endif

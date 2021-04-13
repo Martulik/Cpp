a37c824985e0ed4d631296a7f1a569ecd4b3cd85
vladislav-pochernin <vspochernin@gmail.com>
2021-04-13 13:23:20 +0300

    End task4

--- pochernin.vladislav/B1/task4.cpp
+++ pochernin.vladislav/B1/task4.cpp
@@ -3,9 +3,15 @@
 #include <vector>
 
 #include "functions.hpp"
+#include "accesses.hpp"
 
 void pochernin::task4(const char* sortingDirection, size_t size)
 {
   bool isAscending = pochernin::isAscending(sortingDirection);
   std::vector< double > vec(size);
+  pochernin::fillRandom(vec.data(), size);
+
+  pochernin::print(vec, " ");
+  pochernin::sort< atAccess< double > >(vec, isAscending);
+  pochernin::print(vec, " ");
 }

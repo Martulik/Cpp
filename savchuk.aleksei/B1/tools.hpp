8c9819c72960810ec6a6468047ef73f1d83de32b
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-04-14 23:58:59 +0300

    Add tool function getCompare()

--- savchuk.aleksei/B1/tools.hpp
+++ savchuk.aleksei/B1/tools.hpp
@@ -2,10 +2,24 @@
 #define TOOLS_HPP
 
 #include <string>
+#include <functional>
 
 namespace savchuk
 {
   int convertToNumber(const std::string& order);
+  template< typename T >
+  std::function< bool(const T&, const T&) > getCompare(const std::string& order)
+  {
+    if (order == "ascending")
+    {
+      return std::less< T >();
+    }
+    if (order == "descending")
+    {
+      return std::greater< T >();
+    }
+    throw std::invalid_argument("invalid sort order");
+  }
 }
 
 #endif

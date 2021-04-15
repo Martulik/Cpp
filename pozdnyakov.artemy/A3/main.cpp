a17c8dbd806f0adb05aae38ec5dc29c50da35b30
pozdnyakov-artemy <aapozdnyakove@gmail.com>
2021-04-16 00:39:48 +0300

    make_unique instead of new

--- pozdnyakov.artemy/A3/main.cpp
+++ pozdnyakov.artemy/A3/main.cpp
@@ -10,11 +10,11 @@ int main()
   double rad = 10;
   double width = 20;
   double height = 10;
-  pozdnyakov::UniqueShape circle(new pozdnyakov::Circle(rad, o));
-  pozdnyakov::UniqueShape rect(new pozdnyakov::Rectangle(width, height, o));
-  pozdnyakov::UniqueShapes shapes(new pozdnyakov::UniqueShape[2]);
+  pozdnyakov::UniqueShape circle = std::make_unique<pozdnyakov::Circle>(rad, o);
+  pozdnyakov::UniqueShape rect = std::make_unique<pozdnyakov::Rectangle>(width, height, o);
+  pozdnyakov::UniqueShapes shapes = std::make_unique<pozdnyakov::UniqueShape[]>(2);
   shapes[0] = std::move(circle);
-  shapes.get[1] = std::move(rect);
+  shapes[1] = std::move(rect);
   std::cout << shapes[0]->getArea() << '\n';
   pozdnyakov::CompositeShape cshape(std::move(shapes), 2);
   std::cout << "The area of composite shape is " << cshape.getArea() << '\n';

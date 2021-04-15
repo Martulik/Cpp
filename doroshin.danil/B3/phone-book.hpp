febf5f8e009f47d75641750a2897897c1948dc66
Danil Doroshin <ddddanil5555@gmail.com>
2021-04-15 20:22:09 +0300

    Clean up the phone book spec

--- doroshin.danil/B3/phone-book.hpp
+++ doroshin.danil/B3/phone-book.hpp
@@ -9,6 +9,16 @@
 
 namespace doroshin
 {
+  /* # About the 'current' bookmark
+    The current bookmark is added on initialisation and provides access to
+    the entries for immediate use.
+
+    Upon initialisation it does not point to a valid entry (since there are none)
+    and referencing it is illegal
+
+    After any command that creates a new entry (add, insert before, insert after)
+    it points to the newly created entry
+  */
   class PhoneBook
   {
   public:
@@ -20,20 +30,19 @@ namespace doroshin
 
     void add(Entry);
     void store(const Name& from, const Name& to);
+    void delete_contents(const Name& mark);
     void insert_before(const Name& mark, Entry);
     void insert_after(const Name& mark, Entry);
-    void delete_contents(const Name& mark);
-    Entry show(const Name& mark) const;
+
     void move_front(const Name& mark);
     void move_back(const Name& mark);
     void move(const Name& mark, int steps);
+
+    Entry show(const Name& mark) const;
   private:
     using Iter = typename std::list< Entry >::iterator;
-    // using Bookmark = std::pair< Name, Iter >;
     std::list< Entry > entries_;
     std::map< Name, Iter > bookmarks_;
-
-    // Iter getMark(const Name&);
   };
 }
 

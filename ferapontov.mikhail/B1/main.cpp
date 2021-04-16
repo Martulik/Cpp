379bec569d1c9382513cb9985752317b46798c5f
ferapontov-mikhail <mferapon@gmail.com>
2021-04-16 21:48:28 +0300

    Added reading of arguments

--- ferapontov.mikhail/B1/main.cpp
+++ ferapontov.mikhail/B1/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <sstream>
+
+template< typename T >
+T readArg(std::string arg)
+{
+  T res;
+  std::stringstream in(arg);
+  in >> res;
+  return res;
+}
 
 int main(int argc, char *argv[])
 {
+  std::cout << argc  << '\n';
+  int TaskNumber = readArg< int >(argv[1]);
+  std::string way = readArg< std::string >(argv[2]);
+  std::cout << TaskNumber << ' '<< way << '\n';
   return 0;
 }

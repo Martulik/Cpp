fc57ab3dba26e2646881bc0635e24c695a0a8660
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-04-18 00:53:48 +0300

    Change the solution for the task 4

--- savchuk.aleksei/B1/task-4.cpp
+++ savchuk.aleksei/B1/task-4.cpp
@@ -14,12 +14,8 @@ void lab::doTask4(const char* order, const char* size)
 {
   std::function< bool(const int&, const int&) > cmp = getCompare< int >(order);
   size_t sz = convertToNumber(size);
-  if (sz == 0)
-  {
-    throw std::invalid_argument("size can't be zero");
-  }
   std::vector< double > vec(sz);
   fillRandom(vec.data(), sz);
   selectionSort< double, IndexVectorPolicy >(vec, cmp);
-  print(vec.cbegin(), vec.cend(), std::cout);
+  print(vec.cbegin(), vec.cend(), std::cout, " ");
 }

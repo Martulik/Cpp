6ee5017c680175b503b90b2e3a1b35178141e0da
Danil Doroshin <ddddanil5555@gmail.com>
2021-04-17 16:44:26 +0300

    Change task switch to if chain

--- doroshin.danil/B1/main.cpp
+++ doroshin.danil/B1/main.cpp
@@ -64,49 +64,41 @@ int main(int argc, char* argv[])
       throw std::runtime_error("No task number");
     }
     const int task_num = readArg< int >(argv[1]);
-    switch (task_num)
-    {
-    case 1:
-      {
-        if(argc < 3) {
-          throw std::runtime_error("No sorting order");
-        }
-
-        const std::function< bool(int, int) > cmp = getOrder< int >(argv[2]);
-        const std::vector< int > values = readUntilEof< int >();
-        const std::forward_list< int > l_values(values.begin(), values.end());
-
-        dan::doSort< dan::VectorIndexStrat< int > >(values, cmp);
-        dan::doSort< dan::VectorAtStrat< int > >(values, cmp);
-        dan::doSort< dan::ListIterStrat< int > >(l_values, cmp);
+    if(task_num == 1) {
+      if(argc < 3) {
+        throw std::runtime_error("No sorting order");
       }
-      break;
-    case 2:
-      {
-        if(argc < 3) {
-          throw std::runtime_error("No filename");
-        }
-        std::string filename = argv[2];
-        dan::readFile(filename);
+
+      const std::function< bool(int, int) > cmp = getOrder< int >(argv[2]);
+      const std::vector< int > values = readUntilEof< int >();
+      const std::forward_list< int > l_values(values.begin(), values.end());
+
+      dan::doSort< dan::VectorIndexStrat< int > >(values, cmp);
+      dan::doSort< dan::VectorAtStrat< int > >(values, cmp);
+      dan::doSort< dan::ListIterStrat< int > >(l_values, cmp);
+    }
+    else if(task_num == 2) {
+      if(argc < 3) {
+        throw std::runtime_error("No filename");
       }
-      break;
-    case 3:
+      std::string filename = argv[2];
+      dan::readFile(filename);
+    }
+    else if(task_num == 3) {
       dan::filterInput();
-      break;
-    case 4:
-      {
-        if(argc < 3) {
-          throw std::runtime_error("No sorting order");
-        }
-        if(argc < 4) {
-          throw std::runtime_error("No array length");
-        }
-        const size_t size = readArg< size_t >(argv[3]);
-        std::function< bool(double, double) > cmp = getOrder< double >(argv[2]);
-        dan::testRandom(size, cmp);
+    }
+    else if(task_num == 4) {
+      if(argc < 3) {
+        throw std::runtime_error("No sorting order");
       }
-      break;
-    default:
+      if(argc < 4) {
+        throw std::runtime_error("No array length");
+      }
+      const size_t size = readArg< size_t >(argv[3]);
+      std::function< bool(double, double) > cmp = getOrder< double >(argv[2]);
+      dan::testRandom(size, cmp);
+    }
+    else {
       throw std::runtime_error("Unknown task");
     }
   }

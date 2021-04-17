5c774baf822650d20328f8cced374a1582574258
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-04-18 01:14:43 +0300

    Add tests for function selectionSort()

---
+++ savchuk.aleksei/B1/test-utility.hpp
@@ -0,0 +1,55 @@
+#ifndef TEST_UTILITY_HPP
+#define TEST_UTILITY_HPP
+
+#include <random>
+#include <limits>
+#include <algorithm>
+#include <functional>
+#include <forward_list>
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/tools/floating_point_comparison.hpp>
+
+#include "access-policies.hpp"
+#include "sort.hpp"
+
+namespace savchuk
+{
+  template< typename Iterator >
+  void fillContainer(Iterator first, Iterator last)
+  {
+    using T = typename Iterator::value_type;
+    const T MIN_INT = std::numeric_limits< int >::min();
+    const T MAX_INT = std::numeric_limits< int >::max();
+    std::random_device rd;
+    std::uniform_int_distribution< T > dist(MIN_INT, MAX_INT);
+    while (first != last)
+    {
+      *first = dist(rd);
+      ++first;
+    }
+  }
+  template< typename Container >
+  Container getTestContainer(size_t size)
+  {
+    Container cnt(size);
+    fillContainer(cnt.begin(), cnt.end());
+    return cnt;
+  }
+  template< typename Compare >
+  void testSort(size_t size, Compare cmp)
+  {
+    std::vector< int > v1 = getTestContainer< std::vector< int > >(size);
+    std::vector< int > v2 = getTestContainer< std::vector< int > >(size);
+    std::forward_list< int > fwl = getTestContainer< std::forward_list< int > >(size);
+    selectionSort< int, IndexVectorPolicy >(v1, cmp);
+    selectionSort< int, AtVectorPolicy >(v2, cmp);
+    selectionSort< int, IteratorFwListPolicy >(fwl, cmp);
+    BOOST_CHECK(std::is_sorted(v1.cbegin(), v1.cend(), cmp));
+    BOOST_CHECK(std::is_sorted(v2.cbegin(), v2.cend(), cmp));
+    BOOST_CHECK(std::is_sorted(fwl.cbegin(), fwl.cend(), cmp));
+  }
+}
+
+#endif

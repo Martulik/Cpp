80d1f94a8d307c8d45195266495e5dfc88a67805
Shkurov Igor <lilthewil@gmail.com>
2021-04-19 01:06:19 +0300

    still draft

--- shkurov.igor/B1/task1.cpp
+++ shkurov.igor/B1/task1.cpp
@@ -1,6 +1,14 @@
+#include "tasks.hpp"
 #include "functions.hpp"
+#include "strategies.hpp"
 
-void taskOne(char* cmp)
+namespace lab = shkurov;
+
+void taskOne(const char* cmp)
 {
-  
+  std::vector< int > vecOne {9, 4, 5, 12, 1, 3};
+  std::vector< int > vecTwo;
+  std::forward_list< int > list;
+
+  selectionSort< lab::strategyIndex< int > >(vecOne, comparsionMethod< int >(cmp));
 }

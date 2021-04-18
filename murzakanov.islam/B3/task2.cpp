c88c67ab5fd17fe36eed3099373ede17b8c97755
Islam <murzakanovislam21@gmail.com>
2021-04-18 18:11:29 +0300

    delete unused parametr

--- murzakanov.islam/B3/task2.cpp
+++ murzakanov.islam/B3/task2.cpp
@@ -10,7 +10,6 @@
 void murzakanov::task2()
 {
   murzakanov::FactorialContainer factorial;
-  auto it = factorial.begin();
   std::copy(factorial.begin(), factorial.end(), std::ostream_iterator< size_t >(std::cout, " "));
   std::cout << "\n";
   std::reverse_copy(factorial.begin(), factorial.end(), std::ostream_iterator< size_t >(std::cout, " "));

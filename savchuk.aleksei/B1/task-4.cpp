066ca13846b484d402b811125a78a6e31237b92a
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-04-18 11:47:44 +0300

    Fix the solution for the task 4

--- savchuk.aleksei/B1/task-4.cpp
+++ savchuk.aleksei/B1/task-4.cpp
@@ -12,10 +12,11 @@ namespace lab = savchuk;
 
 void lab::doTask4(const char* order, const char* size)
 {
-  std::function< bool(const int&, const int&) > cmp = getCompare< int >(order);
+  std::function< bool(const double&, const double&) > cmp = getCompare< double >(order);
   size_t sz = convertToNumber(size);
   std::vector< double > vec(sz);
   fillRandom(vec.data(), sz);
+  print(vec.cbegin(), vec.cend(), std::cout, " ");
   selectionSort< double, IndexVectorPolicy >(vec, cmp);
   print(vec.cbegin(), vec.cend(), std::cout, " ");
 }

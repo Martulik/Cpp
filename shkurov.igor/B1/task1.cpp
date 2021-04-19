6500fcdaaeee648b181200db8fd1bf118dcf9953
Shkurov Igor <lilthewil@gmail.com>
2021-04-19 23:13:34 +0300

    task1 finished

--- shkurov.igor/B1/task1.cpp
+++ shkurov.igor/B1/task1.cpp
@@ -4,11 +4,17 @@
 
 namespace lab = shkurov;
 
-void taskOne(const char* cmp)
+void lab::taskOne(const char* cmp)
 {
   std::vector< int > vecOne {9, 4, 5, 12, 1, 3};
-  std::vector< int > vecTwo;
-  std::forward_list< int > list;
+  std::vector< int > vecTwo(vecOne);
+  std::forward_list< int > list(vecOne.begin(), vecOne.end());
 
-  selectionSort< lab::strategyIndex< int > >(vecOne, comparsionMethod< int >(cmp));
+  selectionSort< strategyIndex< int > >(vecOne, comparsionMethod< int >(cmp));
+  selectionSort< strategyAt< int > >(vecTwo, comparsionMethod< int >(cmp));
+  selectionSort< strategyIterator< int > >(list, comparsionMethod< int >(cmp));
+
+  printContainer(vecOne, ' ');
+  printContainer(vecTwo, ' ');
+  printContainer(list, ' ');
 }

b7949e6ab009cebc96a2a252a9460c2e1e80a036
ferapontov-mikhail <mferapon@gmail.com>
2021-04-24 20:52:17 +0300

    Added unit tests

---
+++ ferapontov.mikhail/B1/test-functions.hpp
@@ -0,0 +1,42 @@
+#ifndef TEST_FUNCTIONS_HPP
+#define TEST_FUNCTIONS_HPP
+
+#include <boost/test/unit_test.hpp>
+
+#include <algorithm>
+#include <random>
+#include <functional>
+#include "functions.hpp"
+#include "sort.hpp"
+#include "strategy.hpp"
+namespace ferapontov
+{
+  template <typename iter>
+  void fillVector(iter begin, iter end)
+  {
+    std::random_device rd;
+    std::uniform_int_distribution< int > dist(-100, 100);
+    while(begin != end)
+    {
+      *begin = dist(rd);
+      ++begin;
+    }
+  }
+
+  void testSort(std::function< bool(int, int) > cmp, size_t size)
+  {
+    std::vector< int > v(size);
+    fillVector(v.begin(), v.end());
+
+    std::vector< int > v2(v);
+    std::forward_list< int > fl(v.begin(), v.end());
+    sort< int, index_access< int > >(v, cmp);
+    sort< int, at_access< int > >(v2, cmp);
+    sort< int, iterator_access< int > >(fl, cmp);
+    BOOST_CHECK(!(std::is_sorted(v.begin(), v.end(), cmp)));
+    BOOST_CHECK(!(std::is_sorted(v2.begin(), v2.end(), cmp)));
+    BOOST_CHECK(!(std::is_sorted(fl.begin(), fl.end(), cmp)));
+  }
+}
+
+#endif
---
+++ ferapontov.mikhail/B1/test-sort.cpp
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+
+#include "test-functions.hpp"
+
+BOOST_AUTO_TEST_SUITE(testSort)
+
+BOOST_AUTO_TEST_CASE(sort)
+{
+  ferapontov::testSort(std::less< int >(), 5);
+  ferapontov::testSort(std::less< int >(), 10);
+  ferapontov::testSort(std::greater< int >(), 5);
+  ferapontov::testSort(std::greater< int >(), 10);
+}
+
+BOOST_AUTO_TEST_SUITE_END()

1356b910d0ae36f61316d9313c53dd23b1be2f1c
ferapontov-mikhail <mferapon@gmail.com>
2021-04-24 16:45:24 +0300

    Fixed task4 for the third time

--- ferapontov.mikhail/B1/task4.cpp
+++ ferapontov.mikhail/B1/task4.cpp
@@ -9,15 +9,14 @@
 void ferapontov::task4(const std::string& mode, int size)
 {
   std::function< bool(double, double) > cmp = getSortMode< double >(mode);
-  std::vector< double > vec;
-  for (int i = 0; i < size + 1; i++)
+  std::random_device rd;
+  std::uniform_int_distribution< > dist(-10, 10);
+  std::vector< double > vec(size);
+  for (int i = 0; i < size; i++)
   {
-    std::random_device rd;
-    std::uniform_real_distribution< double > dist(-1.0, 1.0);
-    vec.push_back(dist(rd));
+    vec[i] = dist(rd) / 10.0;
   }
 
-  std::cout << std::setprecision(1);
   print(vec);
   ferapontov::sort< double, ferapontov::at_access< double > >(vec, cmp);
   print(vec);

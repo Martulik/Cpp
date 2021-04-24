49b38f5715ee2f397446171acd21d7ba6b9be68a
Danil Doroshin <ddddanil5555@gmail.com>
2021-04-24 16:55:01 +0300

    Migrate to C style arg parsing

--- doroshin.danil/B1/main.cpp
+++ doroshin.danil/B1/main.cpp
@@ -1,41 +1,13 @@
+#include <algorithm>
 #include <iostream>
 #include <stdexcept>
+#include <cstring>
 #include "task1.hpp"
 #include "task2.hpp"
 #include "task3.hpp"
 #include "task4.hpp"
 
 namespace dan = doroshin;
-
-template< typename T >
-std::function< bool(T, T) > getOrder(std::string order)
-{
-  if(order == "ascending") {
-    return std::less< T >();
-  }
-  else if(order == "descending") {
-    return std::greater< T >();
-  }
-  else {
-    throw std::runtime_error("Invalid sorting order");
-  }
-}
-
-template< typename T >
-T readArg(std::string arg)
-{
-  T res;
-  std::istringstream in (arg);
-  in >> res;
-  if(in.fail()) {
-    throw std::runtime_error("Invalid read");
-  }
-  in.get();
-  if(!in.eof()) {
-    throw std::runtime_error("Extra symbols");
-  }
-  return res;
-}
 
 template< typename T >
 std::vector< T > readUntilEof()
@@ -63,26 +35,37 @@ int main(int argc, char* argv[])
     if(argc < 2) {
       throw std::runtime_error("No task number");
     }
-    const int task_num = readArg< int >(argv[1]);
+    if(!std::all_of(argv[1], argv[1] + std::strlen(argv[1]), isdigit)) {
+      throw std::runtime_error("Task number is invalid");
+    }
+    const int task_num = std::atoi(argv[1]);
     if(task_num == 1) {
       if(argc < 3) {
         throw std::runtime_error("No sorting order");
       }
 
-      const std::function< bool(int, int) > cmp = getOrder< int >(argv[2]);
       const std::vector< int > values = readUntilEof< int >();
       const std::forward_list< int > l_values(values.begin(), values.end());
 
-      dan::task1< dan::VectorIndexStrat< int > >(values, cmp);
-      dan::task1< dan::VectorAtStrat< int > >(values, cmp);
-      dan::task1< dan::ListIterStrat< int > >(l_values, cmp);
+      if(!std::strcmp(argv[2], "ascending")) {
+        dan::task1< dan::VectorIndexStrat< int > >(values, std::less<>());
+        dan::task1< dan::VectorAtStrat< int > >(values, std::less<>());
+        dan::task1< dan::ListIterStrat< int > >(l_values, std::less<>());
+      }
+      else if(!std::strcmp(argv[2], "descending")) {
+        dan::task1< dan::VectorIndexStrat< int > >(values, std::greater<>());
+        dan::task1< dan::VectorAtStrat< int > >(values, std::greater<>());
+        dan::task1< dan::ListIterStrat< int > >(l_values, std::greater<>());
+      }
+      else {
+        throw std::runtime_error("Invalid sorting order");
+      }
     }
     else if(task_num == 2) {
       if(argc < 3) {
         throw std::runtime_error("No filename");
       }
-      std::string filename = argv[2];
-      dan::task2(filename);
+      dan::task2(argv[2]);
     }
     else if(task_num == 3) {
       dan::task3();
@@ -94,9 +77,19 @@ int main(int argc, char* argv[])
       if(argc < 4) {
         throw std::runtime_error("No array length");
       }
-      const size_t size = readArg< size_t >(argv[3]);
-      std::function< bool(double, double) > cmp = getOrder< double >(argv[2]);
-      dan::task4(size, cmp);
+      if(!std::all_of(argv[3], argv[3] + std::strlen(argv[3]), isdigit)) {
+        throw std::runtime_error("Size is invalid");
+      }
+      const size_t size = atoi(argv[3]);
+      if(!std::strcmp(argv[2], "ascending")) {
+        dan::task4(size, std::less<>());
+      }
+      else if(!std::strcmp(argv[2], "descending")) {
+        dan::task4(size, std::greater<>());
+      }
+      else {
+        throw std::runtime_error("Invalid sorting order");
+      }
     }
     else {
       throw std::runtime_error("Unknown task");

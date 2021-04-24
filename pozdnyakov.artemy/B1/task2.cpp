92af9f9a1f5d6bb9cf2c4d8bad0c896458e0157d
pozdnyakov-artemy <aapozdnyakove@gmail.com>
2021-04-24 21:56:43 +0300

    Acceptance fix 2

--- pozdnyakov.artemy/B1/task2.cpp
+++ pozdnyakov.artemy/B1/task2.cpp
@@ -1,6 +1,7 @@
 #include "tasks.hpp"
 #include <string>
 #include <fstream>
+#include <stdexcept>
 #include "print.hpp"
 
 namespace poz = pozdnyakov;
@@ -10,22 +11,23 @@ void poz::task2(char* argv[])
   std::string fileName(argv[0]);
   std::ifstream fs;
   fs.open(fileName, std::ifstream::in);
-  if (fs)
+  if (!fs.is_open())
   {
-    fs.seekg(0, fs.end);
-    size_t length = fs.tellg();
-    fs.seekg(0, fs.beg);
-    char* buf = new char[length]; //make unique
-    size_t read = 0;
-    char c;
-    while (!fs.eof())
-    {
-      fs.get(c);
-      buf[read] = c;
-      read += fs.gcount();
-    }
-    std::vector<char> vector(&buf[0], &buf[length]);
-    poz::print(vector, std::cout);
+    throw std::invalid_argument("Incorrect file");
   }
+  fs.seekg(0, fs.end);
+  size_t length = fs.tellg();
+  fs.seekg(0, fs.beg);
+  char* buf = new char[length]; //make unique
+  size_t read = 0;
+  char c;
+  while (!fs.eof())
+  {
+    fs.get(c);
+    buf[read] = c;
+    read += fs.gcount();
+  }
+  std::vector<char> vector(&buf[0], &buf[length]);
+  poz::print(vector, std::cout);
   fs.close();
 }

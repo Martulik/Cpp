6e3a04b69d50a6c6bbfad94bbd3d3e267bc59064
viktoria.dushechkina <byzaya@icloud.com>
2021-04-25 20:10:05 +0300

    fix whitespace

--- dushechkina.viktoria/B1/test-sort.cpp
+++ dushechkina.viktoria/B1/test-sort.cpp
@@ -13,51 +13,51 @@ BOOST_AUTO_TEST_SUITE(testSort)
 const auto lessComparator = dushechkina::getSortingOrder< int >("ascending");
 const auto greaterComparator = dushechkina::getSortingOrder< int >("descending");
 
-struct fixture
-{
-  fixture():
-	  vector({ 4, 8, 2, 7, -13 }),
-		list({ 4, 8, 2, 7, -13 }),
-		sortedAscendingVector(vector),
-		sortedAscendingList(list)
-	{
-		std::sort(sortedAscendingVector.begin(), sortedAscendingVector.end());
-		sortedDescendingVector = sortedAscendingVector;
-		std::reverse(sortedDescendingVector.begin(), sortedDescendingVector.end());
-		sortedAscendingList.sort();
-		sortedDescendingList = sortedAscendingList;
-		sortedDescendingList.reverse();
-	}
-	std::vector< int > vector;
-	std::list< int > list;
-	std::vector< int > sortedAscendingVector;
-	std::list< int > sortedAscendingList;
-	std::vector< int > sortedDescendingVector;
-	std::list< int > sortedDescendingList;
-};
+  struct fixture
+  {
+	  std::vector< int > vector;
+	  std::list< int > list;
+	  std::vector< int > sortedAscendingVector;
+	  std::list< int > sortedAscendingList;
+	  std::vector< int > sortedDescendingVector;
+	  std::list< int > sortedDescendingList;
 
-BOOST_FIXTURE_TEST_CASE(sortBracketsType, fixture)
-{
-  dushechkina::sort< dushechkina::Brackets >(vector, lessComparator);
-	BOOST_CHECK(vector == sortedAscendingVector);
-	dushechkina::sort< dushechkina::Brackets >(vector, greaterComparator);
-	BOOST_CHECK(vector == sortedDescendingVector);
-}
+    fixture():
+	    vector({ 4, 8, 2, 7, -13 }),
+		  list({ 4, 8, 2, 7, -13 }),
+		  sortedAscendingVector(vector),
+		  sortedAscendingList(list)
+	  {
+		  std::sort(sortedAscendingVector.begin(), sortedAscendingVector.end());
+		  sortedDescendingVector = sortedAscendingVector;
+		  std::reverse(sortedDescendingVector.begin(), sortedDescendingVector.end());
+		  sortedAscendingList.sort();
+		  sortedDescendingList = sortedAscendingList;
+		  sortedDescendingList.reverse();
+	  }
+  };
 
-BOOST_FIXTURE_TEST_CASE(sortAtType, fixture)
-{
-	dushechkina::sort< dushechkina::At >(vector, lessComparator);
-	BOOST_CHECK(vector == sortedAscendingVector);
-	dushechkina::sort< dushechkina::At >(vector, greaterComparator);
-	BOOST_CHECK(vector == sortedDescendingVector);
-}
+  BOOST_FIXTURE_TEST_CASE(sortBracketsType, fixture)
+  {
+    dushechkina::sort< dushechkina::Brackets >(vector, lessComparator);
+	  BOOST_CHECK(vector == sortedAscendingVector);
+	  dushechkina::sort< dushechkina::Brackets >(vector, greaterComparator);
+	  BOOST_CHECK(vector == sortedDescendingVector);
+  }
 
-BOOST_FIXTURE_TEST_CASE(sortIteratorType, fixture)
-{
-	dushechkina::sort< dushechkina::Iterator >(list, lessComparator);
-	BOOST_CHECK(list == sortedAscendingList);
-	dushechkina::sort< dushechkina::Iterator >(list, greaterComparator);
-	BOOST_CHECK(list == sortedDescendingList);
-}
+  BOOST_FIXTURE_TEST_CASE(sortAtType, fixture)
+  {
+	  dushechkina::sort< dushechkina::At >(vector, lessComparator);
+	  BOOST_CHECK(vector == sortedAscendingVector);
+	  dushechkina::sort< dushechkina::At >(vector, greaterComparator);
+	  BOOST_CHECK(vector == sortedDescendingVector);
+  }
 
+  BOOST_FIXTURE_TEST_CASE(sortIteratorType, fixture)
+  {
+	  dushechkina::sort< dushechkina::Iterator >(list, lessComparator);
+	  BOOST_CHECK(list == sortedAscendingList);
+	  dushechkina::sort< dushechkina::Iterator >(list, greaterComparator);
+	  BOOST_CHECK(list == sortedDescendingList);
+  }
 BOOST_AUTO_TEST_SUITE_END()

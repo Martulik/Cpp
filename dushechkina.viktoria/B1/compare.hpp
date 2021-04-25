b11966037652f96757ec780b92221e2b57fd96ea
viktoria.dushechkina <byzaya@icloud.com>
2021-04-25 17:20:14 +0300

    add const into compare and add whitespace into main

--- dushechkina.viktoria/B1/compare.hpp
+++ dushechkina.viktoria/B1/compare.hpp
@@ -11,8 +11,8 @@ namespace dushechkina
   template<class T>
   std::function<bool(const T, const T)> getSortingOrder(const char* order)
   {
-    std::string ascending = "ascending";
-    std::string descending = "descending";
+    const std::string ascending = "ascending";
+    const std::string descending = "descending";
     if (order == nullptr)
     {
       throw std::invalid_argument("Sorting order is undefined");

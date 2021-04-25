c3a2551ac1ffc19c48ef5b9765a54d4511c1ceef
Islam <murzakanovislam21@gmail.com>
2021-04-25 14:46:21 +0300

    Some changes in phone book.cpp

---
+++ murzakanov.islam/B3/phone-book.cpp
@@ -0,0 +1,17 @@
+#include "phone-book.hpp"
+
+
+void murzakanov::insertAfter(iterator it, const note_t& note)
+{
+
+}
+
+void murzakanov::insertBefore(iterator it, const note_t& note)
+{
+
+}
+
+void murzakanov::add(const note_t& note)
+{
+
+}

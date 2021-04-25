104131f63cd9ebd4da33086a8d3fe01efeecc438
LysenkoAleksandra <orange260302@gmail.com>
2021-04-25 20:52:20 +0300

    Fix comparator.hpp

--- lysenko.aleksandra/B1/comparator.hpp
+++ lysenko.aleksandra/B1/comparator.hpp
@@ -2,32 +2,34 @@
 #define COMPARATOR_HPP
 
 #include <cstring>
-
-template<typename T>
-bool cmpAscending(const T& a, const T& b)
+namespace lysenko
 {
-  return a < b;
-}
-
-template<typename T>
-bool cmpDescending(const T& a, const T& b)
-{
-  return a > b;
-}
-
-template<typename T>
-bool (*getComparator(const char* order))(const T&, const T&)
-{
-  bool (*comparator)(const T&, const T&) = nullptr;
-  if (strcmp(order, "ascending") == 0)
+  template<typename T>
+  bool cmpAscending(const T& a, const T& b)
   {
-    comparator = cmpAscending<T>;
+    return a < b;
   }
-  else if (strcmp(order, "descending") == 0)
+
+  template<typename T>
+  bool cmpDescending(const T& a, const T& b)
   {
-    comparator = cmpDescending<T>;
+    return a > b;
   }
-  return comparator;
+
+  template<typename T>
+  bool (*getComparator(const char* order))(const T&, const T&)
+  {
+    bool (*comparator)(const T&, const T&) = nullptr;
+    if (strcmp(order, "ascending") == 0)
+    {
+      comparator = cmpAscending<T>;
+    }
+    else if (strcmp(order, "descending") == 0)
+    {
+      comparator = cmpDescending<T>;
+    }
+    return comparator;
+  }
 }
 
 #endif

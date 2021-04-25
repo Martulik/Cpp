77a6f151a1657bfd825fb8a44c67ded74045820e
Islam <murzakanovislam21@gmail.com>
2021-04-25 15:01:12 +0300

    Delete extra flag in main

--- murzakanov.islam/B1/main.cpp
+++ murzakanov.islam/B1/main.cpp
@@ -18,35 +18,34 @@ int main(int argc, char* argv[])
 {
   namespace mur = murzakanov;
   srand(time(0));
-  bool inArg = true;
 
   if (argc < 2) {
-    inArg = false;
+    code = 1;
   }
 
   int exNum = 0;
-  if (inArg)
+  if (!code)
   {
     exNum = int(argv[1][0] - '0');
     std::string firstArg = argv[1];
     if (mur::checkForSpaces(firstArg))
     {
-      inArg = false;
+      code = 1;
     }
   }
 
-  if (inArg && exNum == 1 && argc == 3) {
+  if (!code && exNum == 1 && argc == 3) {
     std::string order = argv[2];
     code = mur::task1(order);
   }
-  else if (inArg && exNum == 2 && argc == 3) {
+  else if (!code && exNum == 2 && argc == 3) {
     std::string name = argv[2];
     code = mur::task2(name);
   }
-  else if (inArg && exNum == 3 && argc == 2) {
+  else if (!code && exNum == 3 && argc == 2) {
     code = mur::task3();
   }
-  else if (inArg && exNum == 4 && argc == 4) {
+  else if (!code && exNum == 4 && argc == 4) {
     std::string order = argv[2];
     std::string size = argv[3];
     if (mur::checkIsNumber(size))

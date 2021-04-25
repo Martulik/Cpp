6c1744d2f48fe4e42425e7e51ec71e7912e495f2
Islam <murzakanovislam21@gmail.com>
2021-04-25 15:32:44 +0300

    Fix test sort

--- murzakanov.islam/B1/test-sort.cpp
+++ murzakanov.islam/B1/test-sort.cpp
@@ -8,48 +8,49 @@
 #include <string>
 #include <algorithm>
 #include <forward_list>
+#include <list>
+
 
 #include "sorting.hpp"
 #include "strategy.hpp"
 
-int containerSize = 3;
 namespace mur = murzakanov;
-template < typename T >
-void fillContainer (typename T::iterator it, const typename T::iterator& end)
+
+int randFunctional()
 {
-  while (it != end)
-  {
-    *it = rand();
-    it++;
-  }
+  return rand();
 }
 
-void doTestSort()
+void doTestSortOneElement()
 {
   std::string order = "ascending";
   std::function< bool(int, int) > cmp = mur::getSortMode< int >(order);
-  std::vector< int > vecBrackets(containerSize);
-  std::vector< int > vecInd(containerSize);
-  std::forward_list< int > listIt(containerSize);
+  std::function< int() > rand = randFunctional;
+  for (int containerSize = 0; containerSize < 4; containerSize++)
+  {
+    std::vector< int > vecBrackets(containerSize);
+    std::vector< int > vecInd(containerSize);
+    std::forward_list< int > listIt(containerSize);
 
-  fillContainer< std::vector< int > >(vecBrackets.begin(), vecBrackets.end());
-  fillContainer< std::vector< int > >(vecInd.begin(), vecInd.end());
-  fillContainer< std::forward_list< int > >(listIt.begin(), listIt.end());
+    std::generate(vecBrackets.begin(), vecBrackets.end(), rand);
+    std::generate(vecInd.begin(), vecInd.end(), rand);
+    std::generate(listIt.begin(), listIt.end(), rand);
 
-  mur::sort< mur::StrategyBrackets< int >, int >(vecBrackets, cmp);
-  mur::sort< mur::StrategyAt< int >, int >(vecInd, cmp);
-  mur::sort< mur::StrategyIter< int >, int >(listIt, cmp);
+    mur::sort< mur::StrategyBrackets< int >, int >(vecBrackets, cmp);
+    mur::sort< mur::StrategyAt< int >, int >(vecInd, cmp);
+    mur::sort< mur::StrategyIter< int >, int >(listIt, cmp);
 
-  BOOST_CHECK(std::is_sorted(vecBrackets.begin(), vecBrackets.end()));
-  BOOST_CHECK(std::is_sorted(vecInd.begin(), vecInd.end()));
-  BOOST_CHECK(std::is_sorted(listIt.begin(), listIt.end()));
+    BOOST_CHECK(std::is_sorted(vecBrackets.begin(), vecBrackets.end()));
+    BOOST_CHECK(std::is_sorted(vecInd.begin(), vecInd.end()));
+    BOOST_CHECK(std::is_sorted(listIt.begin(), listIt.end()));
+  }
 }
 
 BOOST_AUTO_TEST_SUITE(test_sort)
 
-  BOOST_AUTO_TEST_CASE(sort_vector_by_strategies)
+  BOOST_AUTO_TEST_CASE(sort_by_strategies)
   {
-    doTestSort();
+    doTestSortOneElement();
   }
 
 BOOST_AUTO_TEST_SUITE_END()

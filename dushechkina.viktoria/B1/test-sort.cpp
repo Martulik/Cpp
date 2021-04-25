8cafe71182ee8639d31f7360c09aa1cc23396954
viktoria.dushechkina <byzaya@icloud.com>
2021-04-25 21:09:09 +0300

    fix all

--- dushechkina.viktoria/B1/test-sort.cpp
+++ dushechkina.viktoria/B1/test-sort.cpp
@@ -39,28 +39,28 @@ struct fixture
 
 BOOST_AUTO_TEST_SUITE(testSort)
 
-BOOST_FIXTURE_TEST_CASE(sortBracketsType, fixture)
-{
-	vika::sort< vika::Brackets >(vector, lessComparator);
-	BOOST_CHECK(vector == sortedAscendingVector);
-	vika::sort< vika::Brackets >(vector, greaterComparator);
-	BOOST_CHECK(vector == sortedDescendingVector);
-}
+  BOOST_FIXTURE_TEST_CASE(sortBracketsType, fixture)
+  {
+	  vika::sort< vika::Brackets >(vector, lessComparator);
+	  BOOST_CHECK(vector == sortedAscendingVector);
+	  vika::sort< vika::Brackets >(vector, greaterComparator);
+	  BOOST_CHECK(vector == sortedDescendingVector);
+  }
 
-BOOST_FIXTURE_TEST_CASE(sortAtType, fixture)
-{
-	vika::sort< vika::At >(vector, lessComparator);
-	BOOST_CHECK(vector == sortedAscendingVector);
-	vika::sort< vika::At >(vector, greaterComparator);
-	BOOST_CHECK(vector == sortedDescendingVector);
-}
+  BOOST_FIXTURE_TEST_CASE(sortAtType, fixture)
+  {
+	  vika::sort< vika::At >(vector, lessComparator);
+	  BOOST_CHECK(vector == sortedAscendingVector);
+	  vika::sort< vika::At >(vector, greaterComparator);
+	  BOOST_CHECK(vector == sortedDescendingVector);
+  }
 
-BOOST_FIXTURE_TEST_CASE(sortIteratorType, fixture)
-{
-	vika::sort< vika::Iterator >(list, lessComparator);
-  BOOST_CHECK(list == sortedAscendingList);
-	vika::sort< vika::Iterator >(list, greaterComparator);
-  BOOST_CHECK(list == sortedDescendingList);
-}
+  BOOST_FIXTURE_TEST_CASE(sortIteratorType, fixture)
+  {
+	  vika::sort< vika::Iterator >(list, lessComparator);
+    BOOST_CHECK(list == sortedAscendingList);
+	  vika::sort< vika::Iterator >(list, greaterComparator);
+    BOOST_CHECK(list == sortedDescendingList);
+  }
 
 BOOST_AUTO_TEST_SUITE_END()

a597940f948f4741d66fef85955f910560dd1065
pozdnyakov-artemy <aapozdnyakove@gmail.com>
2021-04-26 21:19:30 +0300

    Task 2 doesnt't throw when file is empty

--- pozdnyakov.artemy/B1/task2.cpp
+++ pozdnyakov.artemy/B1/task2.cpp
@@ -27,10 +27,6 @@ void poz::task2(char* argv[])
     read += fs.gcount();
   }
   fs.close();
-  if (read == 0)
-  {
-    throw std::runtime_error("Empty file");
-  }
   std::vector<char> vector(buf.get(), buf.get() + read);
   poz::print(vector, std::cout);
 }

7d4843321eb7ee75df572febd069f23cc272952f
Vladislav Shilyaev <schilyaev.vlad@gmail.com>
2021-04-27 13:52:03 +0300

    Use std::generate to fill test collection

--- shilyaev.vladislav/B1/test-sort.cpp
+++ shilyaev.vladislav/B1/test-sort.cpp
@@ -1,31 +1,17 @@
 #include <boost/test/unit_test.hpp>
 #include <algorithm>
-#include <limits>
-#include <random>
-
+#include <ctime>
 #include "access-strategies.hpp"
 #include "sort.hpp"
 
 namespace shi = shilyaev;
 
 namespace shilyaev {
-  template < typename Iterator >
-  void fill(Iterator begin, const Iterator& end)
-  {
-    int min = std::numeric_limits< int >::lowest();
-    int max = std::numeric_limits< int >::max();
-    std::uniform_int_distribution< int > distribution(min, max);
-    std::random_device randomDevice;
-    for (; begin != end; begin++) {
-      *begin = distribution(randomDevice);
-    }
-  }
-
   template < typename AccessStrategy >
   bool testSort(const typename std::function< bool(int, int) > &compare, size_t itemCount)
   {
     typename AccessStrategy::Collection collection(itemCount);
-    fill(collection.begin(), collection.end());
+    std::generate(collection.begin(), collection.end(), std::rand);
     bubbleSort< AccessStrategy >(collection, compare);
     return std::is_sorted(collection.cbegin(), collection.cend(), compare);
   }
@@ -33,6 +19,7 @@ namespace shilyaev {
 
 BOOST_AUTO_TEST_CASE(Sort)
 {
+  std::srand(std::time(nullptr));
   const size_t sizes[] = {0, 1, 2, 1000};
   for (size_t size: sizes) {
     BOOST_CHECK((shi::testSort< shi::VectorAtStrategy< int > >(std::less<>(), size)));

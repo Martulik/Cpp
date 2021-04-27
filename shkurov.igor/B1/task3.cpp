40f81e426eebcb62272765653a9b030342634188
Shkurov Igor <lilthewil@gmail.com>
2021-04-27 05:56:22 +0300

    redesigned task3

--- shkurov.igor/B1/task3.cpp
+++ shkurov.igor/B1/task3.cpp
@@ -9,14 +9,23 @@ void lab::taskThree()
   std::vector< int > vec;
 
   int x = 0;
-  while (std::cin >> x && x != 0)
+  while (std::cin >> x)
   {
+    if (x == 0)
+    {
+      break;
+    }
     vec.push_back(x);
   }
 
-  if (std::cin.fail() || x != 0)
+  if (std::cin.fail() && !(std::cin.eof()))
   {
     throw std::invalid_argument("Incorrect input.");
+  }
+
+  if (x != 0)
+  {
+    throw std::invalid_argument("Last number must be zero.");
   }
 
   if (vec.empty())

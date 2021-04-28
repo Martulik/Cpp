3e60ad20bd788b44c4c964707687d90c44028f45
Vladislav Shilyaev <schilyaev.vlad@gmail.com>
2021-04-28 21:43:07 +0300

    Remove std::string usage

--- shilyaev.vladislav/B1/compare-function.hpp
+++ shilyaev.vladislav/B1/compare-function.hpp
@@ -1,16 +1,16 @@
 #ifndef COMPARE_FUNCTION_HPP
 #define COMPARE_FUNCTION_HPP
 
-#include <string>
+#include <cstring>
 #include <boost/optional.hpp>
 
 namespace shilyaev {
   template < typename T >
   boost::optional< std::function< bool(T, T) > > getCompareFunction(const char *string)
   {
-    if (string == std::string("ascending")) {
+    if (strcmp(string, "ascending") == 0) {
       return {std::less< T >()};
-    } else if (string == std::string("descending")) {
+    } else if (strcmp(string, "descending") == 0) {
       return {std::greater< T >()};
     }
     return {};
--- shilyaev.vladislav/B1/string-convert.hpp
+++ shilyaev.vladislav/B1/string-convert.hpp
@@ -1,7 +1,6 @@
 #ifndef STRING_CONVERT_HPP
 #define STRING_CONVERT_HPP
 
-#include <string>
 #include <boost/optional.hpp>
 
 namespace shilyaev {

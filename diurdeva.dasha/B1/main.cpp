110ae755383c5cd6492021a4f85bf0192025e1f7
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-04-29 22:42:52 +0300

    change main

--- diurdeva.dasha/B1/main.cpp
+++ diurdeva.dasha/B1/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
     err.set("Wrong number of arguments\n");
   }
 
-  if (!err.isError() || !lab::isNumber(argv[1])) {
+  if (err.isError() && !lab::isNumber(argv[1])) {
     err.set("Incorrect number for task\n");
   }
   int taskNumber = (err.isError()) ? atoi(argv[1]) : 0;
@@ -32,11 +32,13 @@ int main(int argc, char *argv[])
     lab::task3(err);
   } else if (taskNumber == 4 && argc == 4) {
     const std::function< bool(double, double) > compare = lab::getCompare< double >(argv[2], err);
-    if (!lab::isNumber(argv[3])) {
-      err.set("Incorrect argument");
-    }
-    if (err.isError()) {
-      lab::task4(compare, atoi(argv[3]));
+    if (err.isError) {
+      if (lab::isNumber(argv[3])) {
+        lab::task4(compare, atoi(argv[3]));
+      }
+      else {
+        err.set("Incorrect argument");
+      }
     }
   } else {
     err.set("Incorrect number for task\n");

049413edb5aa3a608a54101873d305d75cfbf90a
pozdnyakov-artemy <aapozdnyakove@gmail.com>
2021-05-01 00:16:15 +0300

    Pass acceptance

--- pozdnyakov.artemy/B1/task3.cpp
+++ pozdnyakov.artemy/B1/task3.cpp
@@ -9,7 +9,7 @@ namespace poz = pozdnyakov;
 void poz::task3()
 {
   std::vector< int > vector;
-  int n;
+  int n = 0;
   while (std::cin >> n && n != 0)
   {
     vector.push_back(n);
@@ -20,11 +20,11 @@ void poz::task3()
   }
   if (vector.size() != 0)
   {
-    if (*std::prev(vector.end()) == 1)
+    if (vector.back() == 1)
     {
       poz::eraseEven(vector);
     }
-    else if (*std::prev(vector.end()) == 2)
+    else if (vector.back() == 2)
     {
       poz::addOnes(vector);
     }

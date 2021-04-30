70d900662a604b280555f6a5dd7a5bd4835a18a0
pozdnyakov-artemy <aapozdnyakove@gmail.com>
2021-05-01 02:31:34 +0300

    Phonebook header draft

---
+++ pozdnyakov.artemy/B3/phonebook.hpp
@@ -0,0 +1,28 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <utility>
+#include <map>
+
+namespace pozdnyakov
+{
+  class Phonebook
+  {
+  public:
+    using EntryType = std::pair< std::string, std::string >;
+    Phonebook();
+    void showCurrent(std::ostream& out);
+    void nextEntry();
+    void prevEntry();
+    void insertEntry(EntryType entry);
+    void pushBackEntry(EntryType entry);
+    void move(int n);
+
+
+  private:
+    typedef std::list< EntryType >::iterator currEntry;
+    std::list< EntryType > entries;
+  };
+}
+
+#endif

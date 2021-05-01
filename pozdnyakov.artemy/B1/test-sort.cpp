72f00d4cd34348854f496c527c0831e3d0c4c904
pozdnyakov-artemy <aapozdnyakove@gmail.com>
2021-05-02 00:40:35 +0300

    Re-writed test to make them compact & useful for debugging

--- pozdnyakov.artemy/B1/test-sort.cpp
+++ pozdnyakov.artemy/B1/test-sort.cpp
@@ -8,64 +8,27 @@
 
 namespace poz = pozdnyakov;
 
-BOOST_AUTO_TEST_SUITE(test_sort)
-
-  using dvector = std::vector< double >;
-  using dlist = std::forward_list< double >;
-
-  BOOST_AUTO_TEST_CASE(empty_vector)
+BOOST_AUTO_TEST_CASE(test_sort)
+{
+  std::vector< size_t > sizes{0, 1, 2, 3, 99};
+  srand(time(0));
+  for (size_t size: sizes)
   {
-    dvector vector(0);
-    BOOST_REQUIRE(poz::testVectorSort(vector, std::greater< double >()));
-    BOOST_REQUIRE(poz::testVectorSort(vector, std::less< double >()));
-  }
-
-  BOOST_AUTO_TEST_CASE(empty_list)
-  {
-    dlist list(0);
-    BOOST_REQUIRE(poz::testListSort(list, std::greater< double >()));
-    BOOST_REQUIRE(poz::testListSort(list, std::less< double >()));
-  }
-
-  BOOST_AUTO_TEST_CASE(vector_1_3)
-  {
-    for (int i = 0; i < 4; i++)
     {
-      dvector vector(i);
-      srand(time(0));
+      std::vector< double > vector(size);
       std::generate(vector.begin(), vector.end(), rand);
+      BOOST_TEST_MESSAGE("Testing std::vector< double > on " << size << "size with std::greater< double >: ");
       BOOST_REQUIRE(poz::testVectorSort(vector, std::greater< double >()));
+      BOOST_TEST_MESSAGE("Testing std::vector< double > on " << size << "size with std::less< double >: ");
       BOOST_REQUIRE(poz::testVectorSort(vector, std::less< double >()));
     }
-  }
-
-  BOOST_AUTO_TEST_CASE(list_1_3)
-  {
-    for (int i = 0; i < 4; i++)
     {
-      dlist list(i);
-      srand(time(0));
+      std::forward_list< double > list(size);
       std::generate(list.begin(), list.end(), rand);
       BOOST_REQUIRE(poz::testListSort(list, std::greater< double >()));
+      BOOST_TEST_MESSAGE("Testing std::vector< double > on " << size << "size with std::greater< double >: ");
       BOOST_REQUIRE(poz::testListSort(list, std::less< double >()));
+      BOOST_TEST_MESSAGE("Testing std::vector< double > on " << size << "size with std::less< double >: ");
     }
   }
-  BOOST_AUTO_TEST_CASE(vector_big)
-  {
-    dvector vector(99);
-    srand(time(0));
-    std::generate(vector.begin(), vector.end(), rand);
-    BOOST_REQUIRE(poz::testVectorSort(vector, std::greater< double >()));
-    BOOST_REQUIRE(poz::testVectorSort(vector, std::less< double >()));
-  }
-
-  BOOST_AUTO_TEST_CASE(list_big)
-  {
-    dlist list(99);
-    srand(time(0));
-    std::generate(list.begin(), list.end(), rand);
-    BOOST_REQUIRE(poz::testListSort(list, std::greater< double >()));
-    BOOST_REQUIRE(poz::testListSort(list, std::less< double >()));
-  }
-
-BOOST_AUTO_TEST_SUITE_END()
+}

55f7037622d8aa5f4d43cf7a06cf6b46d00b4cbd
Islam <murzakanovislam21@gmail.com>
2021-05-01 15:57:48 +0300

    Add std::isdigit()

--- murzakanov.islam/B1/tools.cpp
+++ murzakanov.islam/B1/tools.cpp
@@ -16,7 +16,7 @@ bool murzakanov::checkIsNumber(const char* str)
 {
   for (size_t i = 0; i < strlen(str); i++)
   {
-    if (str[i] < '0' || str[i] > '9')
+    if (!std::isdigit(str[i]))
     {
       return false;
     }

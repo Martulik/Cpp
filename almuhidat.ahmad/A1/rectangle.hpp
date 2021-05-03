fd01e1ee88c11a0709cc9fff00af63d5f811e752
ahmad_almuhidat <ahmad.almuhidat@gmail.com>
2021-05-03 12:37:53 +0300

    fixed the indent

--- almuhidat.ahmad/A1/rectangle.hpp
+++ almuhidat.ahmad/A1/rectangle.hpp
@@ -5,20 +5,20 @@
 class Rectangle : public Shape
 {
 public:
-    Rectangle(double valueHeight, double valueWidth, point_t valuePos) :
-        height_(valueHeight),
-        width_(valueWidth),
-        pos_(valuePos)
+  Rectangle(double valueHeight, double valueWidth, point_t valuePos) :
+    height_(valueHeight),
+    width_(valueWidth),
+    pos_(valuePos)
     {
     }
-    double getArea() const override;
-    rectangle_t getFrameRect() const override;
-    void move(point_t newPos) override;
-    void move(double abscissa, double ordinate) override;
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(point_t newPos) override;
+  void move(double abscissa, double ordinate) override;
 private:
-    double height_;
-    double width_;
-    point_t pos_;
+  double height_;
+  double width_;
+  point_t pos_;
 };
 
 #endif

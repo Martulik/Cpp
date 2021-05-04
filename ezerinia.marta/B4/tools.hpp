23f46e7b5fe971a91ef8e909ffc92b879266ebec
Марта Эзериня <martha@MacBook-Air-Marta.local>
2021-05-04 21:46:52 +0300

    add tests

--- ezerinia.marta/B4/tools.hpp
+++ ezerinia.marta/B4/tools.hpp
@@ -6,16 +6,10 @@
 #include "dataStruct.hpp"
 
 namespace ezerinia {
-
   void checkInput(const DataStruct &data, char comma,std::stringstream &input);
-
   bool compare(DataStruct &lhs, DataStruct &rhs);
-
   void print(std::vector< DataStruct > &vector, std::ostream &out);
-
   void read(std::vector< DataStruct > &vector);
-
   void task();
-
 }
 #endif

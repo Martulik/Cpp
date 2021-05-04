486f9c87ccca60dbdd3df1883512069c9fd3ed47
Danil Doroshin <ddddanil5555@gmail.com>
2021-05-04 14:14:10 +0300

    Add input fail checks

--- doroshin.danil/B4/datastruct.cpp
+++ doroshin.danil/B4/datastruct.cpp
@@ -13,6 +13,10 @@ std::istream& dan::operator>>(std::istream& in, DataStruct& data)
   if(std::getline(in, line_)) {
     std::istringstream line(line_);
     line >> data.key1;
+    if(!line) {
+      fail();
+      return in;
+    }
     if(data.key1 < -5 || data.key1 > 5) {
       fail();
       return in;
@@ -22,6 +26,10 @@ std::istream& dan::operator>>(std::istream& in, DataStruct& data)
       return in;
     }
     line >> data.key2;
+    if(!line) {
+      fail();
+      return in;
+    }
     if(data.key2 < -5 || data.key2 > 5) {
       fail();
       return in;

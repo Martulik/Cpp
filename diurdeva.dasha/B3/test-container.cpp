f42543a7780120291f5b0cbc3dbdf7fda7dcf6ff
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-05-04 14:27:12 +0500

    change test-container

--- diurdeva.dasha/B3/test-container.cpp
+++ diurdeva.dasha/B3/test-container.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_CASE(factorial)
   diurdeva::Container cont;
   unsigned int factorial = 1;
   int i = 1;
-  for (auto iter = container.begin(); iter != container.end(); iter++, i++) {
+  for (auto iter = cont.begin(); iter != cont.end(); iter++, i++) {
     factorial *= i;
     BOOST_CHECK(*iter == factorial);
   }

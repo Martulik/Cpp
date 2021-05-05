52967be96bb56c32311647929e3bd0d4aba12e22
Vladislav <vrazbusiness@mail.ru>
2021-05-06 00:14:02 +0300

    add changes in main.cpp for tasks

--- razukrantov.vladislav/B1/main.cpp
+++ razukrantov.vladislav/B1/main.cpp
@@ -1,6 +1,48 @@
 ﻿#include <iostream>
+#include <stdexcept>
 
-int main(int argc, char *argv[])
+#include "tasks.hpp"
+
+int main(int argc, char* argv[])
 {
+  try
+  {
+	if ((argc < 2) || (argc > 4))
+	{
+	  throw(std::invalid_argument("Incorrect arguments"));
+	}
+	switch (atoi(argv[1]))
+	{
+	case 1:
+	  if (argc != 3)
+	  {
+		throw(std::invalid_argument("Incorrect arguments count"));
+	  }
+	  razukrantov::task1(argv[2]);
+	  break;
+	case 2:
+	  if (argc != 3)
+	  {
+		throw(std::invalid_argument("Incorrect arguments count"));
+	  }
+	  razukrantov::task2(argv[2]);
+	  break;
+	case 3:
+	  if (argc != 2)
+	  {
+		throw(std::invalid_argument("Incorrect arguments count"));
+	  }
+	  razukrantov::task3();
+	  break;
+	default:
+	  throw(std::invalid_argument("incorrect number of task"));
+	  break;
+	}
+  }
+  catch (std::exception& err)
+  {
+	std::cerr << err.what() << "\n";
+	return 1;
+  }
   return 0;
 }

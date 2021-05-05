a4279dcddf57834fbac3f76b39c611ae68c8a97a
Vladislav <vrazbusiness@mail.ru>
2021-05-05 18:32:46 +0300

    add task2

---
+++ razukrantov.vladislav/B1/task2.cpp
@@ -0,0 +1,43 @@
+#include "tasks.hpp"
+#include "task1.cpp"
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <vector>
+
+void razukrantov::task2(const char* file)
+{
+  if (file == nullptr)
+  {
+	throw std::invalid_argument("Incorrect file");
+  }
+  std::ifstream input(file);
+  if (!input)
+  {
+	throw (std::runtime_error("Open error"));
+  }
+
+  size_t capacity = 1;
+  size_t size = 0;
+  std::unique_ptr< char[] > data = std::make_unique< char[] >(capacity);
+
+  while (input)
+  {
+	input.read(data.get() + size, capacity - size);
+	size += input.gcount();
+	if (size == capacity)
+	{
+	  capacity *= 2;
+	  std::unique_ptr< char[] > temp = std::make_unique< char[] >(capacity);
+	  for (size_t i = 0; i < size; i++)
+	  {
+		temp[i] = data[i];
+	  }
+	  data = std::move(temp);
+	}
+  }
+
+  std::vector< char > vector(data.get(), data.get() + size);
+  print(vector);
+}

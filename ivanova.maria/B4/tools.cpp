9e24c8b6736049aaeb9a409732ee01589946e05c
Мария <mashastik2013@,ail.tu>
2021-05-06 11:55:39 +0300

    modified reading og string

--- ivanova.maria/B4/tools.cpp
+++ ivanova.maria/B4/tools.cpp
@@ -8,8 +8,16 @@ namespace ivanova
   template < typename T > void getVector(std::stringstream &input, std::vector < T > &vec)
   {
     DataStruct data;
+    std::string str;
     std::string key;
-    input >> std::ws >> key;
+    size_t i = 0;
+    input >> str;
+    while (str.at(i) != ',')
+    {
+      key += str.at(i);
+      i++;
+    }
+    i++;
     if (abs(std::stoi(key) > 5))
     {
       std::cerr << "key1 must be in range of -5 to 5\n";
@@ -18,7 +26,13 @@ namespace ivanova
     {
       data.key1 = std::move(std::stoi(key));
     }
-    input >> key;
+    key.clear();
+    while (str.at(i) != ',')
+    {
+      key += str.at(i);
+      i++;
+    }
+    i++;
     if (abs(std::stoi(key)) > 5)
     {
       std::cerr << "key2 must be in range of -5 tp 5\n";
@@ -26,7 +40,13 @@ namespace ivanova
     else
     {
       data.key2 = std::move(std::stoi(key));
-      input >> data.str;
+      key.clear();
+      while (i < str.length())
+      {
+        key += str.at(i);
+        i++;
+      }
+      data.str = std::move(key);
       vec.push_back(data);
     }
   }

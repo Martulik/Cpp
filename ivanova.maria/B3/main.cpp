8e10bb57329947ce4882bb6b3e15325e01eae8a8
Мария <mashastik2013@,ail.tu>
2021-05-07 20:28:09 +0300

    added taskNumber check

--- ivanova.maria/B3/main.cpp
+++ ivanova.maria/B3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 #include "task1.hpp"
 #include "task2.hpp"
 
@@ -13,7 +14,7 @@ int main(int argc, char *argv[])
     std::cerr << "Incorrect number of parameters!";
     return exitCode;
   }
-  if (std::isdigit(argv[1][0]))
+  if (std::isdigit(argv[1][0]) && std::strlen(argv[1]) == 1)
   {
     const int taskNum = std::atoi(argv[1]);
     if ((taskNum != 1 && taskNum != 2))

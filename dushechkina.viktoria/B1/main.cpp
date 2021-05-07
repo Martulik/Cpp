2e3e93feb4ab6cd52ac6aea6ccd8d489be91050d
viktoria.dushechkina <byzaya@icloud.com>
2021-05-07 20:07:24 +0300

    change main

--- dushechkina.viktoria/B1/main.cpp
+++ dushechkina.viktoria/B1/main.cpp
@@ -18,99 +18,53 @@ int main(int argc, char* argv[])
     return 1;
   }
   int taskNum = 0;
-  //std::stringstream in(argv[1]);
-  //in >> taskNum;
-
-  //if (!in.eof())
-  //{
-  //  std::cerr << "Incorrect input of task number";
-  //  return 1;
-  //}
+ 
   if (dushechkina::checkIsNumber(argv[1]))
   {
     taskNum = std::atoi(argv[1]);
   }
-
-  if (taskNum == 0)
+  try
   {
-    std::cerr << "Incorrect input of task number";
-    return 1;
-  }
-
-  switch (taskNum)
-  {
-  case 1:
-    if (argc != 3)
+    if (taskNum == 0)
     {
-      std::cerr << "Incorrect number of arguments";
+      std::cerr << "Incorrect input of task number";
       return 1;
     }
-    try
+    else if (taskNum == 1)
     {
+      if (argc != 3)
+      {
+        std::cerr << "Incorrect number of arguments";
+        return 1;
+      }
       std::function< bool(const int&, const int&) > comparator = dushechkina::getSortingOrder< int >(argv[2]);
       task1(comparator);
     }
-    catch (const std::invalid_argument& error)
+    else if (taskNum == 2)
     {
-      std::cerr << error.what() << "\n";
-      return 1;
-    }
-    catch (const std::runtime_error& error)
-    {
-      std::cerr << error.what() << "\n";
-      return 1;
-    }
-    break;
-  case 2:
-    if (argc != 3)
-    {
-      std::cerr << "Incorrect number of arguments";
-      return 1;
-    }
-    try
-    {
+      if (argc != 3)
+      {
+        std::cerr << "Incorrect number of arguments";
+        return 1;
+      }
       task2(argv[2]);
     }
-    catch (const std::runtime_error& error)
+    else if (taskNum == 3)
     {
-      std::cerr << error.what() << "\n";
-      return 1;
-    }
-    catch (const std::invalid_argument& error)
-    {
-      std::cerr << error.what() << "\n";
-      return 1;
-    }
-    break;
-  case 3:
-    if (argc != 2)
-    {
-      std::cerr << "Incorrect number of arguments";
-      return 1;
-    }
-    try
-    {
+      if (argc != 2)
+      {
+        std::cerr << "Incorrect number of arguments";
+        return 1;
+      }
       task3();
     }
-    catch (const std::runtime_error& error)
+    else if (taskNum == 4)
     {
-      std::cerr << error.what() << "\n";
-      return 1;
-    }
-    catch (const std::logic_error& error)
-    {
-      std::cerr << error.what() << "\n";
-      return 1;
-    }
-    break;
-  case 4:
-    if (argc != 4)
-    {
-      std::cerr << "Incorrect number of arguments";
-      return 1;
-    }
-    try
-    {
+      if (argc != 4)
+      {
+        std::cerr << "Incorrect number of arguments";
+        return 1;
+      }
       std::function< bool(double, double) > comparator = dushechkina::getSortingOrder< double >(argv[2]);
       int size = 0;
       std::stringstream stream(argv[3]);
@@ -122,17 +76,26 @@ int main(int argc, char* argv[])
       }
       task4(comparator, size);
     }
-    catch (const std::invalid_argument& error)
+    else
     {
-      std::cerr << error.what() << "\n";
+      std::cerr << "Incorrect task number";
       return 1;
     }
-    break;
-  default:
+  }
+  catch (const std::runtime_error& error)
   {
-    std::cerr << "Incorrect task number";
+    std::cerr << error.what() << "\n";
     return 1;
   }
+  catch (const std::invalid_argument& error)
+  {
+    std::cerr << error.what() << "\n";
+    return 1;
+  }
+  catch (const std::logic_error& error)
+  {
+    std::cerr << error.what() << "\n";
+    return 1;
   }
   return 0;
 }

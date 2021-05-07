c221999d9205c77c8017505019fb3c700e1838af
Мария <mashastik2013@,ail.tu>
2021-05-07 20:24:15 +0300

    fixed output

--- ivanova.maria/B3/record.cpp
+++ ivanova.maria/B3/record.cpp
@@ -59,7 +59,7 @@ std::istream &iva::operator >>(std::istream &in, ivanova::Record &info)
 
 std::ostream &ivanova::operator<<(std::ostream &out, ivanova::Record &info)
 {
-  out << info.data.first << " " << info.data.second;
+  out << info.data.first << " " << info.data.second << std::endl;
   return out;
 }
 

e90368f4ddaeffdc1d4573f5679f879aa2ecb967
LysenkoAleksandra <orange260302@gmail.com>
2021-05-09 18:26:44 +0300

    Ultra raw version of main

--- lysenko.aleksandra/B3/main.cpp
+++ lysenko.aleksandra/B3/main.cpp
@@ -1,4 +1,23 @@
-int main()
+#include <iostream>
+
+#include "tasks.h"
+
+int main(int argc, char* argv[])
 {
-  return 0;
+  int conditionCode = 1;
+
+  if (argc == 2)
+  {
+    int taskNumber = std::atoi(argv[1]);
+    if (taskNumber == 1)
+    {
+      conditionCode = lysenko::task1();
+    }
+    else if (taskNumber == 2)
+    {
+      conditionCode = lysenko::task2();
+    }
+  }
+
+  return conditionCode;
 }
--- lysenko.aleksandra/B3/task1.cpp
+++ lysenko.aleksandra/B3/task1.cpp
@@ -1,19 +1,21 @@
+#include "tasks.h"
+
 #include <iostream>
 
 #include "UsersInterface.h"
 #include "commands.h"
 
-int task1(std::istream& input)
+int lysenko::task1()
 {
   lysenko::UsersInterface myInterface;
   std::string myCommand;
 
-  while (std::getline(input, myCommand))
+  while (std::getline(std::cin, myCommand))
   {
     readCommand(myCommand, myInterface);
   }
 
-  if (!input.eof())
+  if (!std::cin.eof())
   {
     std::cerr << "Error with reading command \n";
     return 2;

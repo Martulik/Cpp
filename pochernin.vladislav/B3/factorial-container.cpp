c66f38f335c7f30d819e875b6a0edf23c3a7eb27
vladislav-pochernin <vspochernin@gmail.com>
2021-05-12 01:11:41 +0300

    Add factorial-container.cpp
    -Добавил первую версию реализации контейнера факторалов.

---
+++ pochernin.vladislav/B3/factorial-container.cpp
@@ -0,0 +1,87 @@
+#include "factorial-container.hpp"
+
+#include <stdexcept>
+
+#include "functions.hpp"
+
+using FC = pochernin::FactorialContainer;
+using FI = pochernin::FactorialContainer::FactorialIterator;
+using ValueType = pochernin::FactorialContainer::ValueType;
+
+
+FI::FactorialIterator(ValueType number):
+  number_(number),
+  factorial_(getFactorial(number))
+{}
+
+bool FI::operator==(const FI& rhs) const
+{
+  return ((number_ == rhs.number_) && (factorial_ == rhs.factorial_));
+}
+
+bool FI::operator!=(const FI& rhs) const
+{
+  return !(this->operator==(rhs));
+}
+
+const ValueType& FI::operator*() const
+{
+  return factorial_;
+}
+
+const ValueType* FI::operator->() const
+{
+  return std::addressof(factorial_);
+}
+
+FI& FI::operator++()
+{
+  if (number_ >= maxNumber)
+  {
+    throw(std::runtime_error("Out of range"));
+  }
+  else
+  {
+    number_++;
+    factorial_ *= number_;
+    return *this;
+  }
+}
+
+FI FI::operator++(int)
+{
+  FI temp = *this;
+  ++(*this);
+  return temp;
+}
+
+FI& FI::operator--()
+{
+  if (number_ <= minNumber)
+  {
+    throw(std::runtime_error("Out of range"));
+  }
+  else
+  {
+    number_--;
+    factorial_ /= number_;
+    return *this;
+  }
+}
+
+FI FI::operator--(int)
+{
+  FI temp = *this;
+  --(*this);
+  return temp;
+}
+
+FI FC::begin() const
+{
+  return FI(minNumber);
+}
+
+FI FC::end() const
+{
+  return FI(maxNumber);
+}

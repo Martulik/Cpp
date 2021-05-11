b77279afeb9dfa37c3d2bb0f2a05c748ccfc514e
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-05-11 12:40:24 +0500

    CG

--- diurdeva.dasha/B3/phonebook.hpp
+++ diurdeva.dasha/B3/phonebook.hpp
@@ -8,11 +8,12 @@
 namespace diurdeva {
   class Phonebook {
   public:
-    using list = std::list< record_t >;
+    using list = std::list<record_t>;
     using iterator = list::iterator;
 
     void pushBack(const record_t &rec);
     bool empty() const;
+
     iterator begin();
     iterator end();
     iterator next(iterator) const;
--- diurdeva.dasha/B3/phonebookInterface.hpp
+++ diurdeva.dasha/B3/phonebookInterface.hpp
@@ -34,4 +34,4 @@ namespace diurdeva {
   };
 }
 
-#endif 
+#endif

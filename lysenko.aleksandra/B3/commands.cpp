b49b963170f39227c1af2d0b2831fd33fc2ac9f2
LysenkoAleksandra <orange260302@gmail.com>
2021-05-12 08:42:59 +0300

    Fix input

--- lysenko.aleksandra/B3/commands.cpp
+++ lysenko.aleksandra/B3/commands.cpp
@@ -5,6 +5,41 @@
 #include <sstream>
 #include <string>
 
+static bool readDelimiter(std::istream& in, char delimiter)
+{
+  in >> std::ws;
+  if (in.peek() != delimiter)
+  {
+    in.setstate(std::istream::failbit);
+    return false;
+  }
+  char c;
+  in >> c;
+  return true;
+}
+
+static std::string readString(std::istream& in)
+{
+  std::string result;
+  std::string buffer;
+
+  if (!readDelimiter(in, '\"'))
+  {
+    return result;
+  }
+  do
+  {
+    getline(in, buffer, '\"');
+    result += buffer;
+    if (!result.empty() && (result.back() == '\\'))
+    {
+      result.back() = '\"';
+    }
+  } 
+  while (in && !result.empty() && (result.back() == '\"'));
+  return result;
+}
+
 bool lysenko::isDigitsOnly(std::string& number)
 {
   if (number == "")
@@ -21,18 +56,13 @@ bool lysenko::isDigitsOnly(std::string& number)
 }
 bool lysenko::checkCorrectNumberAndName(std::string& name, std::string& number)
 {
-  if ((name.size() != 2) && (isDigitsOnly(number)))
+  if ((!name.empty())&&(isDigitsOnly(number)))
   {
-    if (name.front() != '\"' || name.back() != '\"')
-    {
-      return 0;
-    }
-    name = name.substr(1, name.size() - 2);
     return 1;
   }
   else
   {
-    if (name.size() == 2)
+    if (name.empty())
     {
       std::cerr << "Name can not be empty \n";
       return 0;
@@ -68,7 +98,7 @@ void lysenko::readCommand(const std::string& inputCommand, lysenko::UsersInterfa
     std::istringstream in{ inputCommand };
     std::string command;
 
-    in >> command>> std::ws ;
+    in >> command >> std::ws;
     iteratorInt iter = commandsMap.find(command);
 
     if (iter == commandsMap.end())
@@ -90,7 +120,8 @@ void lysenko::executeAdd(std::istream& input, lysenko::UsersInterface& myInterfa
   std::string name;
   std::string number;
 
-  input >> number >> std::ws >> name;
+  input >> number >> std::ws;
+  name = readString(input);
 
   if (checkCorrectNumberAndName(name, number))
   {
@@ -103,7 +134,7 @@ void lysenko::executeStore(std::istream& input, lysenko::UsersInterface& myInter
   std::string oldMarkName;
   std::string newMarkName;
 
-  input  >> oldMarkName >> std::ws >> newMarkName;
+  input >> oldMarkName >> std::ws >> newMarkName;
 
   if (checkIfThisMarkNameContains(oldMarkName, myInterface))
   {
@@ -175,7 +206,7 @@ void lysenko::executeMove(std::istream& input, lysenko::UsersInterface& myInterf
   std::string markName;
   std::string steps;
 
-  input  >> markName >> std::ws >> steps;
+  input >> markName >> std::ws >> steps;
 
   if (checkIfThisMarkNameContains(markName, myInterface))
   {

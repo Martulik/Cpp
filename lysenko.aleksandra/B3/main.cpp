d87e5ab217867d6e92c89c007bde6b135aa4a199
LysenkoAleksandra <orange260302@gmail.com>
2021-05-12 15:31:36 +0300

    Add checking argument's correction

--- lysenko.aleksandra/B3/main.cpp
+++ lysenko.aleksandra/B3/main.cpp
@@ -2,9 +2,14 @@
 
 #include "tasks.h"
 
+namespace lysenko
+{
+  bool correctData(char* argv);
+}
+
 int main(int argc, char* argv[])
 {
-  if (argc == 2)
+  if ((argc == 2)&&(lysenko::correctData(argv[1])))
   {
     int taskNumber = std::atoi(argv[1]);
     if (taskNumber == 1)
@@ -17,5 +22,20 @@ int main(int argc, char* argv[])
     }
   }
 
+  return 1;
+}
+
+bool lysenko::correctData(char* argv)
+{
+  int j = 0;
+  while (argv[j] != '\0')
+  {
+    if (argv[j] == ' ')
+    {
+      std::cerr << "Invalid  argument";
+      return 0;
+    }
+    j += 1;
+  }
   return 1;
 }

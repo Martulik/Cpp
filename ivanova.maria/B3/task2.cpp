855aa88d0d097b8885e12f02c0d0898b4aba5154
Мария <mashastik2013@,ail.tu>
2021-05-12 16:47:43 +0300

    task2 now gets std::ostream argument

--- ivanova.maria/B3/task2.cpp
+++ ivanova.maria/B3/task2.cpp
@@ -5,12 +5,13 @@
 
 namespace iva = ivanova;
 
-int iva::task2()
+int iva::task2(std::ostream &out)
 {
   Container container;
-  std::copy(container.begin(), container.end(), std::ostream_iterator< size_t >(std::cout, " "));
-  std::cout << '\n';
-  std::reverse_copy(container.begin(), container.end(), std::ostream_iterator< size_t >(std::cout, " "));
-  std::cout << '\n';
+  using iterator = std::ostream_iterator< Container::value_type >;
+  std::copy(container.begin(), container.end(), iterator (std::cout, " "));
+  out << '\n';
+  std::reverse_copy(container.begin(), container.end(), iterator (std::cout, " "));
+  out << '\n';
   return 0;
 }
--- ivanova.maria/B3/task2.hpp
+++ ivanova.maria/B3/task2.hpp
@@ -1,8 +1,10 @@
 #ifndef B3_TASK2_HPP
 #define B3_TASK2_HPP
 
+#include <iosfwd>
+
 namespace ivanova
 {
-  int task2();
+  int task2(std::ostream &out);
 }
 #endif

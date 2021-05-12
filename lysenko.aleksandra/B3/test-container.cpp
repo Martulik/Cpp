80abf89b8f15c2bd168e2217d8757e7a0f29c34e
LysenkoAleksandra <orange260302@gmail.com>
2021-05-12 18:23:33 +0300

    Add first unit tests

---
+++ lysenko.aleksandra/B3/test-container.cpp
@@ -0,0 +1,19 @@
+#include <boost/test/unit_test.hpp>
+
+#include "container.h"
+
+BOOST_AUTO_TEST_SUITE(testFactorialContainer)
+
+BOOST_AUTO_TEST_CASE(testFactorialValue)
+{
+  lysenko::Container myCont;
+  int number = 1;
+  int factorialValue = 1;
+  for (lysenko::Container::Iterator iter = myCont.begin(); iter != myCont.end(); iter++)
+  {
+    factorialValue *= number;
+    number++;
+    BOOST_CHECK_EQUAL(*iter, factorialValue);
+  }
+}
+BOOST_AUTO_TEST_SUITE_END()

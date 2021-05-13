ba874fac775f771ee8671eb27200b983199b4d11
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-05-13 11:26:19 +0300

    Add a class 'PhoneBook'

---
+++ savchuk.aleksei/B3/phone-book.cpp
@@ -0,0 +1,99 @@
+#include "phone-book.hpp"
+
+#include <ostream>
+
+namespace lab = savchuk;
+
+void lab::PhoneBook::show(std::ostream& os, lab::PhoneBook::iterator it) const
+{
+  os << *it;
+}
+
+lab::PhoneBook::iterator lab::PhoneBook::moveNext(lab::PhoneBook::iterator it)
+{
+  if (it != --data_.cend())
+  {
+    ++it;
+  }
+  return it;
+}
+
+lab::PhoneBook::iterator lab::PhoneBook::movePrev(lab::PhoneBook::iterator it)
+{
+  if (it != data_.cbegin())
+  {
+    --it;
+  }
+  return it;
+}
+
+lab::PhoneBook::iterator lab::PhoneBook::add(lab::PhoneBook::iterator it, const lab::contact_t& src)
+{
+  return data_.insert(it, src);
+}
+
+lab::PhoneBook::iterator lab::PhoneBook::replace(lab::PhoneBook::iterator it, const contact_t& src)
+{
+  *it = src;
+  return it;
+}
+
+lab::PhoneBook::iterator lab::PhoneBook::add(const lab::contact_t& src)
+{
+  data_.push_back(src);
+  return --data_.end();
+}
+
+lab::PhoneBook::iterator lab::PhoneBook::move(lab::PhoneBook::iterator it, int steps)
+{
+  if (steps >= 0)
+  {
+    while (steps != 0)
+    {
+      if (it == --data_.end())
+      {
+        return it;
+      }
+      ++it;
+      --steps;
+    }
+  }
+  else
+  {
+    while (steps != 0)
+    {
+      if (it == data_.begin())
+      {
+        return it;
+      }
+      --it;
+      ++steps;
+    }
+  }
+  return it;
+}
+
+lab::PhoneBook::iterator lab::PhoneBook::remove(lab::PhoneBook::iterator it)
+{
+  return data_.erase(it);
+}
+
+bool lab::PhoneBook::empty() const
+{
+  return data_.empty();
+}
+
+size_t lab::PhoneBook::size() const
+{
+  return data_.size();
+}
+
+lab::PhoneBook::iterator lab::PhoneBook::begin()
+{
+  return data_.begin();
+}
+
+lab::PhoneBook::iterator lab::PhoneBook::end()
+{
+  return data_.end();
+}
---
+++ savchuk.aleksei/B3/phone-book.hpp
@@ -0,0 +1,34 @@
+#ifndef PHONE_BOOK_HPP
+#define PHONE_BOOK_HPP
+
+#include <list>
+#include <cstdlib>
+
+#include "contact.hpp"
+
+namespace savchuk
+{
+  class PhoneBook
+  {
+    public:
+      using iterator = std::list< contact_t >::iterator;
+
+      void show(std::ostream& os, iterator it) const;
+      iterator moveNext(iterator it);
+      iterator movePrev(iterator it);
+      iterator add(iterator it, const contact_t& src);
+      iterator replace(iterator it, const contact_t& src);
+      iterator add(const contact_t& src);
+      iterator move(iterator it, int steps);
+      iterator remove(iterator it);
+      bool empty() const;
+      size_t size() const;
+      iterator begin();
+      iterator end();
+
+    private:
+      std::list< contact_t > data_;
+  };
+}
+
+#endif

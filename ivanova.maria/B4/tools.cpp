50b24f64f9b63f251cf0fb0f0c8304058a9d24ed
Мария <mashastik2013@,ail.tu>
2021-05-13 14:33:16 +0300

    sort

--- ivanova.maria/B4/tools.cpp
+++ ivanova.maria/B4/tools.cpp
@@ -8,9 +8,9 @@ bool iva::compare(DataStruct &data1, DataStruct &data2)
   {
     if (data1.key2 == data2.key2)
     {
-      return (data1.str.length() > data2.str.length());
+      return (data1.str.length() < data2.str.length());
     }
-    return data1.key2 > data2.key2;
+    return data1.key2 < data2.key2;
   }
-  return data1.key1 > data2.key2;
+  return data1.key1 < data2.key2;
 }

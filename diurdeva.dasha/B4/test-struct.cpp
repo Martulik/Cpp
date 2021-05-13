fca6fc27c5cb373d696077adb5d4ca69891c42b4
dasha-diurdeva <doshikdurdeva@gmail.com>
2021-05-13 18:07:37 +0300

    change two test

--- diurdeva.dasha/B4/test-struct.cpp
+++ diurdeva.dasha/B4/test-struct.cpp
@@ -17,7 +17,7 @@ BOOST_AUTO_TEST_CASE(checkInput)
 BOOST_AUTO_TEST_CASE(wrongInput)
 {
   diurdeva::DataStruct data;
-  std::stringstream input("2,-4,apple");
+  std::stringstream input("-10,4,apple");
   BOOST_CHECK_THROW(input >> data, std::invalid_argument);
 }
 
@@ -33,7 +33,7 @@ BOOST_AUTO_TEST_CASE(checkOutput)
   diurdeva::DataStruct data{ 3, 2, "apple" };
   std::stringstream input;
   input << data;
-  BOOST_CHECK_EQUAL(input.str(), "1,2,apple\n");
+  BOOST_CHECK_EQUAL(input.str(), "3,2,apple\n");
 }
 
 BOOST_AUTO_TEST_SUITE_END()

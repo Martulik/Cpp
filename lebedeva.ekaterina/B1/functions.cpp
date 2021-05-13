6fe7f470996e871dffd950058043135f8247d740
kate-lebedeva <katty.papina@gmail.com>
2021-05-13 18:36:28 +0300

    Added functions templates and realizations
    Added print and getSortOrder template functions, checking functions and
    fillRandom

---
+++ lebedeva.ekaterina/B1/functions.cpp
@@ -0,0 +1,38 @@
+#include <cstring>
+#include <cctype>
+#include <stdexcept>
+#include "functions.hpp"
+
+bool lebedeva::isNumber(const char* num)
+{
+  for (size_t i = 0; i < strlen(num); i++)
+  {
+    if (!std::isdigit(num[i]))
+    {
+      std::cerr << "Not a number";
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
+
+void lebedeva::checkNumOfArgs(int argc, int expected)
+{
+  if (argc != expected)
+  {
+    throw std::invalid_argument("Invalid number of arguments");
+  }
+}
+
+void lebedeva::fillRandom(double* array, int size)
+{
+  if (size <= 0)
+  {
+    throw std::invalid_argument("Invalid array size");
+  }
+  for (size_t i = 0; i < sizeof(array); i++)
+  {
+    array[i] = static_cast<double>(rand()) / RAND_MAX * 2 - 1;
+  }
+}

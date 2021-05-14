3dccab6587f272614aa87badbd492ee03ba4f23a
viktoria.dushechkina <byzaya@icloud.com>
2021-05-15 00:02:26 +0300

    add tests

---
+++ dushechkina.viktoria/B3/test-container.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+
+#include "factorial.hpp"
+
+BOOST_AUTO_TEST_SUITE(FACTORIAL)
+
+struct Fixture
+{
+	dushechkina::Container fact;
+	std::ostringstream output;
+};
+
+BOOST_FIXTURE_TEST_CASE(correct_dereferencing, Fixture)
+{
+	unsigned k = 1;
+	unsigned n = 1;
+	for (auto i = fact.begin(); i != fact.end(); ++i)
+	{
+		BOOST_CHECK_EQUAL(*i, k);
+		k *= ++n;
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()

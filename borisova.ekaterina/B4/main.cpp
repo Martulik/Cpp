e2dd204736b57001c959221a4e17cdfec80461ae
ekaterina-borisova <kate_boriso2002@mail.ru>
2021-05-14 23:20:07 +0400

    Change input in main

--- borisova.ekaterina/B4/main.cpp
+++ borisova.ekaterina/B4/main.cpp
@@ -13,20 +13,17 @@ int main()
   std::vector< lab::DataStruct > vector;
   lab::DataStruct value;
   std::string line;
-  while (std::getline(std::cin, line) && !line.empty())
+  try
   {
-    std::istringstream in(line);
-    try
-    {
-      in >> value;
-    }
-    catch (const std::invalid_argument& ex)
-    {
-      std::cerr << ex.what() << "\n";
-      return -1;
-    }
-    vector.push_back(value);
+    std::istream_iterator< lab::DataStruct > input(std::cin);
+    std::vector< lab::DataStruct > vector(input, std::istream_iterator< lab::DataStruct >());
   }
+  catch (const std::invalid_argument& ex)
+  {
+    std::cerr << ex.what() << "\n";
+    return -1;
+  }
+
   std::sort(vector.begin(), vector.end());
   std::ostream_iterator< lab::DataStruct > out(std::cout);
   std::copy(vector.begin(), vector.end(), out);

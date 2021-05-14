3ff9eb04ae7f434aefe93315146640ba77ba6099
Марта Эзериня <martha@MacBook-Air-Marta.local>
2021-05-14 21:49:16 +0300

    delete function 'read'

--- ezerinia.marta/B4/tools.cpp
+++ ezerinia.marta/B4/tools.cpp
@@ -7,18 +7,11 @@
 
 namespace lab = ezerinia;
 
-void lab::read(std::vector< lab::DataStruct > &vector, std::istream &in)
-{
-
-  lab::DataStruct data;
-  std::istream_iterator< lab::DataStruct > input(in);
-  std::copy(input, std::istream_iterator< DataStruct >(), std::back_inserter(vector));
-}
 
 void lab::task(std::istream &in, std::ostream &out)
 {
-  std::vector< lab::DataStruct > vector;
-  read(vector, in);
+  std::istream_iterator< lab::DataStruct > input(in);
+  std::vector< lab::DataStruct > vector(input, std::istream_iterator< DataStruct >());
   std::sort(vector.begin(), vector.end());
   std::copy(vector.begin(), vector.end(), std::ostream_iterator< DataStruct >(out));
 }
--- ezerinia.marta/B4/tools.hpp
+++ ezerinia.marta/B4/tools.hpp
@@ -6,7 +6,6 @@
 #include "dataStruct.hpp"
 
 namespace ezerinia {
-  void read(std::vector< DataStruct > &vector, std::istream &in);
   void task(std::istream &in, std::ostream &out);
 }
 #endif

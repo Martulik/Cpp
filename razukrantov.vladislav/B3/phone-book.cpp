57630d456950f31e1b8d8a2d16393c2f0f24f6ea
Vladislav <vrazbusiness@mail.ru>
2021-05-15 22:43:12 +0300

    add draft of phone book realization

---
+++ razukrantov.vladislav/B3/phone-book.cpp
@@ -0,0 +1,46 @@
+#include "phone-book.hpp"
+
+void razukrantov::PhoneBook::show(std::ostream& out, iterator iterator) const
+{
+	out  << iterator->number << " " << iterator->name << "\n";
+}
+
+void razukrantov::PhoneBook::insertAfter(iterator iterator, const note_t& note)
+{
+	iterator++;
+	note_t temp{ note.name, note.number };
+	recordings_.insert(iterator, temp);
+}
+
+void razukrantov::PhoneBook::insertBefore(iterator iterator, const note_t& note)
+{
+	note_t temp{ note.name, note.number };
+	recordings_.insert(iterator, temp);
+}
+
+void razukrantov::PhoneBook::pushBack(const note_t& note)
+{
+	note_t temp{ note.name, note.number };
+	recordings_.push_back(temp);
+}
+
+void razukrantov::PhoneBook::replace(iterator iterator, const note_t& note)
+{
+	iterator->name = note.name;
+	iterator->number = note.number;
+}
+
+void razukrantov::PhoneBook::erase(iterator iterator)
+{
+	recordings_.erase(iterator);
+}
+
+size_t razukrantov::PhoneBook::size() const
+{
+	return recordings_.size();
+}
+
+bool razukrantov::PhoneBook::empty() const
+{
+	return recordings_.empty();
+}

8386503d3bdaf663db0c9f48fb733e6eaae97243
LysenkoAleksandra <orange260302@gmail.com>
2021-05-15 16:57:52 +0300

    Add improvements of tasks.h

--- lysenko.aleksandra/B3/tasks.h
+++ lysenko.aleksandra/B3/tasks.h
@@ -1,10 +1,12 @@
 #ifndef TASKS_H
 #define TASKS_H
+ 
+#include <iostream>
 
 namespace lysenko
 {
-  int task1();
-  int task2();
+  int task1(std::istream& in, std::ostream& out);
+  int task2(std::ostream& out);
 }
 
 #endif

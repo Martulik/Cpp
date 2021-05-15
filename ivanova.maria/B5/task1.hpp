12e30fb27cacb119a65b86d62a1344e2de0a2aa2
Мария <mashastik2013@,ail.tu>
2021-05-16 02:12:52 +0300

    checked task1

--- ivanova.maria/B5/task1.hpp
+++ ivanova.maria/B5/task1.hpp
@@ -4,18 +4,12 @@
 #include <string>
 #include <vector>
 #include <iterator>
-//#include "Words.hpp"
 
 namespace ivanova
 {
   int task1(std::istream &in, std::ostream &out)
   {
     std::istream_iterator< std::string> iss(in);
-    if (in.fail() || !in.eof())
-    {
-      std::cerr << "reading failed";
-      return 1;
-    }
     std::vector< std::string > vec(iss, std::istream_iterator< std::string > ());
     std::copy(vec.begin(), vec.end(), std::ostream_iterator< std::string > (out, "\n"));
     return 0;

39bebfa8cf4cf79f40d2cea4f92954c358156502
viktoria.dushechkina <byzaya@icloud.com>
2021-05-15 18:12:47 +0300

    change files

--- dushechkina.viktoria/B3/main.cpp
+++ dushechkina.viktoria/B3/main.cpp
@@ -8,16 +8,14 @@ int main(int argc, char* argv[])
   {
     if (argc != 2)
     {
-      std::cerr << "Incorrect number of arguments! Here must be 1 argument!\n";
+      std::cerr << "Incorrect number of arguments\n";
       return 1;
     }
-
     if (dushechkina::taskNumber.find(argv[1]) == dushechkina::taskNumber.end())
     {
-      std::cerr << "Incorrect task number! Task number must be 1 or 2!\n";
+      std::cerr << "Incorrect task number\n";
       return 1;
     }
-
     dushechkina::taskNumber.at(argv[1])(std::cin, std::cout);
   }
   catch (const std::exception& error)

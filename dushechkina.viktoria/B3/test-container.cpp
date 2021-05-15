010e2f5bbaeffcf28096c20c2a7a3eec7f7d497b
viktoria.dushechkina <byzaya@icloud.com>
2021-05-15 17:39:10 +0300

    add test case fixture

--- dushechkina.viktoria/B3/test-container.cpp
+++ dushechkina.viktoria/B3/test-container.cpp
@@ -6,3 +6,17 @@ struct Fixture
  dushechkina::Container fact;
  std::ostringstream output;
 };
+BOOST_FIXTURE_TEST_SUITE(FACTORIAL, Fixture)
+
+BOOST_AUTO_TEST_CASE(correct_work)
+{
+	unsigned k = 1;
+	unsigned n = 1;
+	for (auto i = fact.begin(); i != fact.end(); ++i)
+	{
+		BOOST_CHECK_EQUAL(*i, k);
+		k *= ++n;
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()

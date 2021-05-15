fbb79fffc1051d568c28aa7bc8b774221873bfa5
ilya-pyatizbyantsev <ilya_2.0@mail.ru>
2021-05-15 16:57:25 +0300

    Fix task2

--- pyatizbyantsev.ilya/B1/task2.cpp
+++ pyatizbyantsev.ilya/B1/task2.cpp
@@ -9,9 +9,9 @@
 
 void pyatizbyantsev::taskTwo(const char* fileName)
 {
-  std::ifstream file(fileName);
-  file.open(fileName);
-  if (!file.is_open())
+  std::ifstream input(fileName);
+  input.open(fileName);
+  if (!input.is_open())
   {
     throw std::invalid_argument("Unable to read the file");
   }
@@ -20,28 +20,19 @@ void pyatizbyantsev::taskTwo(const char* fileName)
   size_t size = 0;
   std::unique_ptr< char[] > resultArray  = std::make_unique< char[] >(capacity);
 
-  while (!file.eof())
+  while (!input.eof())
   {
-    file.read(resultArray.get() + size, capacity - size);
-    size += file.gcount();
-
-    if (file.fail() && (!file.eof()))
+    input.read(resultArray.get() + size, capacity - size);
+    size += input.gcount();
+    capacity *= 2;
+    std::unique_ptr< char[] > temp = std::make_unique< char[] >(capacity);
+    for (size_t i = 0; i < size; ++i)
     {
-      throw std::invalid_argument("Input error");
+      temp[i] = resultArray[i];
     }
-
-    if (size == capacity)
-    {
-      capacity *= 2;
-      std::unique_ptr< char[] > temp = std::make_unique< char[] >(capacity);
-      for (size_t i = 0; i < size; ++i)
-      {
-        temp[i] = resultArray[i];
-      }
-      resultArray.swap(temp);
-    }
+    resultArray.swap(temp);
   }
-  file.close();
+  input.close();
   std::vector< char > vector(resultArray.get(), resultArray.get() + size);
   print(vector.begin(), vector.end(), "");
 }

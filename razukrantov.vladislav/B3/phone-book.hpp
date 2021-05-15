4b8680f5a679d45c3ad0ae21fafd0d932601e19f
Vladislav <vrazbusiness@mail.ru>
2021-05-15 22:03:08 +0300

    add phone book draft

---
+++ razukrantov.vladislav/B3/phone-book.hpp
@@ -0,0 +1,36 @@
+#ifndef PHONE_BOOK_HPP
+#define PHONE_BOOK_HPP
+
+#include <list>
+#include <string>
+
+namespace razukrantov
+{
+	class PhoneBook
+	{
+	public:
+		struct note_t
+		{
+			std::string name;
+			std::string number;
+		};
+
+		using iterator = std::list< note_t >::iterator;
+		size_t size() const;
+		bool empty() const;
+		void show(std::ostream& out, iterator iterator) const;
+		void moveNext(iterator iterator);
+		void movePrev(iterator iterator);
+		void insertAfter(iterator iterator, const note_t& note);
+		void insertBefore(iterator iterator, const note_t& note);
+		void replace(iterator iterator, const note_t& note);
+		void pushBack(const note_t& note);
+		void move(iterator iterator, int step) const;
+		iterator begin();
+		iterator end();
+	private:
+		std::list< note_t > recondings_;
+	};
+}
+
+#endif

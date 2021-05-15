994e0b436fecda89bc63caac82dd6b1e82a59785
LysenkoAleksandra <orange260302@gmail.com>
2021-05-15 10:32:20 +0300

    Improve main.cpp by changing type of argv to string

--- lysenko.aleksandra/B3/main.cpp
+++ lysenko.aleksandra/B3/main.cpp
@@ -1,41 +1,21 @@
 #include <iostream>
+#include <string>
 
 #include "tasks.h"
 
-namespace lysenko
+int main(int argc, std::string argv)
 {
-  bool correctData(char* argv);
-}
-
-int main(int argc, char* argv[])
-{
-  if ((argc == 2)&&(lysenko::correctData(argv[1])))
+  if (argc == 2)
   {
-    int taskNumber = std::atoi(argv[1]);
-    if (taskNumber == 1)
+    if (argv == "1")
     {
       return lysenko::task1();
     }
-    else if (taskNumber == 2)
+    else if (argv == "2")
     {
       return lysenko::task2();
     }
   }
-
   return 1;
 }
 
-bool lysenko::correctData(char* argv)
-{
-  int j = 0;
-  while (argv[j] != '\0')
-  {
-    if (argv[j] == ' ')
-    {
-      std::cerr << "Invalid  argument";
-      return 0;
-    }
-    j += 1;
-  }
-  return 1;
-}

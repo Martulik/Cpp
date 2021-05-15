a00de86bb9ab0d4247ea720d035e44111d5c3631
viktoria.dushechkina <byzaya@icloud.com>
2021-05-15 18:05:23 +0300

    fix function

--- dushechkina.viktoria/B3/common-functions.cpp
+++ dushechkina.viktoria/B3/common-functions.cpp
@@ -7,12 +7,12 @@ void checkForFailInput(const std::istream& in)
 {
   if (in.bad())
   {
-    throw std::runtime_error("Something went wrong! The program stops working!\n");
+    throw std::runtime_error("Something went wrong, program stops working\n");
   }
 
   if (in.fail())
   {
-    throw std::invalid_argument("Incorrect input! Written command is too long!\n");
+    throw std::invalid_argument("Incorrect input\n");
   }
 }
 
@@ -20,6 +20,6 @@ void checkForEmptyInput(const std::string& in)
 {
   if (in.empty())
   {
-    throw std::invalid_argument("Incorrect input! Input must have command (and necessary parameters)!\n");
+    throw std::invalid_argument("Incorrect input,it must have command\n");
   }
 }

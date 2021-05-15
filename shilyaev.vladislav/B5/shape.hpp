63dc5eca5b1e67cedd81f2674bd0edf7e72783d0
Vladislav Shilyaev <schilyaev.vlad@gmail.com>
2021-05-15 21:23:14 +0300

    Remove internal function from header

--- shilyaev.vladislav/B5/shape.hpp
+++ shilyaev.vladislav/B5/shape.hpp
@@ -12,7 +12,6 @@ namespace shilyaev {
   using Shape = std::vector< Point >;
   std::ostream &operator<<(std::ostream &ostream, const Point &point);
   std::istream &operator>>(std::istream &istream, Point &point);
-  int calculateDistanceSquared(const Point &a, const Point &b);
   bool isTriangle(const Shape &shape);
   bool isSquare(const Shape &shape);
   bool isRectangle(const Shape &shape);

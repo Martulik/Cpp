9eaa08aedbd86108b75835b9741e158a9eedee75
Shkurov Igor <lilthewil@gmail.com>
2021-05-16 02:22:14 +0300

    edited main to accept task number as an argument

--- shkurov.igor/B3/main.cpp
+++ shkurov.igor/B3/main.cpp
@@ -5,12 +5,27 @@
 
 namespace lab = shkurov;
 
-int main()
+int main(int argc, char* argv[])
 {
-  task1(std::cin, std::cout);
-  std::list<int> lst;
-  std::list<int>::iterator it = lst.begin();
-  it++;
+  if (argc == 2)
+  {
+    if (atoi(argv[1]) == 1)
+    {
+      lab::task1(std::cin, std::cout);
+    }
+    else if (atoi(argv[1]) == 2)
+    {
+      lab::task2();
+    }
+    else
+    {
+      throw std::invalid_argument("Task number is either 1 or 2.");
+    }
+  }
+  else
+  {
+    throw std::invalid_argument("Only one argument allowed: task number.");
+  }
 
   return 0;
 }

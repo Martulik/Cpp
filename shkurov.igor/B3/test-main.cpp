095dbdd3720e4931d783fc93220c8ce6d2502db7
Shkurov Igor <lilthewil@gmail.com>
2021-05-16 05:48:56 +0300

    fixed draft test main

--- shkurov.igor/B3/test-main.cpp
+++ shkurov.igor/B3/test-main.cpp
@@ -1,11 +1,11 @@
+#define BOOST_TEST_MODULE B3
 #include <boost/test/included/unit_test.hpp>
-#define BOOST_TEST_MAIN
 
-BOOST_AUTO_TEST_SUITE(test_suite);
+BOOST_AUTO_TEST_SUITE(test_suite)
 
 BOOST_AUTO_TEST_CASE(test_case)
 {
   BOOST_REQUIRE(1 == 1);
 }
 
-BOOST_AUTO_TEST_SUITE_END();
+BOOST_AUTO_TEST_SUITE_END()

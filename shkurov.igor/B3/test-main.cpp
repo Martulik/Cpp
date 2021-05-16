c3e3d5d6e564bcdeaeb3c108e3853cf933ecdaed
Shkurov Igor <lilthewil@gmail.com>
2021-05-16 19:14:38 +0300

    added factorial test

--- shkurov.igor/B3/test-main.cpp
+++ shkurov.igor/B3/test-main.cpp
@@ -1,11 +1,2 @@
 #define BOOST_TEST_MODULE B3
 #include <boost/test/included/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_suite)
-
-BOOST_AUTO_TEST_CASE(test_case)
-{
-  BOOST_REQUIRE(1 == 1);
-}
-
-BOOST_AUTO_TEST_SUITE_END()

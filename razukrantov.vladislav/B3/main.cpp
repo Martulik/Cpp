f90364013678b1e5cf65a93068552982bfb619fe
Vladislav <vrazbusiness@mail.ru>
2021-05-16 20:19:15 +0300

    add normal version of main

--- razukrantov.vladislav/B3/main.cpp
+++ razukrantov.vladislav/B3/main.cpp
@@ -1,6 +1,35 @@
 ﻿#include <iostream>
+#include <cstring>
+#include <stdexcept>
+
+#include "tasks.hpp"
 
 int main(int argc, char* argv[])
 {
+  try
+  {
+    if (args != 2)
+    {
+      throw(std::invalid_argument("incorrect arguments count"));
+    }
+    if (!std::strcmp(argv[1], "1"))
+    {
+      razukrantov::taskFactorial();
+    }
+    else if (!std::strcmp(argv[1], "2"))
+    {
+      //razukrantov::taskPhoneBook();
+    }
+    else
+    {
+      throw(std::invalid_argument("incorrect arguments"))
+    }
+  }
+  catch (std::invalid_argument& error)
+  {
+    std::cerr << error.what() << "\n";
+    return 2;
+  }
+
   return 0;
 }

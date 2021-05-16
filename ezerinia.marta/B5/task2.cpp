583f266e05769ef7fd86a1ee7d3d60fa4e094e0c
Марта Эзериня <martha@MacBook-Air-Marta.local>
2021-05-16 17:49:52 +0300

    add accumulate for vertices and transform for points

--- ezerinia.marta/B5/task2.cpp
+++ ezerinia.marta/B5/task2.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
-
+#include <numeric>
 #include "shape.hpp"
 
 namespace lab = ezerinia;
@@ -14,47 +14,33 @@ void lab::task2(std::istream &in, std::ostream &out)
     throw std::runtime_error("Read fail");
   }
   std::vector< Shape > shapes((std::istream_iterator< Shape >(in)), std::istream_iterator< Shape >());
-  auto vertices = 0;
-  int triangles = 0;
-  int squares = 0;
-  int rectangles = 0;
 
-  std::vector< Point > points;
-//  for (auto &&shape : shapes) {
-//    vertices += shape.size();
-//    if (shape.size() == 3) {
-//      triangles++;
-//    } else if (shape.size() == 4) {
-//      rectangles++;
-//      if (isSideEqual(shape)) {
-//        squares++;
-//      }
-//    }
-//    if (shape.size() != 5) {
-//      points.push_back(shape.front());
-//    }
-//  }
-
-  for (auto &&shape : shapes) {
-    vertices += shape.size();
-  }
-
-  triangles = std::count_if(shapes.begin(), shapes.end(),
-                            [](const Shape &shape) {
-                              return shape.size() == 3;
-                            });
-  squares = std::count_if(shapes.begin(), shapes.end(),
-                          [](const Shape &shape) {
-                            return shape.size() == 4 && isSideEqual(shape);
-                          });
-  rectangles = std::count_if(shapes.begin(), shapes.end(),
-                             [](const Shape &shape) {
-                               return shape.size() == 4;
-                             });
+  int vertices = std::accumulate(shapes.begin(), shapes.end(), 0,
+                                 [](int vertices_, const Shape &shape) {
+                                   return vertices_ + shape.size();
+                                 });
+  int triangles = std::count_if(shapes.begin(), shapes.end(),
+                                [](const Shape &shape) {
+                                  return shape.size() == 3;
+                                });
+  int squares = std::count_if(shapes.begin(), shapes.end(),
+                              [](const Shape &shape) {
+                                return shape.size() == 4 && isSideEqual(shape);
+                              });
+  int rectangles = std::count_if(shapes.begin(), shapes.end(),
+                                 [](const Shape &shape) {
+                                   return shape.size() == 4;
+                                 });
   shapes.erase(std::remove_if(shapes.begin(), shapes.end(),
                               [](const Shape &shape) {
                                 return shape.size() == 5;
                               }), shapes.end());
+
+  std::vector< Point > points;
+  std::transform(shapes.begin(), shapes.end(), std::back_inserter(points),
+                 [](const Shape &shape) {
+                   return shape.front();
+                 });
 
   std::sort(shapes.begin(), shapes.end());
   out << "Vertices: " << vertices << "\n";

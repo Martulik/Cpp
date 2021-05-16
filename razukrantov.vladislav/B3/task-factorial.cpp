1ea0ce29450e86af078fc4f8aa6facd12b179f88
Vladislav <vrazbusiness@mail.ru>
2021-05-16 21:07:48 +0300

    add std::ostream in factorial task

--- razukrantov.vladislav/B3/task-factorial.cpp
+++ razukrantov.vladislav/B3/task-factorial.cpp
@@ -1,16 +1,16 @@
 #include "tasks.hpp"
 
-#include <iostream>
+#include <ostream>
 #include <algorithm>
 #include <iterator>
 
 #include "container.hpp"
 
-void razukrantov::taskFactorial()
+void razukrantov::taskFactorial(std::ostream& out)
 {
 	razukrantov::Container container;
-	std::copy(container.begin(), container.end(), std::ostream_iterator< size_t >(std::cout, " "));
-	std::cout << "\n";
-	std::reverse_copy(container.begin(), container.end(), std::ostream_iterator< size_t >(std::cout, " "));
-	std::cout << "\n";
+	std::copy(container.begin(), container.end(), std::ostream_iterator< size_t >(out, " "));
+	out << "\n";
+	std::reverse_copy(container.begin(), container.end(), std::ostream_iterator< size_t >(out, " "));
+	out << "\n";
 }

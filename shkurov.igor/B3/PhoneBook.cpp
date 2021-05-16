03485aaffebcd12d9a419989c32f115aa5b47b6f
Shkurov Igor <lilthewil@gmail.com>
2021-05-16 07:02:07 +0300

    changed insert method in PhoneBook

--- shkurov.igor/B3/PhoneBook.cpp
+++ shkurov.igor/B3/PhoneBook.cpp
@@ -18,12 +18,26 @@ lab::PhoneBook::iterator_t lab::PhoneBook::end()
 
 void lab::PhoneBook::insertForward(lab::PhoneBook::const_iterator_t& it, const lab::PhoneBook::node_t& node)
 {
-  container_.insert(++it, node);
+  if (it == std::prev(container_.end()))
+  {
+    container_.push_back(node);
+  }
+  else
+  {
+    container_.insert(++it, node);
+  }
 }
 
 void lab::PhoneBook::insertBackward(lab::PhoneBook::const_iterator_t& it, const lab::PhoneBook::node_t& node)
 {
-  container_.insert(it, node);
+  if (it == container_.begin())
+  {
+    container_.push_front(node);
+  }
+  else
+  {
+    container_.insert(it, node);
+  }
 }
 
 lab::PhoneBook::node_t lab::PhoneBook::pullCurrent(const lab::PhoneBook::const_iterator_t& it) const

2476d5ed710d17227f61bee9cfae594689df3777
Vladislav <vrazbusiness@mail.ru>
2021-05-16 19:51:26 +0300

    update phone book interface files

--- razukrantov.vladislav/B3/phone-book-interface.cpp
+++ razukrantov.vladislav/B3/phone-book-interface.cpp
@@ -18,3 +18,80 @@ void razukrantov::PhoneBookInterface::add(const note_t& note)
 		bookmarks_["current"] = book_->begin();
 	}
 }
+
+void razukrantov::PhoneBookInterface::store(const std::string& bookMark, const std::string& newBookMark)
+{
+
+}
+
+void razukrantov::PhoneBookInterface::insertBefore(const std::string& bookMark, const note_t& note)
+{
+	if (bookMark == "current" && book_->empty())
+	{
+		add(note);
+		return;
+	}
+	book_.add
+}
+
+void razukrantov::PhoneBookInterface::insertAfter(const std::string& bookMark, const note_t& note)
+{
+	if (bookMark == "current" && book_->empty())
+	{
+		add(note);
+		return;
+	}
+}
+
+void razukrantov::PhoneBookInterface::erase(const std::string& bookMark)
+{
+	iteratorType iterator = bookmarks_.find(bookMark);
+	if (iterator != bookmarks_.end())
+	{
+		iteratorType temp = iterator;
+		iteratorType i = bookmarks_.begin();
+		while (i != bookmarks_.end())
+		{
+			if (i->second == temp->second)
+			{
+				if (std::next(i->second) == book_->end())
+				{
+					i->second = book_->begin();
+				}
+				else 
+				{
+					i->second = std::next(temp->second);
+				}
+			}
+			i++;
+		}
+		book_->erase(temp->second);
+	}
+}
+
+void razukrantov::PhoneBookInterface::show(const std::string& bookMark, std::ostream& out)
+{
+	iteratorType iterator = bookmarks_.find(bookMark);
+	return *iterator->second;
+}
+
+void razukrantov::PhoneBookInterface::move(const std::string& bookMark, const std::string& steps)
+{
+	iteratorType iterator = bookmarks_.find(bookmark);
+	if (steps == "first")
+	{
+		iterator->second = book_->begin();
+	}
+	else if (steps == "last")
+	{
+		if (book_->empty())
+		{
+			iterator->second = book->end();
+		}
+		else
+		{
+			iterator->second = std::prev(book_->end());
+		}
+	}
+	//else if 
+}
--- razukrantov.vladislav/B3/phone-book-interface.hpp
+++ razukrantov.vladislav/B3/phone-book-interface.hpp
@@ -13,8 +13,15 @@ namespace razukrantov
 	{
 	public:
 		using note_t = razukrantov::PhoneBook::note_t;
+		using iteratorType = std::map < std::string, razukrantov::PhoneBook::iterator >::iterator;
 		PhoneBookInterface(PhoneBook& book);
 		void add(const note_t& note);
+		void store(const std::string& bookMark, const std::string& newBookMark);
+		void insertBefore(const std::string& bookMark, const note_t& note);
+		void insertAfter(const std::string& bookMark, const note_t& note);
+		void erase(const std::string& bookMark);
+		void show(const std::string& bookMark, std::ostream& out);
+		void move(const std::string& bookMark, const std::string& steps);
 
 	private:
 		std::unique_ptr< razukrantov::PhoneBook > book_;

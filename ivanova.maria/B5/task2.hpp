bbfd336631ed08fcfb07948fadf276c1f17b5a5f
Мария <mashastik2013@,ail.tu>
2021-05-17 15:55:46 +0300

    finisheded task2

--- ivanova.maria/B5/task2.hpp
+++ ivanova.maria/B5/task2.hpp
@@ -1,12 +1,96 @@
-//#ifndef B5_TASK2_HPP
-//#define B5_TASK2_HPP
-//#include <iosfwd>
-//
-//namespace ivanova
-//{
-//  int task2(std::istream &in, std::ostream &out)
-//  {
-//    return 0;
-//  }
-//}
-//#endif
+#ifndef B5_TASK2_HPP
+#define B5_TASK2_HPP
+#include <iosfwd>
+#include <sstream>
+#include <iterator>
+#include <numeric>
+#include "point.hpp"
+#include "count.hpp"
+
+namespace ivanova
+{
+  std::istream &operator >>(std::istream &in, Shape &shp);
+  std::ostream &operator <<(std::ostream &out, const std::vector < Shape > &shp);
+  std::ostream &operator <<(std::ostream &out, const Shape &elem);
+  void doCount(count &shapes, std::vector < Shape > &vec);
+  void deletePentagons(std::vector < Shape > &vec);
+  bool compare(Shape &elem1, Shape &elem2);
+  void print(const std::vector < Shape > &shp, const count &info, std::ostream &out);
+
+  int task2(std::istream &in, std::ostream &out)
+  {
+    std::vector < Shape > shapes;
+    std::istream_iterator< Shape > isi(in);
+    shapes = std::move(std::vector < Shape > (isi, std::istream_iterator< Shape >()));
+    count info;
+    doCount(info, shapes);
+    deletePentagons(shapes);
+    std::vector< Point > points(shapes.size());
+    std::for_each(shapes.begin(), shapes.end(), [&points](const Shape &elem){ points.push_back(elem.at(0)); });
+    std::sort(shapes.begin(), shapes.end(), compare);
+    out << "Points: ";
+    std::ostream_iterator < Point > oit(out, " ");
+    std::copy(points.begin(), points.end(), oit);
+    print(shapes, info, out);
+  }
+}
+
+std::istream &ivanova::operator >>(std::istream &in, Shape &shp)
+{
+  size_t vertices = 0;
+  std::string line;
+  std::getline(in, line);
+  if (line.empty())
+  {
+    std::cerr << "invalid input";
+    exit(1);
+  }
+  std::istringstream iss(line);
+  iss >> vertices;
+  std::istream_iterator < Point > isi(iss);
+  shp = std::move(std::vector< Point > (isi, std::istream_iterator< Point >()));
+  return in;
+}
+
+std::ostream &ivanova::operator <<(std::ostream &out, const std::vector < Shape > &shp)
+{
+  std::ostream_iterator< Shape > oit(out);
+  std::copy(shp.begin(), shp.end(), oit);
+  return out;
+}
+
+std::ostream &ivanova::operator <<(std::ostream &out, const Shape &elem)
+{
+  std::ostream_iterator< Point > oit(out);
+  std::copy(elem.begin(), elem.end(), oit);
+  return out;
+}
+
+
+void ivanova::doCount(count &shapes, std::vector < Shape > &vec)
+{
+  std::for_each(vec.begin(), vec.end(),[&shapes](const Shape &elem){ shapes.countShape(elem); });
+}
+
+void ivanova::deletePentagons(std::vector < ivanova::Shape > &vec)
+{
+  auto it = vec.begin();
+  using shp = const Shape;
+  std::for_each(vec.begin(), vec.end(), [&vec, &it](shp &elem){ if (isPentagon(elem)) { vec.erase(it); } it++;});
+}
+
+bool ivanova::compare(Shape &elem1, Shape &elem2)
+{
+  return (elem1.size() < elem2.size());
+}
+
+void ivanova::print(const std::vector < Shape > &shp, const count &info, std::ostream &out)
+{
+  out << "Vertices: " << info.vertices << "\n";
+  out << "Triangles: " << info.triangles << '\n';
+  out << "Rectangles: " << info.rectangles << '\n';
+  out << "Squares: " << info.squares << '\n';
+  out << "Points: ";
+  out << shp;
+}
+#endif

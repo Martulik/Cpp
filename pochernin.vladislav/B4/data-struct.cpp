01cd26d0692b76c2fb9bbc6d5e24dcaeb57a1377
vladislav-pochernin <vspochernin@gmail.com>
2021-05-17 09:05:13 +0300

    Add operator<

---
+++ pochernin.vladislav/B4/data-struct.cpp
@@ -0,0 +1,17 @@
+#include "data-struct.hpp"
+
+bool pochernin::operator<(const DataStruct& lhs, const DataStruct& rhs)
+{
+  if (lhs.key1 != rhs.key1)
+  {
+    return lhs.key1 < rhs.key1;
+  }
+  else if (lhs.key2 != rhs.key2)
+  {
+    return lhs.key2 < rhs.key2;
+  }
+  else
+  {
+    return lhs.str < rhs.str;
+  }
+}

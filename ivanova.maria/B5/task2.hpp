00060212b0ade22bc93dfa5e16920eea15fc3c7f
Мария <mashastik2013@,ail.tu>
2021-05-17 20:33:11 +0300

    checked test

--- ivanova.maria/B5/task2.hpp
+++ ivanova.maria/B5/task2.hpp
@@ -73,7 +73,7 @@ std::istream &ivanova::operator >>(std::istream &in, Shape &shp)
 std::ostream &ivanova::operator <<(std::ostream &out, const Shape &elem)
 {
   std::ostream_iterator< Point > oit(out);
-  out << elem.size() << " ";
+  out << elem.size();
   std::copy(elem.begin(), elem.end(), oit);
   return out;
 }

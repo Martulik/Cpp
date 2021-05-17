43f0ce3a1230890ffc3640befeb99f2dac21822e
Мария <mashastik2013@,ail.tu>
2021-05-17 20:10:59 +0300

    checked test

--- ivanova.maria/B5/test-task2.cpp
+++ ivanova.maria/B5/test-task2.cpp
@@ -16,7 +16,7 @@ BOOST_AUTO_TEST_CASE(correctInput)
     BOOST_CHECK_EQUAL(shape[0].y, 36);
     BOOST_CHECK_EQUAL(shape[1].x, 3);
     BOOST_CHECK_EQUAL(shape[1].y, -2225);
-    BOOST_CHECK_EQUAL(shape[2].x, --435);
+    BOOST_CHECK_EQUAL(shape[2].x, -435);
     BOOST_CHECK_EQUAL(shape[2].y, 8726);
     BOOST_CHECK_EQUAL(shape[3].x, 0);
     BOOST_CHECK_EQUAL(shape[3].y, -1);
@@ -29,7 +29,7 @@ BOOST_AUTO_TEST_SUITE(testShapeOut)
 BOOST_AUTO_TEST_CASE(output)
   {
     std::ostringstream oss;
-    iva::Shape shape{{12; 36}  {3; -2225}  {-435; 8726} {0; -1}};
+    iva::Shape shape{{12, 36}  {3, -2225}  {-435, 8726} {0, -1}};
     oss << shape;
     BOOST_CHECK_EQUAL(oss.str(), "4  (12; 36)  (3; -2225)  (-435; 8726) (0; -1)");
   }

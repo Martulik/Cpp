c12a1d07ce12a01765f8554cdb5a11d6105ac707
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-05-17 15:54:24 +0300

    Add solution for the task

--- savchuk.aleksei/B4/main.cpp
+++ savchuk.aleksei/B4/main.cpp
@@ -1,4 +1,37 @@
+#include <vector>
+#include <iterator>
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+
+#include "data-struct.hpp"
+#include "comparator.hpp"
+
+namespace lab = savchuk;
+
 int main()
 {
-  return 0;
+  try
+  {
+    std::istream_iterator< lab::DataStruct > first(std::cin);
+    std::istream_iterator< lab::DataStruct > last;
+    std::vector< lab::DataStruct > v(first, last);
+    std::sort(v.begin(), v.end(), lab::compare);
+    std::copy(v.begin(), v.end(), std::ostream_iterator< lab::DataStruct >(std::cout, "\n"));
+  }
+  catch (const std::invalid_argument& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  catch (const std::runtime_error& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 2;
+  }
+  catch (...)
+  {
+    std::cerr << "Internal program error\n";
+    return 2;
+  }
 }

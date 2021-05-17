68bc4e16562ee7d6c97874a0a1cf7cd87af1c433
ferapontov-mikhail <mferapon@gmail.com>
2021-05-17 17:31:29 +0300

    Added unit tests

---
+++ ferapontov.mikhail/B3/testFactorial.cpp
@@ -0,0 +1,25 @@
+#include <boost/test/unit_test.hpp>
+
+#include "container.hpp"
+
+void startFactorialTest()
+{
+  ferapontov::Container container;
+  unsigned int factorial = 1;
+  ferapontov::Container::Iterator myFactorial = container.begin();
+  for(unsigned int i = 1; i < 11; i++)
+  {
+    factorial *= i;
+    BOOST_CHECK_EQUAL(factorial, *myFactorial);
+    myFactorial++;
+  }
+}
+
+BOOST_AUTO_TEST_SUITE(testFactorial)
+
+BOOST_AUTO_TEST_CASE(fact)
+{
+  startFactorialTest();
+}
+
+BOOST_AUTO_TEST_SUITE_END()

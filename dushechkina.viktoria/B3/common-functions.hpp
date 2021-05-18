4038b9326035d1364d334629a9c5e2aa8fffb3d1
viktoria.dushechkina <byzaya@icloud.com>
2021-05-18 20:41:53 +0300

    fix CG

--- dushechkina.viktoria/B3/common-functions.hpp
+++ dushechkina.viktoria/B3/common-functions.hpp
@@ -2,10 +2,9 @@
 #define COMMON_FUNCTIONS_HPP
 #include <iostream>
 #include <string>
-
 namespace dushechkina
 {
-	void checkForFailInput(const std::istream& in);
-	void checkForEmptyInput(const std::string& in);
+ void checkForFailInput(const std::istream& in);
+ void checkForEmptyInput(const std::string& in);
 }
 #endif

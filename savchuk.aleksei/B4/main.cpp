1da33bb28d045b036a361fe8711ec70c91ee8215
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-05-19 00:24:32 +0300

    Remove exception handling

--- savchuk.aleksei/B4/main.cpp
+++ savchuk.aleksei/B4/main.cpp
@@ -2,7 +2,6 @@
 #include <iterator>
 #include <iostream>
 #include <algorithm>
-#include <stdexcept>
 
 #include "data-struct.hpp"
 #include "comparator.hpp"
@@ -11,31 +10,14 @@ namespace lab = savchuk;
 
 int main()
 {
-  try
+  std::istream_iterator< lab::DataStruct > first(std::cin);
+  std::istream_iterator< lab::DataStruct > last;
+  std::vector< lab::DataStruct > v(first, last);
+  if (std::cin.fail() && !std::cin.eof())
   {
-    std::istream_iterator< lab::DataStruct > first(std::cin);
-    std::istream_iterator< lab::DataStruct > last;
-    std::vector< lab::DataStruct > v(first, last);
-    if (std::cin.fail() && !std::cin.eof())
-    {
-      throw std::runtime_error("Input error");
-    }
-    std::sort(v.begin(), v.end(), lab::compare);
-    std::copy(v.begin(), v.end(), std::ostream_iterator< lab::DataStruct >(std::cout, "\n"));
-  }
-  catch (const std::invalid_argument& e)
-  {
-    std::cerr << e.what() << '\n';
+    std::cerr << "Input error\n";
     return 1;
   }
-  catch (const std::runtime_error& e)
-  {
-    std::cerr << e.what() << '\n';
-    return 2;
-  }
-  catch (...)
-  {
-    std::cerr << "Internal program error\n";
-    return 2;
-  }
+  std::sort(v.begin(), v.end(), lab::compare);
+  std::copy(v.begin(), v.end(), std::ostream_iterator< lab::DataStruct >(std::cout, "\n"));
 }

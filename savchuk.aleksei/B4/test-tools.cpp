536ebab9bc8c33918e7ea7fc15bc74e9c1cdb1d8
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-05-19 00:32:25 +0300

    Change test tool functions

--- savchuk.aleksei/B4/test-tools.cpp
+++ savchuk.aleksei/B4/test-tools.cpp
@@ -11,7 +11,6 @@ namespace lab = savchuk;
 void lab::checkCorrectInput(std::istringstream& ss, const char* data)
 {
   DataStruct ds;
-  ss.clear();
   ss.str(data);
   BOOST_CHECK(ss >> ds);
 }
@@ -29,5 +28,5 @@ void lab::checkInvalidKey(std::istringstream& ss, const char* data)
   DataStruct ds;
   ss.clear();
   ss.str(data);
-  BOOST_CHECK_THROW(ss >> ds, std::invalid_argument);
+  BOOST_CHECK(!(ss >> ds));
 }

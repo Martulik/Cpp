1b53d2431abf63e4b4f29404cf2fcfa9d4375620
pozdnyakov-artemy <aapozdnyakove@gmail.com>
2021-05-21 21:44:07 +0100

    B4 v1

--- pozdnyakov.artemy/B4/data-struct.hpp
+++ pozdnyakov.artemy/B4/data-struct.hpp
@@ -2,6 +2,7 @@
 #define DATA_STRUCT_HPP
 
 #include <string>
+#include <iosfwd>
 
 namespace pozdnyakov
 {
@@ -11,6 +12,10 @@ namespace pozdnyakov
     int key2;
     std::string str;
   };
+  std::ostream& operator<<(std::ostream& out, const DataStruct& obj);
+  std::istream& operator>>(std::istream& in, DataStruct& obj);
+  int extractKey(std::string& str);
+  bool comparator(DataStruct s1, DataStruct s2);
 }
 
 #endif
--- pozdnyakov.artemy/B4/main.cpp
+++ pozdnyakov.artemy/B4/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 #include "data-struct.hpp"
+
+namespace poz = pozdnyakov;
 
 int main()
 {
-  
+  std::istream_iterator< poz::DataStruct > begin(std::cin);
+  std::istream_iterator< poz::DataStruct > eos;
+  std::vector< poz::DataStruct > vector(begin, eos);
+  std::sort(vector.begin(), vector.end(), *poz::comparator);
+  std::ostream_iterator< poz::DataStruct > out(std::cout, "\n");
+  std::copy(vector.begin(), vector.end(), out);
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    return -2;
+  }
+  return 0;
 }

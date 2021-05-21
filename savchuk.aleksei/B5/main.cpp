73c3da2e998b45f92a7666faa01d27514ad48c38
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-05-21 22:06:06 +0300

    Add solution for task 1

--- savchuk.aleksei/B5/main.cpp
+++ savchuk.aleksei/B5/main.cpp
@@ -1,4 +1,35 @@
+#include <cstring>
+#include <iostream>
+#include <stdexcept>
+
+#include "tasks.hpp"
+
+namespace lab = savchuk;
+
 int main(int argc, char** argv)
 {
-  return 0;
+  try
+  {
+    if (argc != 2)
+    {
+      throw std::runtime_error("Invalid command line arguments");
+    }
+    if (!strcmp(argv[1], "1"))
+    {
+      lab::doTask1(std::cin, std::cout);
+    }
+    else if (!strcmp(argv[1], "2"))
+    {
+      // lab::doTask2(std::cin, std::cout);
+    }
+    else
+    {
+      throw std::runtime_error("Invalid command line arguments");
+    }
+  }
+  catch (const std::runtime_error& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
 }
---
+++ savchuk.aleksei/B5/tasks.hpp
@@ -0,0 +1,11 @@
+#ifndef TASKS_HPP
+#define TASKS_HPP
+
+#include <iosfwd>
+
+namespace savchuk
+{
+  void doTask1(std::istream& is, std::ostream& os);
+}
+
+#endif

ecf635d10acfe4bc4d9cba4548974f1a239452de
Vladislav <vrazbusiness@mail.ru>
2021-05-22 13:00:53 +0300

    add main

--- razukrantov.vladislav/B4/main.cpp
+++ razukrantov.vladislav/B4/main.cpp
@@ -1,4 +1,19 @@
-﻿int main()
+﻿#include <iterator>
+#include <vector>
+
+#include "data-struct.hpp"
+
+int main()
 {
-  return 0;
+  std::istream_iterator< razukrantov::DataStruct > start(std::cin);
+  std::istream_iterator< razukrantov::DataStruct > finish;
+  std::vector < razukrantov::DataStruct > vector(start, finish);
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    std::current_exception << "Incorrect input" << "\n";
+    return 1;
+  }
+  std::sort(vec.begin(), vec.end());
+  std::ostream_iterator< razukrantov::DataStruct > out(std::count, "\n");
+  std::copy(vector.begin(), vector.end(), out);
 }

6dff885bd7e424d32b2597bd403a0b8d6da736c0
ferapontov-mikhail <mferapon@gmail.com>
2021-05-22 18:18:09 +0300

    Added unit tests
    accidentally committed everything in another branch, I'm transferring it here now

---
+++ ferapontov.mikhail/B4/test-sort.cpp
@@ -0,0 +1,22 @@
+#include <boost/test/unit_test.hpp>
+
+#include <vector>
+#include <algorithm>
+#include "DataStruct.hpp"
+
+BOOST_AUTO_TEST_SUITE(TestSort)
+
+BOOST_AUTO_TEST_CASE(sort)
+{
+  std::vector< ferapontov::DataStruct > unsortedVec;
+  unsortedVec = {{5, 5, "data"}, {-1, -1, "sort"}, {-4, 2, "sorted moment"}};
+  std::vector< ferapontov::DataStruct > sortedVec;
+  sortedVec = {{-4, 2, "sorted moment"}, {-1, -1, "sort"}, {5, 5, "data"}};
+  std::sort(unsortedVec.begin(), unsortedVec.end());
+  for (size_t i = 0; i < sortedVec.size(); i++)
+  {
+    BOOST_CHECK_EQUAL(unsortedVec[i], sortedVec[i]);
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()

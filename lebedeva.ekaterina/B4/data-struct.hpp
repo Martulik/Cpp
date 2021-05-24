f6040d9a0bcdc1498d4bdb0b40ac309278cf283b
kate-lebedeva <katty.papina@gmail.com>
2021-05-24 23:56:59 +0300

    Added dataStruct

---
+++ lebedeva.ekaterina/B4/data-struct.hpp
@@ -0,0 +1,14 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+#include <string>
+
+namespace lebedeva
+{
+  struct DataStruct
+  {
+    int key1;
+    int key2;
+    std::string str;
+  };
+}
+#endif
---
+++ lebedeva.ekaterina/B4/task.cpp
@@ -0,0 +1,7 @@
+#include "task.hpp"
+#include "data-struct.hpp"
+
+void lebedeva::doTask1(std::ostream& out)
+{
+
+}

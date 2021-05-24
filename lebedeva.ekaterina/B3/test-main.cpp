01ab10d2d2f1e42673fbee7bc2c238744e5ee74f
kate-lebedeva <katty.papina@gmail.com>
2021-05-24 21:08:36 +0300

    Added boost test suit for Container

--- lebedeva.ekaterina/B3/test-main.cpp
+++ lebedeva.ekaterina/B3/test-main.cpp
@@ -1,8 +1,19 @@
 #define BOOST_TEST_MODULE mytests
 #include <boost/test/included/unit_test.hpp>
+#include "container.hpp"
 
-BOOST_AUTO_TEST_CASE(myTestCase)
+BOOST_AUTO_TEST_SUITE(testContainer)
+BOOST_AUTO_TEST_CASE(factorialTest)
 {
-  BOOST_TEST(1 == 1);
-  BOOST_TEST(true);
+  lebedeva::Container factContainer;
+  using Type = lebedeva::Container::valueType;
+  using iter = lebedeva::Container::Iterator;
+  Type elem = 1;
+  Type factorial = 1;
+  for (iter i = factContainer.begin(); i != factContainer.end(); i++)
+  {
+    factorial *= elem++;
+    BOOST_CHECK_EQUAL(*i, factorial);
+  }
 }
+BOOST_AUTO_TEST_SUITE_END()

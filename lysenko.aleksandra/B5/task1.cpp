49b2f84591d3e9afc0fdaf5df441b514e9fd4b11
LysenkoAleksandra <orange260302@gmail.com>
2021-05-24 15:13:44 +0300

    Add istream_iterators

--- lysenko.aleksandra/B5/task1.cpp
+++ lysenko.aleksandra/B5/task1.cpp
@@ -9,17 +9,9 @@
 
 int lysenko::task1(std::istream& in, std::ostream& out)
 {
-  std::set< std::string > uniqueWords;
-  while (!in.eof())
-  {
-    in >> std::ws;
-    std::string word;
-    in >> word;
-    if (word.size() != 1)
-    {
-      uniqueWords.insert(word);
-    }
-  }
+  std::istream_iterator< std::string > begin(in);
+  std::istream_iterator< std::string > end;
+  std::set< std::string > uniqueWords(begin, end);
   std::copy(uniqueWords.begin(), uniqueWords.end(), std::ostream_iterator< std::string >(out, "\n"));
   return 0;
 }

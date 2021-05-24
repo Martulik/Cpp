69b8b52f5ab5c807fb963fb5ee4e897cacda2572
vladislav-pochernin <vspochernin@gmail.com>
2021-05-25 02:03:18 +0300

    Count vertices

--- pochernin.vladislav/B5/task2.cpp
+++ pochernin.vladislav/B5/task2.cpp
@@ -12,4 +12,6 @@ void pochernin::task2(std::istream& in, std::ostream& out)
 {
   std::vector< Shape > shapes;
   std::copy(std::istream_iterator< Shape>(in), std::istream_iterator< Shape >(), std::back_inserter(shapes));
+
+  unsigned int countOfVertices = std::accumulate(shapes.begin(), shapes.end(), 0, pochernin::sumVertices);
 }

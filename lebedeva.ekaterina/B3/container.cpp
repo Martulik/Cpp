87daa5e1e40efcb0eb2327190564d15f728ecdcb
kate-lebedeva <katty.papina@gmail.com>
2021-05-24 03:02:03 +0300

    Added container and iterator classes for task 2

---
+++ lebedeva.ekaterina/B3/container.cpp
@@ -0,0 +1,87 @@
+#include "container.hpp"
+#include <cassert>
+
+namespace lebedeva
+{
+  const Container::valueType MAX = 11;
+  const Container::valueType MIN = 1;
+
+  constexpr Container::valueType factorial(const Container::valueType i)
+  {
+    if (i == MIN)
+    {
+      return 1;
+    }
+    return i * factorial(i - 1);
+  }
+}
+
+lebedeva::Container::Iterator::Iterator():
+  index_(0),
+  value_(1)
+{}
+
+lebedeva::Container::Iterator::Iterator(valueType i, valueType value):
+  index_(i),
+  value_(value)
+{}
+
+const lebedeva::Container::valueType* lebedeva::Container::Iterator::operator->()
+{
+  return std::addressof(value_);
+}
+
+const lebedeva::Container::valueType& lebedeva::Container::Iterator::operator*()
+{
+  return value_;
+}
+
+lebedeva::Container::Iterator& lebedeva::Container::Iterator::operator++()
+{
+  assert(index_ < MAX);
+  index_++;
+  value_ *= index_;
+  return *this;
+}
+
+lebedeva::Container::Iterator lebedeva::Container::Iterator::operator++(int)
+{
+  Iterator iter = *this;
+  ++(*this);
+  return iter;
+}
+
+lebedeva::Container::Iterator& lebedeva::Container::Iterator::operator--()
+{
+  assert(index_ > MIN);
+  index_--;
+  value_ /= index_;
+  return *this;
+}
+
+lebedeva::Container::Iterator lebedeva::Container::Iterator::operator--(int)
+{
+  Iterator iter = *this;
+  --(*this);
+  return iter;
+}
+
+bool lebedeva::Container::Iterator::operator==(const Iterator& rhs) const
+{
+  return (index_ == rhs.index_);
+}
+
+bool lebedeva::Container::Iterator::operator!=(const Iterator& rhs) const
+{
+  return (index_ != rhs.index_);
+}
+
+lebedeva::Container::Iterator lebedeva::Container::begin() const
+{
+  return { MIN, factorial(MIN) };
+}
+
+lebedeva::Container::Iterator lebedeva::Container::end() const
+{
+  return { MAX, factorial(MAX) };
+}
---
+++ lebedeva.ekaterina/B3/container.hpp
@@ -0,0 +1,36 @@
+#ifndef CONTAINER_HPP
+#define CONTAINER_HPP
+#include <iterator>
+
+namespace lebedeva
+{
+  class Container
+  {
+  public:
+    Container() = default;
+    using valueType = unsigned int;
+    class Iterator : std::iterator< std::bidirectional_iterator_tag, valueType >
+    {
+    public:
+      const valueType* operator->();
+      const valueType& operator*();
+      Iterator& operator++();
+      Iterator operator++(int);
+      Iterator& operator--();
+      Iterator operator--(int);
+
+      bool operator==(const Iterator& rhs) const;
+      bool operator!=(const Iterator& rhs) const;
+    private:
+      friend class Container;
+      valueType index_;
+      valueType value_;
+
+      Iterator();
+      Iterator(valueType i, valueType value);
+    };
+    Iterator begin() const;
+    Iterator end() const;
+  };
+}
+#endif

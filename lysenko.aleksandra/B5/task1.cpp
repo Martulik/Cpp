d29a42ddecadc8c64cfe70d1de082b81bdc18614
LysenkoAleksandra <orange260302@gmail.com>
2021-05-24 14:54:35 +0300

    Fix reading of word

--- lysenko.aleksandra/B5/task1.cpp
+++ lysenko.aleksandra/B5/task1.cpp
@@ -12,24 +12,11 @@ int lysenko::task1(std::istream& in, std::ostream& out)
   std::set< std::string > uniqueWords;
   while (!in.eof())
   {
-    std::streampos numberOfReadLetters = 0;
-    if (!((in.peek() == ' ') || (in.peek() == '\n') || (in.peek() == '\t')))
+    std::string word;
+    in >> word;
+    if (word.size() != 1)
     {
-      char wordLetter;
-      in >> wordLetter;
-      numberOfReadLetters += 1;;
-    }
-    else
-    {
-      char delimiter;
-      in >> delimiter;
-      std::string word;
-      in.seekg(in.tellg() - numberOfReadLetters);
-      getline(in, word, delimiter);
-      if (word.size() != 0)
-      {
-        uniqueWords.insert(word);
-      }
+      uniqueWords.insert(word);
     }
   }
   std::copy(uniqueWords.begin(), uniqueWords.end(), std::ostream_iterator< std::string >(out, "\n"));

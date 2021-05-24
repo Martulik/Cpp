de20d6ddc513ac873b27b7a0f5bba6593a3c4f57
vladislav-pochernin <vspochernin@gmail.com>
2021-05-24 22:15:12 +0300

    Add isNumber() function

--- pochernin.vladislav/B5/functions.cpp
+++ pochernin.vladislav/B5/functions.cpp
@@ -21,3 +21,30 @@ int pochernin::getTaskNumber(const char* string)
   }
   return argument;
 }
+
+bool pochernin::isNumber(const std::string& str)
+{
+  if (str.size() == 0)
+  {
+    return false;
+  }
+  else if ((str.size() == 1) && !std::isdigit(str[0]))
+  {
+    return false;
+  }
+  else
+  {
+    if ((str[0] != '-') && !std::isdigit(str[0]))
+    {
+      return false;
+    }
+    for (size_t i = 1; i < str.length(); i++)
+    {
+      if (!std::isdigit(str[i]))
+      {
+        return false;
+      }
+    }
+  }
+  return true;
+}
--- pochernin.vladislav/B5/functions.hpp
+++ pochernin.vladislav/B5/functions.hpp
@@ -1,9 +1,12 @@
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
 
+#include <string>
+
 namespace pochernin
 {
   int getTaskNumber(const char* string);
+  bool isNumber(const std::string& str);
 }
 
 #endif

d3be1a4d8b0bb4c818e734cca24d4127c4d6d019
Islam <murzakanovislam21@gmail.com>
2021-05-25 18:26:37 +0300

    CG fixes

--- murzakanov.islam/B5/test-point.cpp
+++ murzakanov.islam/B5/test-point.cpp
@@ -4,44 +4,45 @@
 #include "point.hpp"
 
 BOOST_AUTO_TEST_SUITE(test_point)
-BOOST_AUTO_TEST_CASE(firstWrongPointInput)
-{
-  std::string line("(152;\n123)");
-  std::stringstream sin(line);
-  murzakanov::Point point;
-  BOOST_CHECK(!(sin >> point));
-}
 
-BOOST_AUTO_TEST_CASE(secondWrongPointInput)
-{
-  std::string line("(152;123\n)");
-  std::stringstream sin(line);
-  murzakanov::Point point;
-  BOOST_CHECK(!(sin >> point));
-}
+  BOOST_AUTO_TEST_CASE(firstWrongPointInput)
+  {
+    std::string line("(152;\n123)");
+    std::stringstream sin(line);
+    murzakanov::Point point;
+    BOOST_CHECK(!(sin >> point));
+  }
 
-BOOST_AUTO_TEST_CASE(thirdWrongPointInput)
-{
-  std::string line("(\n152;123)");
-  std::stringstream sin(line);
-  murzakanov::Point point;
-  BOOST_CHECK(!(sin >> point));
-}
+  BOOST_AUTO_TEST_CASE(secondWrongPointInput)
+  {
+    std::string line("(152;123\n)");
+    std::stringstream sin(line);
+    murzakanov::Point point;
+    BOOST_CHECK(!(sin >> point));
+  }
 
-BOOST_AUTO_TEST_CASE(fouthWrongPointInput)
-{
-  std::string line("(152\n;123)");
-  std::stringstream sin(line);
-  murzakanov::Point point;
-  BOOST_CHECK(!(sin >> point));
-}
+  BOOST_AUTO_TEST_CASE(thirdWrongPointInput)
+  {
+    std::string line("(\n152;123)");
+    std::stringstream sin(line);
+    murzakanov::Point point;
+    BOOST_CHECK(!(sin >> point));
+  }
 
-BOOST_AUTO_TEST_CASE(succsessfulPointInput)
-{
-  std::string line("( -152   ;   123   )");
-  std::stringstream sin(line);
-  murzakanov::Point point;
-  BOOST_CHECK((sin >> point));
-}
+  BOOST_AUTO_TEST_CASE(fouthWrongPointInput)
+  {
+    std::string line("(152\n;123)");
+    std::stringstream sin(line);
+    murzakanov::Point point;
+    BOOST_CHECK(!(sin >> point));
+  }
+
+  BOOST_AUTO_TEST_CASE(succsessfulPointInput)
+  {
+    std::string line("( -152   ;   123   )");
+    std::stringstream sin(line);
+    murzakanov::Point point;
+    BOOST_CHECK((sin >> point));
+  }
 
 BOOST_AUTO_TEST_SUITE_END()

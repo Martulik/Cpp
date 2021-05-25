ab088475c4e66fa2c88ff6800fe6f1f4b365a201
vladislav-pochernin <vspochernin@gmail.com>
2021-05-25 21:18:30 +0300

    Add space

--- pochernin.vladislav/B5/task2.cpp
+++ pochernin.vladislav/B5/task2.cpp
@@ -11,7 +11,7 @@
 void pochernin::task2(std::istream& in, std::ostream& out)
 {
   std::vector< Shape > shapes;
-  std::copy(std::istream_iterator< Shape>(in), std::istream_iterator< Shape >(), std::back_inserter(shapes));
+  std::copy(std::istream_iterator< Shape >(in), std::istream_iterator< Shape >(), std::back_inserter(shapes));
 
   unsigned int countOfVertices = std::accumulate(shapes.begin(), shapes.end(), 0, pochernin::sumVertices);
 

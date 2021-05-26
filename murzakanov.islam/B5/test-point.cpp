88db971c1670796cde89ce12daf5fdd458509179
Islam <murzakanovislam21@gmail.com>
2021-05-26 16:50:44 +0300

    Fix unit tests

--- murzakanov.islam/B5/test-point.cpp
+++ murzakanov.islam/B5/test-point.cpp
@@ -7,7 +7,7 @@ BOOST_AUTO_TEST_SUITE(test_point)
 
   BOOST_AUTO_TEST_CASE(firstWrongPointInput)
   {
-    std::string line("(152;\n123)");
+    std::string line("(152-;123)");
     std::stringstream sin(line);
     murzakanov::Point point;
     BOOST_CHECK(!(sin >> point));
@@ -15,7 +15,7 @@ BOOST_AUTO_TEST_SUITE(test_point)
 
   BOOST_AUTO_TEST_CASE(secondWrongPointInput)
   {
-    std::string line("(152;123\n)");
+    std::string line("(aa152;123)");
     std::stringstream sin(line);
     murzakanov::Point point;
     BOOST_CHECK(!(sin >> point));
@@ -23,7 +23,7 @@ BOOST_AUTO_TEST_SUITE(test_point)
 
   BOOST_AUTO_TEST_CASE(thirdWrongPointInput)
   {
-    std::string line("(\n152;123)");
+    std::string line("(152;asdf12)");
     std::stringstream sin(line);
     murzakanov::Point point;
     BOOST_CHECK(!(sin >> point));
@@ -31,7 +31,7 @@ BOOST_AUTO_TEST_SUITE(test_point)
 
   BOOST_AUTO_TEST_CASE(fouthWrongPointInput)
   {
-    std::string line("(152\n;123)");
+    std::string line("(asd12;123)");
     std::stringstream sin(line);
     murzakanov::Point point;
     BOOST_CHECK(!(sin >> point));

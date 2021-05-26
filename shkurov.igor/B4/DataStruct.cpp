e8ac5f91a2b6f63cf87812d020639b6ef2a54f43
Shkurov Igor <lilthewil@gmail.com>
2021-05-26 05:14:12 +0300

    init commit

---
+++ shkurov.igor/B4/DataStruct.cpp
@@ -0,0 +1,61 @@
+#include "DataStruct.hpp"
+
+#include <iostream>
+#include <cmath>
+
+namespace lab = shkurov;
+
+std::istream& lab::operator>>(std::istream& in, lab::DataStruct& data)
+{
+  data.key1 = readKey(in);
+  data.key2 = readKey(in);
+  getline(in, data.str);
+
+  if (in.fail())
+  {
+    throw std::logic_error("Error while reading.");
+  }
+
+  return in;
+}
+
+int lab::readKey(std::istream& in)
+{
+  std::string keyStr;
+  std::getline(in, keyStr, ',');
+
+  int key;
+  try
+  {
+    key = std::stoi(keyStr);
+  }
+  catch (const std::exception& ex)
+  {
+    in.setstate(std::ios_base::failbit);
+  }
+
+  if (std::abs(key) > 5)
+  {
+    in.setstate(std::ios_base::failbit);
+  }
+
+  return (in.good() ? key : 0);
+}
+
+bool lab::compare(const lab::DataStruct& lhs, const lab::DataStruct& rhs)
+{
+  if (lhs.key1 != rhs.key1)
+  {
+    return lhs.key1 < lhs.key1;
+  }
+  else if (lhs.key2 != rhs.key2)
+  {
+    return lhs.key2 < rhs.key2;
+  }
+  return lhs.str.length() < rhs.str.length();
+}
+
+std::ostream& lab::operator<<(std::ostream& out, const lab::DataStruct& data)
+{
+  out << data.key1 << ", " << data.key2 << ", " << data.str << '\n';
+}
---
+++ shkurov.igor/B4/DataStruct.hpp
@@ -0,0 +1,24 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include <string>
+#include <iosfwd>
+
+namespace shkurov
+{
+  struct DataStruct
+  {
+    int         key1;
+    int         key2;
+    std::string str;
+  };
+
+  std::istream& operator>>(std::istream& in, DataStruct& data);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+
+  int readKey(std::istream& in);
+
+  bool compare(const DataStruct& lhs, const DataStruct& rhs);
+}
+
+#endif

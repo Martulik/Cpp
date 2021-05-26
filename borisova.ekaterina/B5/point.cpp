76aadfe66f362d4ce3518ec6805a3ce1e3927b7c
Ekaterina Borisova <kate_boriso2002@mail.ru>
2021-05-26 16:43:12 +0400

    Delete trailing whitespace

--- borisova.ekaterina/B5/point.cpp
+++ borisova.ekaterina/B5/point.cpp
@@ -7,20 +7,20 @@ namespace lab = borisova;
 double lab::getDistance(const Point& first, const Point& second)
 {
   return std::sqrt((first.x - second.x) * (first.x - second.x) +
-    (first.y - second.y) * (first.y - second.y));
+      (first.y - second.y) * (first.y - second.y));
 }
 
 bool lab::isPerpendicularity(const Point& one, const Point& two, const Point three)
 {
-  return ((one.x - two.x) * (three.x - two.x)
-    + (one.y - two.y) * (three.y - two.y)) == 0;
+  return ((one.x - two.x) * (three.x - two.x) +
+      (one.y - two.y) * (three.y - two.y)) == 0;
 }
 
 std::istream& lab::operator>>(std::istream& in, Point& point)
 {
   std::istream::sentry sentry(in);
   if (!sentry)
-  { 
+  {
     throw std::invalid_argument("Empty input\n");
     return in;
   }

b27660766c326e09246ed4ca32e73672c6dc4d79
Shkurov Igor <lilthewil@gmail.com>
2021-05-26 08:50:42 +0300

    added tests

--- shkurov.igor/B4/test-main.cpp
+++ shkurov.igor/B4/test-main.cpp
@@ -1,11 +1,2 @@
 #define BOOST_TEST_MODULE B4
 #include <boost/test/included/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(DataStruct)
-
-BOOST_AUTO_TEST_CASE(test)
-{
-  BOOST_REQUIRE(1 == 1);
-}
-
-BOOST_AUTO_TEST_SUITE_END()

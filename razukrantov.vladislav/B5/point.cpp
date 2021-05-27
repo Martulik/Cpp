14103090672ff847ae82c3ddbc588a335c1f9483
Vladislav <vrazbusiness@mail.ru>
2021-05-27 23:32:26 +0300

    add operator>> for point

--- razukrantov.vladislav/B5/point.cpp
+++ razukrantov.vladislav/B5/point.cpp
@@ -1,6 +1,9 @@
 #include "point.hpp"
 
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
 
 std::ostream& razukrantov::operator<<(std::ostream& out, const Point& point)
 {
@@ -10,5 +13,20 @@ std::ostream& razukrantov::operator<<(std::ostream& out, const Point& point)
 
 std::istream& razukrantov::operator>>(std::istream& in, Point& point)
 {
-  
+  std::string str;
+  std::getline(in, str, '(');
+  if (str.find('\n') != std::string::npos)
+  {
+    throw(std::invalid_argument("Incorrect reading"));
+  }
+  std::getline(in, str, ';');
+  if (str.find(';') == std::string::npos)
+  {
+    throw(std::invalid_argument("Incorrect reading"));
+  }
+  std::string x = str.substr(0, str.find(';'));
+  std::string y = str.substr(0, str.find(';') + 1);
+  point.x = std::stoi(x);
+  point.y = std::stoi(y);
+  return in;
 }

085198b70edb9c605e40bb83e3420b1f1d51a937
Vladislav <vrazbusiness@mail.ru>
2021-05-27 14:44:22 +0300

    add main draft

--- razukrantov.vladislav/B5/main.cpp
+++ razukrantov.vladislav/B5/main.cpp
@@ -1,7 +1,31 @@
 ﻿#include <iostream>
+#include <stdexcept>
+#include <cstring>
 
 int main(int argc, char* argv[])
 {
-  return 0;
+  try
+  {
+    if (argc != 2)
+    {
+      throw(std::invalid_argument("Incorrect atguments count"));
+    }
+    if (!strcmp(argv[1], "1"))
+    {
+      //razukrantov::wordsTask(std::cin, std::cout);
+    }
+    else if (!strcmp(argv[1], "2"))
+    {
+      //razukrantov::shapeTask(std::cin, std::cout);
+    }
+    else
+    {
+      throw(std::invalid_argument("Incorrect arguments"));
+    }
+  }
+  catch (std::exception& error)
+  {
+    std::cerr << error.what() << "\n";
+    return 1;
+  }
 }
-

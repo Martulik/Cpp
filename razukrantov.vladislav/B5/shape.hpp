7cf0f7477101ce33a187956d17a5cf3f92969154
Vladislav <vrazbusiness@mail.ru>
2021-05-28 17:50:24 +0300

    add shape.hpp init version

---
+++ razukrantov.vladislav/B5/shape.hpp
@@ -0,0 +1,16 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include <vector>
+#include <iosfwd>
+
+#include "point.hpp"
+
+namespace razukrantov
+{
+  using Shape = std::vector< Point >;
+  std::istream& operator>>(std::istream& in, Shape& shape);
+  std::ostream& operator<<(std::ostream& out, const Shape& shape);
+}
+
+#endif

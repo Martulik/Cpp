e35b8d446bd4feac8f1cad27ff4dbd04d3f7c26e
ferapontov-mikhail <mferapon@gmail.com>
2021-05-29 12:52:09 +0300

    Fix

--- ferapontov.mikhail/B5/point.cpp
+++ ferapontov.mikhail/B5/point.cpp
@@ -46,7 +46,7 @@ std::ostream& ferapontov::operator<<(std::ostream& out, const Point& point)
   std::ostream::sentry sentry(out);
   if(sentry)
   {
-    return out << '(' << point.x << ';' << point.y << ')';
+    return out << '(' << point.x << "; " << point.y << ')';
   }
   return out;
 }

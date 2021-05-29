fe6e247a6ddd19bbef19a1f0623b7f6e1fcb97ef
LysenkoAleksandra <orange260302@gmail.com>
2021-05-29 16:04:26 +0300

    Change of Shape according to taks2

--- lysenko.aleksandra/B5/Shape.cpp
+++ lysenko.aleksandra/B5/Shape.cpp
@@ -6,72 +6,71 @@
 #include <functional>
 #include <iterator>
 
-std::ostream& lysenko::operator<<(std::ostream& out, const lysenko::point_t& point)
+namespace lysenko
+{
+  void checkCorrectDelimiter(std::istream& in, const char& expected)
+  {
+    char delimiter;
+    in >> delimiter;
+    if ((in.fail()) || (delimiter != expected))
+    {
+      throw std::invalid_argument("Incorrect format point");
+    }
+  }
+}
+
+std::istream& lysenko::operator>>(std::istream& in, lysenko::Shape& Shape)
+{
+  size_t numberOfVerticles;
+  in >> numberOfVerticles;
+
+  if (in.fail())
+  {
+    throw std::invalid_argument("Incorrect format of number of verticles");
+  }
+
+  if (numberOfVerticles == 0)
+  {
+    throw std::invalid_argument("Incorrect number of verticles");
+  }
+
+  std::vector< lysenko::Point > verticles;
+  std::copy_n(std::istream_iterator< Point >(in), numberOfVerticles, std::back_inserter(verticles));
+
+  std::swap(Shape, verticles);
+  return in;
+}
+
+std::ostream& lysenko::operator<<(std::ostream& out, const lysenko::Point& point)
 {
   out << '(' << point.x << ';' << point.y << ')';
   return out;
 }
 
-std::istream& lysenko::operator>>(std::istream& in, lysenko::point_t& point)
+std::istream& lysenko::operator>>(std::istream& in, lysenko::Point& point)
 {
   in >> std::ws;
-  char delimiter;
   int number;
-  in >> delimiter;
-  if ((in.fail()) || (delimiter != '('))
-  {
-    throw std::invalid_argument("Incorrect format point");
-  }
+
+  checkCorrectDelimiter(in, '(');
+
   in >> number;
   if (in.fail())
   {
     throw std::invalid_argument("Incorrect format point");
   }
   point.x = number;
-  in >> delimiter;
-  if ((in.fail()) || (delimiter != ';'))
-  {
-    throw std::invalid_argument("Incorrect format point");
-  }
+
+  checkCorrectDelimiter(in, ';');
+
   in >> number;
   if (in.fail())
   {
     throw std::invalid_argument("Incorrect format point");
   }
   point.y = number;
-  in >> delimiter;
-  if ((in.fail()) || (delimiter != ')'))
-  {
-    throw std::invalid_argument("Incorrect format point");
-  }
+
+  checkCorrectDelimiter(in, ')');
+
   return in;
-}
-
-namespace lysenko
-{
-  std::istream& operator>>(std::istream& in, lysenko::Shape& Shape)
-  {
-    size_t numberOfVerticles;
-    in >> numberOfVerticles;
-
-    if (in.fail())
-    {
-      throw std::invalid_argument("Incorrect format of number of verticles");
-    }
-
-    if (numberOfVerticles == 0)
-    {
-      throw std::invalid_argument("Incorrect number of verticles");
-    }
-
-    std::vector< lysenko::point_t > verticles;
-    std::copy_n(std::istream_iterator< point_t >(in), numberOfVerticles, std::back_inserter(verticles));
-
-    if (!(in.eof()))
-    {
-      throw std::invalid_argument("Too many points for figure");
-    }
-
-    std::swap(Shape.verticles_, verticles);
-  }
 }
--- lysenko.aleksandra/B5/Shape.h
+++ lysenko.aleksandra/B5/Shape.h
@@ -6,22 +6,17 @@
 
 namespace lysenko
 {
-  struct point_t
+  struct Point
   {
     int x;
     int y;
   };
 
-  std::istream& operator>>(std::istream& in, point_t& point);
-  std::ostream& operator<<(std::ostream& out, const point_t& point);
+  std::istream& operator>>(std::istream& in, Point& point);
+  std::ostream& operator<<(std::ostream& out, const Point& point);
 
-  class Shape
-  {
-  public:
-    friend std::istream& operator>>(std::istream& in, Shape& Shape);
-  private:
-    std::vector< point_t > verticles_;
-  };
+  using Shape = std::vector< Point >;
+  std::istream& operator>>(std::istream& in, Shape& Shape);
 }
 
 #endif

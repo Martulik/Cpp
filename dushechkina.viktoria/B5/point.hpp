d4099754a3d74292ca56d6a8d9b5604717229e3d
viktoria.dushechkina <byzaya@icloud.com>
2021-06-03 22:59:31 +0300

    fix point.hpp

--- dushechkina.viktoria/B5/point.hpp
+++ dushechkina.viktoria/B5/point.hpp
@@ -6,21 +6,21 @@
 
 namespace dushechkina
 {
-	struct Point
-	{
+ struct Point
+ {
 	 int x;
 	 int y;
-	};
+};
 
-	std::istream& operator>>(std::istream& in, Point& point);
-	std::ostream& operator<<(std::ostream& out, const Point& point);
+ std::istream& operator>>(std::istream& in, Point& point);
+ std::ostream& operator<<(std::ostream& out, const Point& point);
 
-	using Shape = std::vector< Point >;
-	bool isTriangle(const Shape& shape);
-	bool isRectangle(const Shape& shape);
-	bool isPentagon(const Shape& shape);
-	bool isSquare(const Shape& shape);
-	std::istream& operator>>(std::istream& in, Shape& shape);
-	std::ostream& operator<<(std::ostream& out, const Shape& shape);
+ using Shape = std::vector< Point >;
+ bool isTriangle(const Shape& shape);
+ bool isRectangle(const Shape& shape);
+ bool isPentagon(const Shape& shape);
+ bool isSquare(const Shape& shape);
+ std::istream& operator>>(std::istream& in, Shape& shape);
+ std::ostream& operator<<(std::ostream& out, const Shape& shape);
 }
 #endif

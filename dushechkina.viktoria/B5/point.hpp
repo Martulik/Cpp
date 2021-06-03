608a6f59e9e2d6cc46530c7695227c1c79848e32
viktoria.dushechkina <byzaya@icloud.com>
2021-06-03 23:01:14 +0300

    fix CG

--- dushechkina.viktoria/B5/point.hpp
+++ dushechkina.viktoria/B5/point.hpp
@@ -8,9 +8,9 @@ namespace dushechkina
 {
  struct Point
  {
-	 int x;
-	 int y;
-};
+	int x;
+	int y;
+ };
 
  std::istream& operator>>(std::istream& in, Point& point);
  std::ostream& operator<<(std::ostream& out, const Point& point);

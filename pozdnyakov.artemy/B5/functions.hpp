0057e10e0cb723f3aa5ac3bbdc922d6280c3586e
pozdnyakov-artemy <aapozdnyakove@gmail.com>
2021-06-03 03:00:58 +0100

    Removed unneccessary functions

--- pozdnyakov.artemy/B5/functions.hpp
+++ pozdnyakov.artemy/B5/functions.hpp
@@ -8,25 +8,6 @@
 
 namespace pozdnyakov
 {
-  template < class T, class InputIterator >
-  void for_each_and_first(InputIterator it1, InputIterator it2, std::function< void(T, T) >& op)
-  {
-    assert(it1 != it2);
-    std::function< void(T) > boundOp = std::bind(op, *it1, std::placeholders::_1);
-    std::for_each(std::next(it1), it2, boundOp);
-  }
-  template < class T, class InputIterator >
-  void for_comb(InputIterator it1, InputIterator it2, std::function< void(T, T) >& op)
-  {
-    std::function< void(InputIterator) > boundForEach = std::bind(for_each_and_first< T, InputIterator >, std::placeholders::_1, it2, op);
-  notFor:
-    if (it1 != it2)
-    {
-      boundForEach(it1);
-      it1++;
-      goto notFor;
-    }
-  }
   void checkLineEnding(const std::string& str);
   bool compChar(char a, char b);
   bool compCharNeg(char a, char b);

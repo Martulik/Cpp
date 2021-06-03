6686af9c5726c123b8555d4c71a7e281f3491b47
viktoria.dushechkina <byzaya@icloud.com>
2021-06-03 23:05:07 +0300

    fix all x2

--- dushechkina.viktoria/B5/point.hpp
+++ dushechkina.viktoria/B5/point.hpp
@@ -4,23 +4,4 @@
 #include <vector>
 #include <iosfwd>
 
-namespace dushechkina
-{
- struct Point
- {
-	int x;
-	int y;
- };
-
- std::istream& operator>>(std::istream& in, Point& point);
- std::ostream& operator<<(std::ostream& out, const Point& point);
-
- using Shape = std::vector< Point >;
- bool isTriangle(const Shape& shape);
- bool isRectangle(const Shape& shape);
- bool isPentagon(const Shape& shape);
- bool isSquare(const Shape& shape);
- std::istream& operator>>(std::istream& in, Shape& shape);
- std::ostream& operator<<(std::ostream& out, const Shape& shape);
-}
 #endif

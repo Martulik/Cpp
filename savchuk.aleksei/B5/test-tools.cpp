805262c0be21aae892a2445520c1f576b75bc8f1
Aleksei Savchuk <sav.alex1917@gmail.com>
2021-06-03 13:01:46 +0300

    Add unit tests for ShapeGenerator

--- savchuk.aleksei/B5/test-tools.cpp
+++ savchuk.aleksei/B5/test-tools.cpp
@@ -4,8 +4,6 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include "shape.hpp"
-
 namespace lab = savchuk;
 
 void lab::checkCorrectInput(const char* str)
@@ -20,4 +18,16 @@ void lab::checkIncorrectInput(const char* str)
   std::istringstream ss(str);
   Shape shape;
   BOOST_CHECK(!(ss >> shape));
+}
+
+void lab::checkRandomShape(const Shape& shape)
+{
+  if (shape.size() == 4)
+  {
+    BOOST_CHECK(isSquare(shape));
+  }
+  else
+  {
+    BOOST_CHECK(shape.size() == 8);
+  }
 }

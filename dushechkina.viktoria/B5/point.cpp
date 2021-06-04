f35c615969500e2f282867d65c93fff72c16dce7
viktoria.dushechkina <byzaya@icloud.com>
2021-06-04 17:54:01 +0300

    add all for shape

--- dushechkina.viktoria/B5/point.cpp
+++ dushechkina.viktoria/B5/point.cpp
@@ -58,9 +58,43 @@ bool dushechkina::isPentagon(const Shape& shape)
 
 bool dushechkina::isSquare(const Shape& shape)
 {
-  int side1 = getDistance(shape[0], shape[1]);
-  int side2 = getDistance(shape[1], shape[2]);
-  int side3 = getDistance(shape[2], shape[3]);
-  int side4 = getDistance(shape[3], shape[0]);
-  return (side1 == side2) && (side2 == side3) && (side3 == side4) && (side1 == side4);
+  int AB = getDistance(shape[0], shape[1]);
+  int BC = getDistance(shape[1], shape[2]);
+  int CD = getDistance(shape[2], shape[3]);
+  int AD = getDistance(shape[3], shape[0]);
+
+  return (AB == BC) && (BC == CD) && (CD == AD) && (AB == AD);
+}
+
+std::istream& dushechkina::operator>>(std::istream& in, Shape& shape)
+{
+  size_t n = 0;
+  if (!(in >> n))
+  {
+    return in;
+  }
+
+  if (n <= 2)
+  {
+    throw std::invalid_argument("Invalid figure input");
+  }
+
+  Shape tempShape;
+  tempShape.reserve(n);
+  std::copy_n(std::istream_iterator< Point >(in), n, std::back_inserter(tempShape));
+
+  if (tempShape.size() != n || (in.fail() && !in.eof()))
+  {
+    throw std::invalid_argument("Invalid number of shape vertices");
+  }
+
+  shape = tempShape;
+  return in;
+}
+
+std::ostream& dushechkina::operator<<(std::ostream& out, const Shape& shape)
+{
+  out << shape.size() << ' ';
+  std::copy(shape.begin(), shape.end(), std::ostream_iterator< Point >(out, " "));
+  return out;
 }

da9a5a6a4ffe02bd959761e7932b15656120537b
Shkurov Igor <lilthewil@gmail.com>
2021-06-05 05:17:57 +0300

    redesigned set fill

--- shkurov.igor/B5/task1.cpp
+++ shkurov.igor/B5/task1.cpp
@@ -8,7 +8,6 @@ using istreamIt = std::istream_iterator< std::string >;
 
 void task1(std::istream& in, std::ostream& out)
 {
-  std::unordered_set< std::string > set;
-  std::copy(istreamIt(in), istreamIt(), std::inserter(set, std::next(set.begin())));
+  std::unordered_set< std::string > set(istreamIt(in), istreamIt());
   std::copy(istreamIt(in), istreamIt(), std::ostream_iterator< std::string >( out, "\n"));
 }

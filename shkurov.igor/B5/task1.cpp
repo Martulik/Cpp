314a63aeb1545ee4d07716200225db937c5b3adb
Shkurov Igor <lilthewil@gmail.com>
2021-06-06 21:29:36 +0300

    fixed output in task1

--- shkurov.igor/B5/task1.cpp
+++ shkurov.igor/B5/task1.cpp
@@ -9,5 +9,5 @@ using istreamIt = std::istream_iterator< std::string >;
 void lab::task1(std::istream& in, std::ostream& out)
 {
   std::unordered_set< std::string > set((istreamIt(in)), istreamIt());
-  std::copy(istreamIt(in), istreamIt(), std::ostream_iterator< std::string >( out, "\n"));
+  std::copy(set.begin(), set.end(), std::ostream_iterator< std::string >(out, "\n"));
 }

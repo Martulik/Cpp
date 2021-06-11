3b44782a87aab0541b6e953b746a0923e4f10671
viktoria.dushechkina <byzaya@icloud.com>
2021-06-11 22:33:21 +0300

    fix whitespaces

--- dushechkina.viktoria/B5/extra-task.hpp
+++ dushechkina.viktoria/B5/extra-task.hpp
@@ -7,11 +7,11 @@
 
 namespace dushechkina
 {
-	Shape generateFigure(std::mt19937& rand);
-	bool isSquareNotSelfInter(const Shape& shape);
-	bool isRectNotSelfInter(const Shape& shape);
-	Shape changeSquareSelfInter(Shape shape);
-	Shape changeRectSelfInter(Shape& shape);
+  Shape generateFigure(std::mt19937& rand);
+  bool isSquareNotSelfInter(const Shape& shape);
+  bool isRectNotSelfInter(const Shape& shape);
+  Shape changeSquareSelfInter(Shape shape);
+  Shape changeRectSelfInter(Shape& shape);
 }
 
 #endif

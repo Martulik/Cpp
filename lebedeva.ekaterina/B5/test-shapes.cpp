41ab620c1d01fdc74327dcbfdc814dd28cd82d67
kate-lebedeva <katty.papina@gmail.com>
2021-06-14 02:57:40 +0300

    Added tests for personal task

--- lebedeva.ekaterina/B5/test-shapes.cpp
+++ lebedeva.ekaterina/B5/test-shapes.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <random>
 #include "functions.hpp"
+#include "test-variables.hpp"
 
 BOOST_AUTO_TEST_SUITE(TestShape)
 BOOST_AUTO_TEST_CASE(myTestCase)
@@ -15,36 +16,49 @@ BOOST_AUTO_TEST_CASE(myTestCase)
   lebedeva::printShapes(std::cout, shapes);
   BOOST_TEST(info.squares == 0);
 }
+
+BOOST_AUTO_TEST_CASE(myTestCase1)
+{
+  lebedeva::Shape s1 = { { 6147, 5156 }, { 8036, 3394 }, { 6892, 2168 }, { 5003, 3930 } };
+  lebedeva::printPoints(std::cout, s1);
+  std::cout << '\n';
+  BOOST_TEST(lebedeva::isRectangleAdvanced(s1) == false);
+}
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(TestExtraTask)
+std::random_device gen;
 
 BOOST_AUTO_TEST_CASE(TestRandowQuadrilateral)
 {
-  std::random_device gen;
   lebedeva::Shape quadrilateral = lebedeva::makeQuadrilateral(gen);
+  std::cout << "\nTest random quadrilateral\n";
   lebedeva::printPoints(std::cout, quadrilateral);
   bool isSquare = lebedeva::isSquareAdvanced(quadrilateral);
   bool isRect = lebedeva::isRectangleAdvanced(quadrilateral);
   std::cout << "\nIs quadrilateral a rectangle? " << (isRect ? "yes\n" : "no\n");
   if (isRect)
   {
-    if (isSquare)
+    bool isCrossedRect = lebedeva::isCrossedRectangle(quadrilateral);
+    std::cout << "\nIs quadrilateral a crossed rectangle? " << (isCrossedRect ? "yes\n" : "no\n");
+    if (isCrossedRect)
+    {
+      BOOST_CHECK_EQUAL(isCrossedRect, true);
+    }
+    else if (isSquare)
     {
       std::cout << "\nIs quadrilateral a square? " << (isSquare ? "yes\n" : "no\n");
-      BOOST_CHECK_EQUAL(lebedeva::isSquare(quadrilateral), true);
+      isSquare = lebedeva::isSquare(quadrilateral);
+      if (isSquare)
+      {
+        BOOST_CHECK_EQUAL(lebedeva::isSquare(quadrilateral), true);
+      }
       isSquare = lebedeva::isCrossedSquare(quadrilateral);
       std::cout << "\nIs quadrilateral a crossed square? " << (isSquare ? "yes\n" : "no\n");
       if (isSquare)
       {
         BOOST_CHECK_EQUAL(isSquare, true);
       }
-    }
-    else
-    {
-      bool isCrossedRect = lebedeva::isCrossedRectangle(quadrilateral);
-      std::cout << "\nIs quadrilateral a crossed rectangle? " << (isCrossedRect ? "yes\n" : "no\n");
-      BOOST_CHECK_EQUAL(isCrossedRect, true);
     }
   }
   else
@@ -53,4 +67,69 @@ BOOST_AUTO_TEST_CASE(TestRandowQuadrilateral)
   }
 }
 
+BOOST_AUTO_TEST_CASE(TestDefaultRect)
+{
+  lebedeva::Shape tRect = lebedeva::rect;
+  BOOST_CHECK_EQUAL(lebedeva::isRectangle(tRect), true);
+  BOOST_CHECK_EQUAL(lebedeva::isRectangleAdvanced(tRect), true);
+  BOOST_CHECK_EQUAL(lebedeva::isCrossedRectangle(tRect), false);
+}
+
+BOOST_AUTO_TEST_CASE(TestCrossedNotRotatedRect)
+{
+  lebedeva::Shape tRect = lebedeva::rect;
+  lebedeva::makeCrossed(tRect, gen);
+  BOOST_CHECK_EQUAL(lebedeva::isRectangle(tRect), true);
+  BOOST_CHECK_EQUAL(lebedeva::isRectangleAdvanced(tRect), true);
+  BOOST_CHECK_EQUAL(lebedeva::isCrossedRectangle(tRect), true);
+}
+
+BOOST_AUTO_TEST_CASE(TestNotCrossedRotatedRect)
+{
+  lebedeva::Shape rect = lebedeva::rectRotated;
+  BOOST_CHECK_EQUAL(lebedeva::isRectangle(rect), true);
+  BOOST_CHECK_EQUAL(lebedeva::isRectangleAdvanced(rect), true);
+  BOOST_CHECK_EQUAL(lebedeva::isCrossedRectangle(rect), false);
+}
+
+BOOST_AUTO_TEST_CASE(TestCrossedRotatedRect)
+{
+  lebedeva::Shape rect = lebedeva::rectRotated;
+  lebedeva::makeCrossed(rect, gen);
+  BOOST_CHECK_EQUAL(lebedeva::isRectangle(rect), true);
+  BOOST_CHECK_EQUAL(lebedeva::isRectangleAdvanced(rect), true);
+  BOOST_CHECK_EQUAL(lebedeva::isCrossedRectangle(rect), true);
+}
+
+BOOST_AUTO_TEST_CASE(TestDefaultSquare)
+{
+  lebedeva::Shape square = lebedeva::square;
+  BOOST_CHECK_EQUAL(lebedeva::isSquare(square), true);
+  BOOST_CHECK_EQUAL(lebedeva::isSquareAdvanced(square), true);
+  BOOST_CHECK_EQUAL(lebedeva::isCrossedSquare(square), false);
+}
+
+BOOST_AUTO_TEST_CASE(TestCrossedNotRotatedSquare)
+{
+  lebedeva::Shape square = lebedeva::square;
+  lebedeva::makeCrossed(square, gen);
+  BOOST_CHECK_EQUAL(lebedeva::isSquareAdvanced(square), true);
+  BOOST_CHECK_EQUAL(lebedeva::isCrossedSquare(square), true);
+}
+
+BOOST_AUTO_TEST_CASE(TestNotCrossedRotatedSquare)
+{
+  lebedeva::Shape square = lebedeva::squareRotated;
+  BOOST_CHECK_EQUAL(lebedeva::isSquare(square), true);
+  BOOST_CHECK_EQUAL(lebedeva::isSquareAdvanced(square), true);
+  BOOST_CHECK_EQUAL(lebedeva::isCrossedSquare(square), false);
+}
+
+BOOST_AUTO_TEST_CASE(TestCrossedRotatedSquare)
+{
+  lebedeva::Shape square = lebedeva::squareRotated;
+  lebedeva::makeCrossed(square, gen);
+  BOOST_CHECK_EQUAL(lebedeva::isSquareAdvanced(square), true);
+  BOOST_CHECK_EQUAL(lebedeva::isCrossedSquare(square), true);
+}
 BOOST_AUTO_TEST_SUITE_END()
---
+++ lebedeva.ekaterina/B5/test-variables.hpp
@@ -0,0 +1,12 @@
+#ifndef TEST_VARIABLES_HPP
+#define TEST_VARIABLES_HPP
+#include "functions.hpp"
+
+namespace lebedeva
+{
+  const Shape rect = { { -7, -8 }, { -7, -4 }, { -5, -4 }, { -5, -8 } };
+  const Shape rectRotated = { { 3, 2 }, { 4, 5 }, { 7, 4 }, { 6, 1 } };
+  const Shape square = { { 1, 2 }, { 1, 5 }, { 4, 5 }, { 4, 2 } };
+  const Shape squareRotated = { { 9, -2 }, { 14, -7 }, { 9, -12 }, { 4, -7 } };
+}
+#endif

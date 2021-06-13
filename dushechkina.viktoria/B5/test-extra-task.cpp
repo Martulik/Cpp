6501b1183e6ab905874ad941b4c0ee9246d9705a
viktoria.dushechkina <byzaya@icloud.com>
2021-06-13 15:03:47 +0300

    fix CG x2

--- dushechkina.viktoria/B5/test-extra-task.cpp
+++ dushechkina.viktoria/B5/test-extra-task.cpp
@@ -8,27 +8,27 @@ BOOST_AUTO_TEST_SUITE(testingExtraTask)
 
 BOOST_AUTO_TEST_CASE(correctGenerateFigure)
 {
-	std::mt19937 rand(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-	dushechkina::Shape shape;
-	shape = dushechkina::generateFigure(rand);
-	BOOST_CHECK(dushechkina::isRectangle(shape));
+ std::mt19937 rand(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+ dushechkina::Shape shape;
+ shape = dushechkina::generateFigure(rand);
+ BOOST_CHECK(dushechkina::isRectangle(shape));
 }
 
 BOOST_AUTO_TEST_CASE(correctWorkFunctionNotSelfInter)
 {
-	std::istringstream in("4 (0;0) (0;1) (1;1) (1;0)");
-	dushechkina::Shape shape;
-	in >> shape;
-	BOOST_CHECK(dushechkina::isShapeNotSelfInter(shape));
+ std::istringstream in("4 (0;0) (0;1) (1;1) (1;0)");
+ dushechkina::Shape shape;
+ in >> shape;
+ BOOST_CHECK(dushechkina::isShapeNotSelfInter(shape));
 }
 
 BOOST_AUTO_TEST_CASE(correctWorkFunctionDeleteSelfInter)
 {
-	std::istringstream in("4 (0;1) (0;0) (1;1) (1;0)");
-	dushechkina::Shape shape;
-	in >> shape;
-	dushechkina::changeShapeSelfInter(f);
-	BOOST_CHECK(dushechkina::isShapeNotSelfInter(shape));
+ std::istringstream in("4 (0;1) (0;0) (1;1) (1;0)");
+ dushechkina::Shape shape;
+ in >> shape;
+ dushechkina::changeShapeSelfInter(f);
+ BOOST_CHECK(dushechkina::isShapeNotSelfInter(shape));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
